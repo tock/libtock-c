09a2dbbb3e45e0de76a5aa6d92bc3a3a312264f4
Brad Campbell <bradjc5@gmail.com>
2019-06-11 17:59:42 -0400

    crt0: add startup support for riscv

--- libtock/crt0.c
+++ libtock/crt0.c
@@ -143,6 +143,87 @@ void _start(void* app_start __attribute__((unused)),
     "bl _c_start\n"
     "bkpt #255\n"
     );
+
+#elif defined(__riscv)
+
+  asm volatile (
+    // Compute the stack top.
+    //
+    // struct hdr* myhdr = (struct hdr*) app_start;
+    // uint32_t stacktop = (((uint32_t) mem_start + myhdr->stack_size + 7) & 0xfffffff8);
+    "lw   t0, 36(a0)\n"         // t0 = myhdr->stack_size
+    "addi t0, t0, 7\n"          // t0 = myhdr->stack_size + 7
+    "add  t0, t0, a1\n"         // t0 = mem_start + myhdr->stack_size + 7
+    "li   t1, 7\n"              // t1 = 7
+    "not  t1, t1\n"             // t1 = ~0x7
+    "and  t0, t0, t1\n"         // t0 = (mem_start + myhdr->stack_size + 7) & ~0x7
+    //
+    // Compute the app data size and where initial app brk should go.
+    // This includes the GOT, data, and BSS sections. However, we can't be sure
+    // the linker puts them back-to-back, but we do assume that BSS is last
+    // (i.e. myhdr->got_start < myhdr->bss_start && myhdr->data_start <
+    // myhdr->bss_start). With all of that true, then the size is equivalent
+    // to the end of the BSS section.
+    //
+    // uint32_t appdata_size = myhdr->bss_start + myhdr->bss_size;
+    "lw   t1, 24(a0)\n"         // t1 = myhdr->bss_start
+    "lw   t2, 28(a0)\n"         // t2 = myhdr->bss_size
+    "lw   t3,  4(a0)\n"         // t3 = myhdr->got_start
+    "add  t1, t1, t2\n"         // t1 = bss_start + bss_size
+    //
+    // Move arguments we need to keep over to callee-saved locations.
+    "mv   s0, a0\n"             // s0 = void* app_start
+    "mv   s1, t0\n"             // s1 = stack_top
+    //
+    // Now we may want to move the stack pointer. If the kernel set the
+    // `app_heap_break` larger than we need (and we are going to call `brk()`
+    // to reduce it) then our stack pointer will fit and we can move it now.
+    // Otherwise after the first syscall (the memop to set the brk), the return
+    // will use a stack that is outside of the process accessible memory.
+    //
+    "add t2, t0, t1\n"          // t2 = stacktop + appdata_size
+    "bgt t2, a3, skip_set_sp\n" // Compare `app_heap_break` with new brk.
+                                // If our current `app_heap_break` is larger
+                                // then we need to move the stack pointer
+                                // before we call the `brk` syscall.
+    "mv  sp, t0\n"              // Update the stack pointer
+
+    "skip_set_sp:\n"            // Back to regularly scheduled programming.
+
+    // Call `brk` to set to requested memory
+
+    // memop(0, stacktop + appdata_size);
+    "li  a0, 4\n"               // a0 = 4   // memop syscall
+    "li  a1, 0\n"               // a1 = 0
+    "mv  a2, t2\n"              // a2 = stacktop + appdata_size
+    "ecall\n"                   // memop
+    //
+    // Debug support, tell the kernel the stack location
+    //
+    // memop(10, stacktop);
+    "li  a0, 4\n"               // a0 = 4   // memop syscall
+    "li  a1, 10\n"              // a1 = 10
+    "mv  a2, s1\n"              // a2 = stacktop
+    "ecall\n"                   // memop
+    //
+    // Debug support, tell the kernel the heap location
+    //
+    // memop(11, stacktop + appdata_size);
+    "li  a0, 4\n"               // a0 = 4   // memop syscall
+    "li  a1, 11\n"              // a1 = 10
+    "mv  a2, t2\n"              // a2 = stacktop + appdata_size
+    "ecall\n"                   // memop
+    //
+    // Setup initial stack pointer for normal execution
+    "mv   sp, s1\n"             // sp = stacktop
+    "mv   s0, sp\n"             // Set the frame pointer to sp.
+    //
+    // Call into the rest of startup. This should never return.
+    "mv   a0, s0\n"             // first arg is app_start
+    "mv   a1, s1\n"             // second arg is stacktop
+    "jal  _c_start\n"
+  );
+
 #else
 #error Missing initial stack setup trampoline for current arch.
 #endif
