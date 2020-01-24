2df1ac8b6cb903c1a28c71e3d6f09e75b7928744
Adam H. Leventhal <ahl@oxide.computer>
2020-01-24 11:57:28 -0800

    fix debug flags for riscv

--- Configuration.mk
+++ Configuration.mk
@@ -105,7 +105,13 @@ override LEGACY_LIBS_rv32imac += \
 override CPPFLAGS += -include $(TOCK_USERLAND_BASE_DIR)/support/warning_header.h
 
 # Flags for creating application Object files
-OBJDUMP_FLAGS += --disassemble-all --source --disassembler-options=force-thumb -C --section-headers
+OBJDUMP_FLAGS += --disassemble-all --source -C --section-headers
+
+override CPPFLAGS_cortex-m += \
+      -disassembler-options=force-thumb
+override OBJDUMP_cortex-m4 += $(CPPFLAGS_cortex-m)
+override OBJDUMP_cortex-m3 += $(CPPFLAGS_cortex-m)
+override OBJDUMP_cortex-m0 += $(CPPFLAGS_cortex-m)
 
 # Use a generic linker script that over provisions.
 LAYOUT ?= $(TOCK_USERLAND_BASE_DIR)/userland_generic.ld
