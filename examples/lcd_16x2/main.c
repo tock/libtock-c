6b9cac179c31c70ce74178a361901b8e10b458e7
Teona Severin <teona.severin9@gmail.com>
2020-03-04 18:28:31 +0200

    format example lcd

--- examples/lcd_16x2/main.c
+++ examples/lcd_16x2/main.c
@@ -15,20 +15,20 @@ int d2 = 2;
 int d3 = 1;
 
 int main(void) {
-    char str[12] = "Hello world";
+  char str[12] = "Hello world";
 
-    LCD_16x2 lcd;
-    lcd_1602_start(&lcd, 6, rs, en, d0, d1, d2, d3);
-    lcd_1602_begin(&lcd, 16, 2);
+  LCD_16x2 lcd;
+  lcd_1602_start(&lcd, 6, rs, en, d0, d1, d2, d3);
+  lcd_1602_begin(&lcd, 16, 2);
 
-    lcd_1602_print_string(&lcd, str);
-    lcd_1602_set_cursor(&lcd, 0, 1);
-    lcd_1602_print_number(&lcd, 123456);
+  lcd_1602_print_string(&lcd, str);
+  lcd_1602_set_cursor(&lcd, 0, 1);
+  lcd_1602_print_number(&lcd, 123456);
 
-    delay_ms(2000);
+  delay_ms(2000);
 
-    while (1) {
-        // 
-    }
-    return 0;
+  while (1) {
+    //
+  }
+  return 0;
 }
