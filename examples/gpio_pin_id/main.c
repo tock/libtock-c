8a06f3fe155a6ab103f5251ea26097830b25dfcb
Alexandru Radovici <msg4alex@gmail.com>
2020-03-08 15:23:53 +0200

    format

--- examples/gpio_pin_id/main.c
+++ examples/gpio_pin_id/main.c
@@ -1,8 +1,8 @@
 #include <gpio.h>
-#include <timer.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <timer.h>
 #include <unistd.h>
 
 #include <console.h>
@@ -18,15 +18,14 @@ static void nop(
 int main(void) {
   // search for pin PE09 (nucleo f429zi)
   // msb 4 bits, port E (4th port, staring from 0) - 0100
-  // lsb 4 bits, pin 9 - 1001 
+  // lsb 4 bits, pin 9 - 1001
   int pin_number = gpio_get_pin_by_id (0b01001001);
 
-  snprintf (pin_str, sizeof (pin_str)-1, "Found pin number %d\r\n", pin_number);
-  
+  snprintf (pin_str, sizeof(pin_str) - 1, "Found pin number %d\r\n", pin_number);
+
   putnstr_async(pin_str, strlen(pin_str), nop, NULL);
 
-  if (pin_number > 0)
-  {
+  if (pin_number > 0) {
     gpio_enable_output (pin_number);
     while (1)
     {
@@ -38,7 +37,5 @@ int main(void) {
   }
 
   while (1)
-  {
-
-  }
+  {}
 }
