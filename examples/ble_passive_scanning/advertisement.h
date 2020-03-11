f4ab36a1d4eb228802d4de2bac907e44dc40fbc4
Tobias Schaffner <tobiasschaffner87@outlook.com>
2020-03-11 14:35:33 +0100

    Fix wrong header handling in ble passive scanning
    * Only read the first 4 bits instead of the first byte to get the PDU type.
    * Read out TxAdd and RxAdd bytes correctly.
    * Some improvements on printouts and some additional constants.

--- examples/ble_passive_scanning/advertisement.h
+++ examples/ble_passive_scanning/advertisement.h
@@ -2,31 +2,41 @@
 
 #include <cstring>
 
-// this is left outside the class because it doesn't work
-// code-generation bug?!
-const int ADDRESS_SIZE = 6;
-const int DATA_SIZE = 32;
-const int DATA_START = 8;
-const int DATA_END = 39;
-const int ADV_TYPE = 0;
+const unsigned char HEADER_START = 0;
+const unsigned char HEADER_SIZE = 2;
+const unsigned char ADDRESS_START = 2;
+const unsigned char ADDRESS_SIZE = 6;
+const unsigned char DATA_START = 8;
+const unsigned char DATA_MAX_SIZE = 31;
+
+const unsigned char ADV_MIN_SIZE = 8;
+const unsigned char ADV_MAX_SIZE = HEADER_SIZE + ADDRESS_SIZE + DATA_MAX_SIZE;
+
+const unsigned char PDU_ADDRESS_HEADER_MASK = 0x3F;
+const unsigned char PDU_TYPE_HEADER_MASK = 0xF;
+const unsigned char PDU_TXADD_HEADER_MASK = 0x40;
+const unsigned char PDU_RXADD_HEADER_MASK = 0x80;
 
 class Advertisement {
   private:
-    unsigned char advertisementType_;
-    int len_;
+    unsigned char header_[HEADER_SIZE];
     unsigned char address_[ADDRESS_SIZE];
-    unsigned char data_[DATA_SIZE];
+    unsigned char data_[DATA_MAX_SIZE];
 
   public:
-    // Constructors 
+    // Constructors
     Advertisement(const unsigned char* buf, int len);
     Advertisement();
-    
+
     // Methods
     bool device_detected(const Advertisement& other) const;
     bool operator==(const Advertisement& other) const;
     bool operator!=(const Advertisement& other) const;
-    void print() const;
-    const char* advertisementTypeToStr() const;
     static bool validAdvertisement(const unsigned char* buf, int len);
+    unsigned char pduType() const;
+    const char* pduTypeStr() const;
+    bool pduTxAddSet() const;
+    bool pduRxAddSet() const;
+    unsigned char pduLength() const;
+    void print() const;
 };
