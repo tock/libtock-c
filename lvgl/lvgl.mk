5cf19b69d388b0b82e6c7b8f241c55be62fec845
Alexandru Radovici <msg4alex@gmail.com>
2020-05-09 20:32:47 +0300

    lvgl stable

--- lvgl/lvgl.mk
+++ lvgl/lvgl.mk
@@ -1,4 +1,4 @@
 
-override CFLAGS += -Wno-error=unused-macros
+override CFLAGS += -Wno-error=unused-macros -Wno-error=sign-compare -Wno-error=float-equal -Wno-error=unused-parameter -Wno-error=unused-variable -Wno-error=shadow
 
 include $(LVGL_DIR)/lvgl/lvgl.mk
