8ce2321da02938b8f4318b2e445928b71d6549da
Alexandru Radovici <msg4alex@gmail.com>
2020-05-09 20:38:36 +0300

    lvgl makefile

--- lvgl/lvgl.mk
+++ lvgl/lvgl.mk
@@ -1,4 +1,6 @@
 
-override CFLAGS += -Wno-error=unused-macros -Wno-error=sign-compare -Wno-error=float-equal -Wno-error=unused-parameter -Wno-error=unused-variable -Wno-error=shadow
+override CFLAGS += -Wno-error=unused-macros -Wno-error=sign-compare -Wno-error=float-equal\
+  -Wno-error=unused-parameter -Wno-error=unused-variable\
+  -Wno-error=shadow -Wno-error=type-limits -Wno-error=overflow
 
 include $(LVGL_DIR)/lvgl/lvgl.mk
