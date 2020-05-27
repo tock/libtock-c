54b623504724000477f35ef7c02726c18dfae9d6
Brad Campbell <bradjc5@gmail.com>
2020-05-27 14:30:55 -0400

    revert riscv changes for PR

--- examples/blink/main.c
+++ examples/blink/main.c
@@ -18,13 +18,5 @@ int main(void) {
 
     // This delay uses an underlying timer in the kernel.
     delay_ms(250);
-
-
-    // NOTE!
-    //
-    // For arty-e21 board, I have to change `delay_ms(250)`
-    // to something like `delay_ms(5000)` for it to work. The
-    // e21 hardware does not seem to like setting machine timer
-    // compare to too low of a value.
   }
 }
