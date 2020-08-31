0a1376f2d0e44c2a783eef4dae8fbd338cd17214
Arjun Deopujari <ajd4mq@virginia.edu>
2020-08-31 01:58:25 -0400

    Test prox sensor in sensors/main.c

--- examples/sensors/main.c
+++ examples/sensors/main.c
@@ -15,24 +15,25 @@
 static bool isl29035 = false;
 // TODO: modify tmp006 to comply with the generic temperature interface
 // and then remove tmp006!!!
-static bool tmp006        = false;
-static bool tsl2561       = false;
-static bool lps25hb       = false;
-static bool temperature   = false;
-static bool humidity      = false;
-static bool ninedof       = false;
+static bool tmp006 = false;
+static bool tsl2561 = false;
+static bool lps25hb = false;
+static bool temperature = false;
+static bool humidity = false;
+static bool ninedof = false;
 static bool ninedof_accel = false;
-static bool ninedof_mag   = false;
-static bool ninedof_gyro  = false;
-static bool proximity     = false;
+static bool ninedof_mag = false;
+static bool ninedof_gyro = false;
+static bool proximity = false;
 
-static void timer_fired(__attribute__ ((unused)) int arg0,
-                        __attribute__ ((unused)) int arg1,
-                        __attribute__ ((unused)) int arg2,
-                        __attribute__ ((unused)) void* ud) {
+static void timer_fired(__attribute__((unused)) int arg0,
+                        __attribute__((unused)) int arg1,
+                        __attribute__((unused)) int arg2,
+                        __attribute__((unused)) void *ud)
+{
   int light = 0;
-  int16_t tmp006_temp  = 0;
-  int tsl2561_lux      = 0;
+  int16_t tmp006_temp = 0;
+  int tsl2561_lux = 0;
   int lps25hb_pressure = 0;
   int temp = 0;
   unsigned humi = 0;
@@ -41,55 +42,92 @@ static void timer_fired(__attribute__ ((unused)) int arg0,
   int ninedof_gyro_x = 0, ninedof_gyro_y = 0, ninedof_gyro_z = 0;
   unsigned proxReading = 0;
 
-
   /* *INDENT-OFF* */
-  if (isl29035)     ambient_light_read_intensity_sync(&light);
-  if (tmp006)       tmp006_read_sync(&tmp006_temp);
-  if (tsl2561)      tsl2561_lux = tsl2561_get_lux_sync();
-  if (lps25hb)      lps25hb_pressure = lps25hb_get_pressure_sync();
-  if (temperature)  temperature_read_sync(&temp);
-  if (humidity)     humidity_read_sync(&humi);
-  if (proximity)    proximity_read_sync(&proxReading);
+  if (isl29035)
+    ambient_light_read_intensity_sync(&light);
+  if (tmp006)
+    tmp006_read_sync(&tmp006_temp);
+  if (tsl2561)
+    tsl2561_lux = tsl2561_get_lux_sync();
+  if (lps25hb)
+    lps25hb_pressure = lps25hb_get_pressure_sync();
+  if (temperature)
+    temperature_read_sync(&temp);
+  if (humidity)
+    humidity_read_sync(&humi);
+  if (proximity)
+    proximity_read_sync(&proxReading);
 
-  if (isl29035)       printf("ISL29035:   Light Intensity: %d\n", light);
-  if (tmp006)         printf("TMP006:     Temperature:     %d\n", tmp006_temp);
-  if (tsl2561)        printf("TSL2561:    Light:           %d lux\n", tsl2561_lux);
-  if (lps25hb)        printf("LPS25HB:    Pressure:        %d\n", lps25hb_pressure);
-  if (temp)           printf("Temperature:                 %d deg C\n", temp/100);
-  if (humi)           printf("Humidity:                    %u%%\n", humi/100);
-  if (ninedof_accel)  printf("Acceleration: X: %d Y: %d Z: %d\n", ninedof_accel_x, ninedof_accel_y, ninedof_accel_z);
-  if (ninedof_mag)    printf("Magnetometer: X: %d Y: %d Z: %d\n", ninedof_magneto_x, ninedof_magneto_y, ninedof_magneto_z);
-  if (ninedof_gyro)   printf("Gyro:         X: %d Y: %d Z: %d\n", ninedof_gyro_x, ninedof_gyro_y, ninedof_gyro_z);
-  if (proxReading)    printf("PROXIMITY:                   %u%%\n", proxReading);
+  if (isl29035)
+    printf("ISL29035:   Light Intensity: %d\n", light);
+  if (tmp006)
+    printf("TMP006:     Temperature:     %d\n", tmp006_temp);
+  if (tsl2561)
+    printf("TSL2561:    Light:           %d lux\n", tsl2561_lux);
+  if (lps25hb)
+    printf("LPS25HB:    Pressure:        %d\n", lps25hb_pressure);
+  if (temp)
+    printf("Temperature:                 %d deg C\n", temp / 100);
+  if (humi)
+    printf("Humidity:                    %u%%\n", humi / 100);
+  if (ninedof_accel)
+    printf("Acceleration: X: %d Y: %d Z: %d\n", ninedof_accel_x, ninedof_accel_y, ninedof_accel_z);
+  if (ninedof_mag)
+    printf("Magnetometer: X: %d Y: %d Z: %d\n", ninedof_magneto_x, ninedof_magneto_y, ninedof_magneto_z);
+  if (ninedof_gyro)
+    printf("Gyro:         X: %d Y: %d Z: %d\n", ninedof_gyro_x, ninedof_gyro_y, ninedof_gyro_z);
+  if (proxReading)
+    printf("PROXIMITY:                   %u%%\n", proxReading);
 
   /* *INDENT-ON* */
 
   printf("\n");
 }
 
-int main(void) {
+int main(void)
+{
   printf("[Sensors] Starting Sensors App.\n");
   printf("[Sensors] All available sensors on the platform will be sampled.\n");
 
-  isl29035    = driver_exists(DRIVER_NUM_AMBIENT_LIGHT);
-  tmp006      = driver_exists(DRIVER_NUM_TMP006);
-  tsl2561     = driver_exists(DRIVER_NUM_TSL2561);
-  lps25hb     = driver_exists(DRIVER_NUM_LPS25HB);
+  isl29035 = driver_exists(DRIVER_NUM_AMBIENT_LIGHT);
+  tmp006 = driver_exists(DRIVER_NUM_TMP006);
+  tsl2561 = driver_exists(DRIVER_NUM_TSL2561);
+  lps25hb = driver_exists(DRIVER_NUM_LPS25HB);
   temperature = driver_exists(DRIVER_NUM_TEMPERATURE);
-  humidity    = driver_exists(DRIVER_NUM_HUMIDITY);
-  ninedof     = driver_exists(DRIVER_NUM_NINEDOF);
-  proximity   = driver_exists(DRIVER_NUM_PROXIMITY);
+  humidity = driver_exists(DRIVER_NUM_HUMIDITY);
+  ninedof = driver_exists(DRIVER_NUM_NINEDOF);
+  proximity = driver_exists(DRIVER_NUM_PROXIMITY);
 
-  if (ninedof) {
+  if (ninedof)
+  {
     int buffer;
     ninedof_accel = (ninedof_read_acceleration_sync(&buffer, &buffer, &buffer) == TOCK_SUCCESS);
-    ninedof_mag   = (ninedof_read_magnetometer_sync(&buffer, &buffer, &buffer) == TOCK_SUCCESS);
-    ninedof_gyro  = (ninedof_read_gyroscope_sync(&buffer, &buffer, &buffer) == TOCK_SUCCESS);
+    ninedof_mag = (ninedof_read_magnetometer_sync(&buffer, &buffer, &buffer) == TOCK_SUCCESS);
+    ninedof_gyro = (ninedof_read_gyroscope_sync(&buffer, &buffer, &buffer) == TOCK_SUCCESS);
   }
 
   // Setup periodic timer to sample the sensors.
-  static tock_timer_t timer;
-  timer_every(1000, timer_fired, NULL, &timer);
+  //static tock_timer_t timer;
+  //timer_every(5000, timer_fired, NULL, &timer);
+
+  if (proximity)
+  {
+    printf("Driver does exist\n");
+  }
+  else
+  {
+    printf("Driver does not exist\n");
+    return -1;
+  }
+
+  proximity_set_interrupt_thresholds(0, 175);
+
+  unsigned reading = 0;
+  while (1)
+  {
+    proximity_read_on_interrupt_sync(&reading);
+    printf("Reading --> %u\n", reading);
+  }
 
   return 0;
 }
--- libtock/proximity.c
+++ libtock/proximity.c
@@ -1,88 +1,104 @@
 #include "proximity.h"
 #include "tock.h"
+#include <stdio.h>
 
-struct thresholds {
+struct thresholds
+{
     uint8_t lowerThresh;
     uint8_t higherThresh;
 };
 
-static struct thresholds threshes = { .lowerThresh = 0 , .higherThresh = 175 };
+static struct thresholds threshes = {.lowerThresh = 0, .higherThresh = 175};
 
-struct data {
+struct data
+{
     bool fired;
     int proximity;
 };
 
-static struct data result = { .fired = false };
+static struct data result = {.fired = false};
 
 // Internal callback for faking synchronous reads
 static void cb(int proximity,
-               __attribute__ ((unused)) int unused,
-               __attribute__ ((unused)) int unused1,
-               void* ud) {
-  struct data* data = (struct data*) ud;
-  data->proximity = proximity;
-  data->fired    = true;
+               __attribute__((unused)) int unused,
+               __attribute__((unused)) int unused1,
+               void *ud)
+{
+    struct data *data = (struct data *)ud;
+    data->proximity = proximity;
+    data->fired = true;
 }
 
-
-int proximity_set_callback(subscribe_cb callback , void* callback_args){
-    return subscribe(DRIVER_NUM_PROXIMITY , 0 , callback , callback_args);
+int proximity_set_callback(subscribe_cb callback, void *callback_args)
+{
+    return subscribe(DRIVER_NUM_PROXIMITY, 0, callback, callback_args);
 }
 
-int proximity_read(void){
-    return command(DRIVER_NUM_PROXIMITY , 1 , 0 , 0);
+int proximity_read(void)
+{
+    return command(DRIVER_NUM_PROXIMITY, 1, 0, 0);
 }
 
-int proximity_read_on_interrupt(uint8_t lower , uint8_t upper){
-    return command(DRIVER_NUM_PROXIMITY , 2 , lower , upper);
+int proximity_read_on_interrupt(uint8_t lower, uint8_t upper)
+{
+    return command(DRIVER_NUM_PROXIMITY, 2, lower, upper);
 }
 
-int proximity_set_interrupt_thresholds( uint8_t lower , uint8_t upper ){
+int proximity_set_interrupt_thresholds(uint8_t lower, uint8_t upper)
+{
     threshes.lowerThresh = lower;
     threshes.higherThresh = upper;
     return 0;
 }
 
-
-int proximity_read_sync(unsigned* proximity){
+int proximity_read_sync(unsigned *proximity)
+{
 
     int err;
     result.fired = false;
 
-    err = proximity_set_callback(cb , (void*) &result);
-    if (err < 0){ return err; }
+    err = proximity_set_callback(cb, (void *)&result);
+    if (err < 0)
+    {
+        return err;
+    }
 
     err = proximity_read();
-    if (err < 0){ return err; }
+    if (err < 0)
+    {
+        return err;
+    }
 
     yield_for(&result.fired);
 
     *proximity = result.proximity;
 
-
     return 0;
-
-
 }
 
-int proximity_read_on_interrupt_sync(unsigned* proximity){
+int proximity_read_on_interrupt_sync(unsigned *proximity)
+{
 
     int err;
     result.fired = false;
 
-    err = proximity_set_callback(cb , (void*) &result);
-    if (err < 0){ return err; }
+    err = proximity_set_callback(cb, (void *)&result);
+    if (err < 0)
+    {
+        return err;
+    }
 
-    err = proximity_read_on_interrupt(threshes.lowerThresh , threshes.higherThresh );
-    if (err < 0){ return err; }
+    err = proximity_read_on_interrupt(threshes.lowerThresh, threshes.higherThresh);
+    if (err < 0)
+    {
+        return err;
+    }
 
     yield_for(&result.fired);
 
     *proximity = result.proximity;
 
     return 0;
-
 }
 
 /*
@@ -128,5 +144,3 @@ int proximity_set_gain_sync(uint8_t gain){
 
 }
 */
-
-
