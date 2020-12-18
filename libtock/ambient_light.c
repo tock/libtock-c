d66dfcb4c8df39fd6ad9ad30bd7b71b895442fc7
Philip Levis <pal@cs.stanford.edu>
2020-12-18 08:26:28 -0800

    Revert "Updating ambient_light to 2.0 API."
    This reverts commit e6c2e133ab93ea187e561b11c4e124a2df08a2c2.

--- libtock/ambient_light.c
+++ libtock/ambient_light.c
@@ -21,12 +21,12 @@ int ambient_light_read_intensity_sync(int* lux_value) {
   result.fired = false;
 
   err = ambient_light_subscribe(ambient_light_cb, (void*)(&result));
-  if (err != TOCK_SUCCESS) {
+  if (err < TOCK_SUCCESS) {
     return err;
   }
 
   err = ambient_light_start_intensity_reading();
-  if (err != TOCK_SUCCESS) {
+  if (err < TOCK_SUCCESS) {
     return err;
   }
 
@@ -38,22 +38,10 @@ int ambient_light_read_intensity_sync(int* lux_value) {
 }
 
 int ambient_light_subscribe(subscribe_cb callback, void* userdata) {
-  subscribe_return_t sval = subscribe2(DRIVER_NUM_AMBIENT_LIGHT, 0, callback, userdata);
-  if (!sval.success) {
-    return tock_error_to_rcode(sval.error);
-  } else {
-    return TOCK_SUCCESS;
-  }
+  return subscribe(DRIVER_NUM_AMBIENT_LIGHT, 0, callback, userdata);
 }
 
 int ambient_light_start_intensity_reading(void) {
-  syscall_return_t rval = command2(DRIVER_NUM_AMBIENT_LIGHT, 1, 0, 0);
-  if (rval.type > TOCK_SYSCALL_SUCCESS) { // Wrong success tpe
-    return TOCK_FAIL;
-  } else if (rval.type < TOCK_SYSCALL_SUCCESS) {
-    return tock_error_to_rcode(rval.data[0]);
-  } else {
-    return TOCK_SUCCESS;
-  }
+  return command(DRIVER_NUM_AMBIENT_LIGHT, 1, 0, 0);
 }
 
