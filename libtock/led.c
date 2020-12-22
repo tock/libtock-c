6cfe0e8067aa644fcfc2dfa1d1688c1717458249
Alexandru Radovici <msg4alex@gmail.com>
2020-12-22 21:05:36 +0200

    update led drive to tock 2.0 api

--- libtock/led.c
+++ libtock/led.c
@@ -1,17 +1,37 @@
 #include "led.h"
 
 int led_count(void) {
-  return command(DRIVER_NUM_LEDS, 0, 0, 0);
+  syscall_return_t ret = command2(DRIVER_NUM_LEDS, 0, 0, 0);
+  if (ret.type == TOCK_SYSCALL_SUCCESS_U32) {
+    return ret.data[0];
+  } else {
+    return 0;
+  }
 }
 
 int led_on(int led_num) {
-  return command(DRIVER_NUM_LEDS, 1, led_num, 0);
+  syscall_return_t ret = command2(DRIVER_NUM_LEDS, 1, led_num, 0);
+  if (ret.type == TOCK_SYSCALL_SUCCESS) {
+    return TOCK_SUCCESS;
+  } else {
+    return ret.data[0];
+  }
 }
 
 int led_off(int led_num) {
-  return command(DRIVER_NUM_LEDS, 2, led_num, 0);
+  syscall_return_t ret = command2(DRIVER_NUM_LEDS, 2, led_num, 0);
+  if (ret.type == TOCK_SYSCALL_SUCCESS) {
+    return TOCK_SUCCESS;
+  } else {
+    return ret.data[0];
+  }
 }
 
 int led_toggle(int led_num) {
-  return command(DRIVER_NUM_LEDS, 3, led_num, 0);
+  syscall_return_t ret = command2(DRIVER_NUM_LEDS, 3, led_num, 0);
+  if (ret.type == TOCK_SYSCALL_SUCCESS) {
+    return TOCK_SUCCESS;
+  } else {
+    return ret.data[0];
+  }
 }
