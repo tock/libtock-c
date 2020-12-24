81a16f991c4c77747138be0c3e1680d6a1b67a81
Philip Levis <pal@cs.stanford.edu>
2020-12-23 20:38:58 -0800

    Porting libtock-c to new 2.0 syscall API.
    Replaces internal calls to relative and absolute alarms (command 4+5) in
    reference+dt calls (command 6). This fixes bugs when applications can hang
    because a very short alarm appears to the kernel as a very long one.
    
    This also includes porting the LED system calls to the 2.0 API; without
    this, the multi_alarm_test test application fails because it uses
    the number of LEDs to determine how many alarms to create. Without this
    fix it creates > 100 alarms and crashes immediately.

--- libtock/internal/alarm_internal.c
+++ libtock/internal/alarm_internal.c
@@ -1,17 +1,37 @@
 #include "internal/alarm.h"
 
 int alarm_internal_subscribe(subscribe_cb cb, void *userdata) {
-  return subscribe(DRIVER_NUM_ALARM, 0, cb, userdata);
+  subscribe_return_t sval = subscribe2(DRIVER_NUM_ALARM, 0, cb, userdata);
+  if (sval.success) {
+    return 0;
+  } else {
+    return tock_error_to_rcode(sval.error);
+  }
 }
 
-int alarm_internal_set(uint32_t tics) {
-  return command(DRIVER_NUM_ALARM, 4, (int)tics, 0);
+int alarm_internal_set(uint32_t reference, uint32_t tics) {
+  syscall_return_t rval = command2(DRIVER_NUM_ALARM, 6, reference, tics);
+  if (rval.type == TOCK_SYSCALL_SUCCESS) {
+    return 0;
+  } else {
+    return tock_error_to_rcode(rval.data[0]);
+  }
 }
 
 int alarm_internal_stop(void) {
-  return command(DRIVER_NUM_ALARM, 3, 0, 0);
+  syscall_return_t rval = command2(DRIVER_NUM_ALARM, 3, 0, 0);
+  if (rval.type == TOCK_SYSCALL_SUCCESS) {
+    return 0;
+  } else {
+    return tock_error_to_rcode(rval.data[0]);
+  }
 }
 
 unsigned int alarm_internal_frequency(void) {
-  return (unsigned int) command(DRIVER_NUM_ALARM, 1, 0, 0);
+  syscall_return_t rval = command2(DRIVER_NUM_ALARM, 1, 0, 0);
+  if (rval.type == TOCK_SYSCALL_SUCCESS_U32) {
+    return (unsigned int)rval.data[0];
+  } else {
+    return 0;
+  }
 }
--- libtock/led.c
+++ libtock/led.c
@@ -1,17 +1,37 @@
 #include "led.h"
 
 int led_count(void) {
-  return command(DRIVER_NUM_LEDS, 0, 0, 0);
+  syscall_return_t rval = command2(DRIVER_NUM_LEDS, 0, 0, 0);
+  if (rval.type == TOCK_SYSCALL_SUCCESS_U32) {
+    return rval.data[0];
+  } else {
+    return 0;
+  }
 }
 
 int led_on(int led_num) {
-  return command(DRIVER_NUM_LEDS, 1, led_num, 0);
+  syscall_return_t rval = command2(DRIVER_NUM_LEDS, 1, led_num, 0);
+  if (rval.type == TOCK_SYSCALL_SUCCESS) {
+    return 0;
+  } else {
+    return 1;
+  }
 }
 
 int led_off(int led_num) {
-  return command(DRIVER_NUM_LEDS, 2, led_num, 0);
+  syscall_return_t rval = command2(DRIVER_NUM_LEDS, 2, led_num, 0);
+  if (rval.type == TOCK_SYSCALL_SUCCESS) {
+    return 0;
+  } else {
+    return 1;
+  }
 }
 
 int led_toggle(int led_num) {
-  return command(DRIVER_NUM_LEDS, 3, led_num, 0);
+  syscall_return_t rval = command2(DRIVER_NUM_LEDS, 3, led_num, 0);
+  if (rval.type == TOCK_SYSCALL_SUCCESS) {
+    return 0;
+  } else {
+    return 1;
+  }
 }
