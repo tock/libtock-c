193a629ea56fed2608d5fa261ccc571b0cc70f1a
Alexandru Radovici <msg4alex@gmail.com>
2021-01-01 19:53:44 +0200

    port hd44780 driver to 2.0

--- libtock/hd44780.c
+++ libtock/hd44780.c
@@ -2,8 +2,6 @@
 #include "hd44780.h"
 #include "tock.h"
 
-#define ALLOW_BAD_VALUE 200
-
 /*
  * hd44780_start is the first function to be called and initializes the
  * functioning parameters and communication parameters of the LCD, according
@@ -15,10 +13,14 @@
  */
 int hd44780_begin(void)
 {
-  int ret = command(DRIVER_HD44780_NUM, 0, 16, 1);
-  if (ret)
-    return TOCK_EBUSY;
-  return command(DRIVER_HD44780_NUM, 0, 16, 2);
+  syscall_return_t ret = command2(DRIVER_HD44780_NUM, 0, 16, 1);
+  if (ret.type != TOCK_SYSCALL_SUCCESS)
+    return tock_error_to_rcode (ret.data[0]);
+  ret = command2(DRIVER_HD44780_NUM, 0, 16, 2);
+  if (ret.type != TOCK_SYSCALL_SUCCESS) {
+    return tock_error_to_rcode (ret.data[0]);
+  }
+  return TOCK_SUCCESS;
 }
 
 /* hd44780_clear clears the display and sets the cursor posiziton to zero
@@ -28,7 +30,10 @@ int hd44780_begin(void)
  */
 int hd44780_clear(void)
 {
-  return command(DRIVER_HD44780_NUM, 3, 0, 0);
+  syscall_return_t ret = command2(DRIVER_HD44780_NUM, 3, 0, 0);
+  if (ret.type != TOCK_SYSCALL_SUCCESS)
+    return tock_error_to_rcode (ret.data[0]);
+  return TOCK_SUCCESS;
 }
 
 /* hd44780_home, as hd44780_clear, clears the display and sets the cursor posiziton to zero
@@ -38,7 +43,10 @@ int hd44780_clear(void)
  */
 int hd44780_home(void)
 {
-  return command(DRIVER_HD44780_NUM, 2, 0, 0);
+  syscall_return_t ret = command2(DRIVER_HD44780_NUM, 2, 0, 0);
+  if (ret.type != TOCK_SYSCALL_SUCCESS)
+    return tock_error_to_rcode (ret.data[0]);
+  return TOCK_SUCCESS;
 }
 
 /* hd44780_no_display turns the display off very quickly
@@ -48,7 +56,10 @@ int hd44780_home(void)
  */
 int hd44780_no_display(void)
 {
-  return command(DRIVER_HD44780_NUM, 7, 1, 0);
+  syscall_return_t ret = command2(DRIVER_HD44780_NUM, 7, 1, 0);
+  if (ret.type != TOCK_SYSCALL_SUCCESS)
+    return tock_error_to_rcode (ret.data[0]);
+  return TOCK_SUCCESS;
 }
 
 /* hd44780_display turns the display on very quickly
@@ -58,7 +69,10 @@ int hd44780_no_display(void)
  */
 int hd44780_display(void)
 {
-  return command(DRIVER_HD44780_NUM, 7, 0, 0);
+  syscall_return_t ret = command2(DRIVER_HD44780_NUM, 7, 0, 0);
+  if (ret.type != TOCK_SYSCALL_SUCCESS)
+    return tock_error_to_rcode (ret.data[0]);
+  return TOCK_SUCCESS;
 }
 
 /* hd44780_no_blink turns off the blinking cursor display
@@ -68,7 +82,10 @@ int hd44780_display(void)
  */
 int hd44780_no_blink(void)
 {
-  return command(DRIVER_HD44780_NUM, 8, 1, 0);
+  syscall_return_t ret = command2(DRIVER_HD44780_NUM, 8, 1, 0);
+  if (ret.type != TOCK_SYSCALL_SUCCESS)
+    return tock_error_to_rcode (ret.data[0]);
+  return TOCK_SUCCESS;
 }
 
 /* hd44780_blink turns on the blinking cursor display
@@ -78,7 +95,10 @@ int hd44780_no_blink(void)
  */
 int hd44780_blink(void)
 {
-  return command(DRIVER_HD44780_NUM, 8, 0, 0);
+  syscall_return_t ret = command2(DRIVER_HD44780_NUM, 8, 0, 0);
+  if (ret.type != TOCK_SYSCALL_SUCCESS)
+    return tock_error_to_rcode (ret.data[0]);
+  return TOCK_SUCCESS;
 }
 
 /* hd44780_no_cursor turns off the underline cursor
@@ -88,7 +108,10 @@ int hd44780_blink(void)
  */
 int hd44780_no_cursor(void)
 {
-  return command(DRIVER_HD44780_NUM, 6, 1, 0);
+  syscall_return_t ret = command2(DRIVER_HD44780_NUM, 6, 1, 0);
+  if (ret.type != TOCK_SYSCALL_SUCCESS)
+    return tock_error_to_rcode (ret.data[0]);
+  return TOCK_SUCCESS;
 }
 
 /* hd44780_cursor turns on the underline cursor
@@ -98,7 +121,10 @@ int hd44780_no_cursor(void)
  */
 int hd44780_cursor(void)
 {
-  return command(DRIVER_HD44780_NUM, 6, 0, 0);
+  syscall_return_t ret = command2(DRIVER_HD44780_NUM, 6, 0, 0);
+  if (ret.type != TOCK_SYSCALL_SUCCESS)
+    return tock_error_to_rcode (ret.data[0]);
+  return TOCK_SUCCESS;
 }
 
 /* hd44780_scroll_display_left scrolls the display to the left without
@@ -109,7 +135,10 @@ int hd44780_cursor(void)
  */
 int hd44780_scroll_display_left(void)
 {
-  return command(DRIVER_HD44780_NUM, 9, 0, 0);
+  syscall_return_t ret = command2(DRIVER_HD44780_NUM, 9, 0, 0);
+  if (ret.type != TOCK_SYSCALL_SUCCESS)
+    return tock_error_to_rcode (ret.data[0]);
+  return TOCK_SUCCESS;
 }
 
 /* hd44780_scroll_display_right scrolls the display to the right without
@@ -120,7 +149,10 @@ int hd44780_scroll_display_left(void)
  */
 int hd44780_scroll_display_right(void)
 {
-  return command(DRIVER_HD44780_NUM, 9, 1, 0);
+  syscall_return_t ret = command2(DRIVER_HD44780_NUM, 9, 1, 0);
+  if (ret.type != TOCK_SYSCALL_SUCCESS)
+    return tock_error_to_rcode (ret.data[0]);
+  return TOCK_SUCCESS;
 }
 
 /* hd44780_left_to_right should flow the text from left to right on the display
@@ -130,7 +162,10 @@ int hd44780_scroll_display_right(void)
  */
 int hd44780_left_to_right(void)
 {
-  return command(DRIVER_HD44780_NUM, 4, 0, 0);
+  syscall_return_t ret = command2(DRIVER_HD44780_NUM, 4, 0, 0);
+  if (ret.type != TOCK_SYSCALL_SUCCESS)
+    return tock_error_to_rcode (ret.data[0]);
+  return TOCK_SUCCESS;
 }
 
 /* hd44780_right_to_left should flow the text from right to left on the display
@@ -140,7 +175,10 @@ int hd44780_left_to_right(void)
  */
 int hd44780_right_to_left(void)
 {
-  return command(DRIVER_HD44780_NUM, 4, 1, 0);
+  syscall_return_t ret = command2(DRIVER_HD44780_NUM, 4, 1, 0);
+  if (ret.type != TOCK_SYSCALL_SUCCESS)
+    return tock_error_to_rcode (ret.data[0]);
+  return TOCK_SUCCESS;
 }
 
 /* hd44780_autoscroll will 'right justify' the text from the cursor
@@ -150,7 +188,10 @@ int hd44780_right_to_left(void)
  */
 int hd44780_autoscroll(void)
 {
-  return command(DRIVER_HD44780_NUM, 5, 0, 0);
+  syscall_return_t ret = command2(DRIVER_HD44780_NUM, 5, 0, 0);
+  if (ret.type != TOCK_SYSCALL_SUCCESS)
+    return tock_error_to_rcode (ret.data[0]);
+  return TOCK_SUCCESS;
 }
 
 /* hd44780_no_autoscroll will 'left justify' the text from the cursor
@@ -160,13 +201,16 @@ int hd44780_autoscroll(void)
  */
 int hd44780_no_autoscroll(void)
 {
-  return command(DRIVER_HD44780_NUM, 5, 1, 0);
+  syscall_return_t ret = command2(DRIVER_HD44780_NUM, 5, 1, 0);
+  if (ret.type != TOCK_SYSCALL_SUCCESS)
+    return tock_error_to_rcode (ret.data[0]);
+  return TOCK_SUCCESS;
 }
 
 /* hd44780_print_full_string will do some checks for the number of bytes to
  * be sent and will try to send to the kernel all the bytes in the buffer.
  * When the syscall returns a certain value (ALLOW_BAD_VALUE) it means that the
- * command buffer in the kernel is full and a delay is needed in order for the
+ * command2 buffer in the kernel is full and a delay is needed in order for the
  * buffer to be emptied (the commands already saved to be actually executed).
  * As argument, there is the string to be printed.
  *
@@ -176,22 +220,14 @@ int hd44780_no_autoscroll(void)
 uint8_t hd44780_print_full_string(char* string)
 {
   char buff[128];
-  uint8_t len = strlen(string);
-  uint8_t n   = 0;
-  while (n < len) {
-    uint8_t to_write = ((uint8_t) sizeof(buff) > len - n) ? len - n : (uint8_t) sizeof(buff);
-    memcpy(buff, string, to_write);
-    int ret = allow(DRIVER_HD44780_NUM, 0, (void* )buff, to_write);
-    if (ret == ALLOW_BAD_VALUE) {
-      while (ret == ALLOW_BAD_VALUE) {
-        delay_ms(1000);
-        ret = allow(DRIVER_HD44780_NUM, 0, (void* )buff, to_write);
-      }
-    }
-    n      += ret;
-    string += ret;
+  uint8_t len      = strlen(string);
+  uint8_t to_write = ((uint8_t) sizeof(buff) > len) ? len : (uint8_t) sizeof(buff);
+  memcpy(buff, string, to_write);
+  allow_ro_return_t ret = allow_readonly(DRIVER_HD44780_NUM, 0, (void* )buff, to_write);
+  if (ret.success == 0) {
+    return tock_error_to_rcode(ret.error);
   }
-  return n;
+  return to_write;
 }
 
 /* hd44780_print_string will do some checks for the number of bytes to be sent
@@ -205,17 +241,14 @@ uint8_t hd44780_print_full_string(char* string)
 uint8_t hd44780_print_string(char* string)
 {
   char buff[128];
-  uint8_t len = strlen(string);
-  uint8_t n   = 0;
-  while (n < len) {
-    uint8_t to_write = ((uint8_t) sizeof(buff) > len - n) ? len - n : (uint8_t) sizeof(buff);
-    memcpy(buff, string, to_write);
-    int ret = allow(DRIVER_HD44780_NUM, 0, (void* )buff, to_write);
-    if (!ret || ret == ALLOW_BAD_VALUE) return n;
-    n      += ret;
-    string += ret;
+  uint8_t len      = strlen(string);
+  uint8_t to_write = ((uint8_t) sizeof(buff) > len) ? len : (uint8_t) sizeof(buff);
+  memcpy(buff, string, to_write);
+  allow_ro_return_t ret = allow_readonly(DRIVER_HD44780_NUM, 0, (void* )buff, to_write);
+  if (ret.success == 0) {
+    return tock_error_to_rcode(ret.error);
   }
-  return n;
+  return to_write;
 }
 
 /* hd44780_set_cursor will set the cursor position to the row and col given as
@@ -230,5 +263,8 @@ uint8_t hd44780_print_string(char* string)
 
 int hd44780_set_cursor(uint8_t col, uint8_t row)
 {
-  return command(DRIVER_HD44780_NUM, 1, col, row);
+  syscall_return_t ret = command2(DRIVER_HD44780_NUM, 1, col, row);
+  if (ret.type != TOCK_SYSCALL_SUCCESS)
+    return tock_error_to_rcode (ret.data[0]);
+  return TOCK_SUCCESS;
 }
