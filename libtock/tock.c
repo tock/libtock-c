e25be95e9740696547f1ec4355376625e40b86bb
Philip Levis <pal@cs.stanford.edu>
2021-01-03 12:49:47 -0800

    First implementation of 2.0 yield.

--- libtock/tock.c
+++ libtock/tock.c
@@ -98,6 +98,54 @@ void yield(void) {
   }
 }
 
+syscall_return_t yield2(void) {
+  if (task_cur != task_last) {
+    tock_task_t task = task_queue[task_cur];
+    task_cur = (task_cur + 1) % TASK_QUEUE_SIZE;
+    task.cb(task.arg0, task.arg1, task.arg2, task.ud);
+    syscall_return_t rval = {TOCK_SYSCALL_SUCCESS, {0, 0, 0}};
+    return rval;
+  } else {
+    // Note: A process stops yielding when there is a callback ready to run,
+    // which the kernel executes by modifying the stack frame pushed by the
+    // hardware. The kernel copies the PC value from the stack frame to the LR
+    // field, and sets the PC value to callback to run. When this frame is
+    // unstacked during the interrupt return, the effectively clobbers the LR
+    // register.
+    //
+    // At this point, the callback function is now executing, which may itself
+    // clobber any of the other caller-saved registers. Thus we mark this
+    // inline assembly as conservatively clobbering all caller-saved registers,
+    // forcing yield to save any live registers.
+    //
+    // Upon direct observation of this function, the LR is the only register
+    // that is live across the SVC invocation, however, if the yield call is
+    // inlined, it is possible that the LR won't be live at all (commonly seen
+    // for the `while (1) { yield(); }` idiom) or that other registers are
+    // live, thus it is important to let the compiler do the work here.
+    //
+    // According to the AAPCS: A subroutine must preserve the contents of the
+    // registers r4-r8, r10, r11 and SP (and r9 in PCS variants that designate
+    // r9 as v6) As our compilation flags mark r9 as the PIC base register, it
+    // does not need to be saved. Thus we must clobber r0-3, r12, and LR
+    register uint32_t wait asm ("r0") = 1; // yield-wait
+    register int rtype asm ("r0");
+    asm volatile (
+      "svc 0       \n"
+      : "=r" (rtype)
+      : "r" (wait)
+      : "memory", "r1", "r2", "r3", "r12", "lr"
+      );
+    if (rtype == TOCK_SYSCALL_SUCCESS) {
+      syscall_return_t rval = {rtype, {0, 0, 0}};
+      return rval;
+    } else {
+      syscall_return_t rval = {TOCK_SYSCALL_FAILURE, {TOCK_ERROR_FAIL, 0, 0}};
+      return rval;
+    }
+  }
+}
+
 int subscribe(uint32_t driver, uint32_t subscribe,
               subscribe_cb cb, void* userdata) {
   register uint32_t r0 asm ("r0") = driver;
