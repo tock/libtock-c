e9c13fe244ae8ae863f4a648356ee044e32b1449
Philip Levis <pal@cs.stanford.edu>
2021-01-27 12:48:10 -0800

    I2C master library and test app.

---
+++ libtock/i2c_master.c
@@ -0,0 +1,125 @@
+#include "i2c_master.h"
+
+#define DRIVER_NUM_I2CMASTER 0x20003
+
+#define TOCK_I2C_MASTER_CB       1
+#define TOCK_I2C_MASTER_BUF      1
+
+
+int i2c_master_set_callback (subscribe_cb callback, void* callback_args) {
+  subscribe_return_t subval = subscribe2(DRIVER_NUM_I2CMASTER, TOCK_I2C_MASTER_CB, callback, callback_args);
+  if (subval.success == 0) {
+    return tock_error_to_rcode(subval.error);
+  } else {
+    return TOCK_SUCCESS;
+  }
+}
+
+int i2c_master_set_buffer(uint8_t* buffer, uint32_t len) {
+  allow_rw_return_t aval = allow_readwrite(DRIVER_NUM_I2CMASTER, TOCK_I2C_MASTER_BUF, (void*) buffer, len);
+  if (aval.success == 0) {
+    return tock_error_to_rcode(aval.error);
+  } else {
+    return TOCK_SUCCESS;
+  }
+}
+
+int i2c_master_write(uint8_t address, uint8_t len) {
+  uint32_t a = (((uint32_t) len) << 16) | address;
+  syscall_return_t comval = command2(DRIVER_NUM_I2CMASTER, 1, a, len);
+  if (comval.type < TOCK_SYSCALL_SUCCESS) {
+    return tock_error_to_rcode(comval.data[0]);
+  } else {
+    return TOCK_SUCCESS;
+  }
+}
+
+int i2c_master_read(uint8_t address, uint8_t len) {
+  uint32_t a = (((uint32_t) len) << 16) | address;
+  syscall_return_t comval = command2(DRIVER_NUM_I2CMASTER, 2, a, len);
+  if (comval.type < TOCK_SYSCALL_SUCCESS) {
+    return tock_error_to_rcode(comval.data[0]);
+  } else {
+    return TOCK_SUCCESS;
+  }
+}
+int i2c_master_write_read(uint8_t address, uint8_t len) {
+  uint32_t a = (((uint32_t) len) << 16) | address;
+  syscall_return_t comval = command2(DRIVER_NUM_I2CMASTER, 3, a, len);
+  if (comval.type < TOCK_SYSCALL_SUCCESS) {
+    return tock_error_to_rcode(comval.data[0]);
+  } else {
+    return TOCK_SUCCESS;
+  }
+}
+
+static void i2c_callback(__attribute__ ((unused)) int a1,
+                         __attribute__ ((unused)) int a2,
+                         __attribute__ ((unused)) int unused,
+                         void* ud) {
+  (*(bool*)ud) = true;
+}
+
+int i2c_master_write_sync(uint16_t address, uint8_t* buffer, uint16_t len) {
+  bool ready = 0;
+  int rval   = i2c_master_set_buffer(buffer, len);
+  if (rval != TOCK_SUCCESS) {
+    return rval;
+  }
+
+  rval = i2c_master_set_callback(i2c_callback, &ready);
+  if (rval != TOCK_SUCCESS) {
+    return rval;
+  }
+
+  rval = i2c_master_write(address, len);
+  if (rval != TOCK_SUCCESS) {
+    return rval;
+  }
+
+  yield_for(&ready);
+  return TOCK_SUCCESS;
+}
+
+int i2c_master_read_sync(uint16_t address, uint8_t* buffer, uint16_t len) {
+  bool ready = 0;
+  int rval   = i2c_master_set_buffer(buffer, len);
+  if (rval != TOCK_SUCCESS) {
+    return rval;
+  }
+
+  rval = i2c_master_set_callback(i2c_callback, &ready);
+  if (rval != TOCK_SUCCESS) {
+    return rval;
+  }
+
+  rval = i2c_master_read(address, len);
+  if (rval != TOCK_SUCCESS) {
+    return rval;
+  }
+
+  yield_for(&ready);
+  return TOCK_SUCCESS;
+}
+
+int i2c_master_write_read_sync(uint16_t address, uint8_t* buffer, uint16_t len) {
+  bool ready = 0;
+  int rval   = i2c_master_set_buffer(buffer, len);
+  if (rval != TOCK_SUCCESS) {
+    return rval;
+  }
+
+  rval = i2c_master_set_callback(i2c_callback, &ready);
+  if (rval != TOCK_SUCCESS) {
+    return rval;
+  }
+
+  rval = i2c_master_write_read(address, len);
+  if (rval != TOCK_SUCCESS) {
+    return rval;
+  }
+
+  yield_for(&ready);
+  return TOCK_SUCCESS;
+}
+
