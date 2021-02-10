be82c085271840c60dce2e6c2d9471599506d2c6
Amit Aryeh Levy <amit@amitlevy.com>
2021-02-09 19:30:48 -0800

    Update ble interfaces for 2.0

--- libtock/ble.c
+++ libtock/ble.c
@@ -10,15 +10,25 @@
 #include <string.h>
 
 int ble_start_advertising(int pdu_type, uint8_t* advd, int len, uint16_t interval) {
-  int err = allow(BLE_DRIVER_NUMBER, 0, advd, len);
-  if (err < TOCK_SUCCESS)
-    return err;
+  allow_ro_return_t err = allow_readonly(BLE_DRIVER_NUMBER, BLE_CFG_ADV_BUF_ALLOWRO, advd, len);
+  if (!err.success)
+    return tock_error_to_rcode(err.error);
 
-  return command(BLE_DRIVER_NUMBER, BLE_ADV_START_CMD, pdu_type, interval);
+  syscall_return_t res = command2(BLE_DRIVER_NUMBER, BLE_ADV_START_CMD, pdu_type, interval);
+  if (res.type == TOCK_SYSCALL_SUCCESS) {
+    return TOCK_SUCCESS;
+  } else {
+    return tock_error_to_rcode(res.data[0]);
+  }
 }
 
 int ble_stop_advertising(void) {
-  return command(BLE_DRIVER_NUMBER, BLE_ADV_STOP_CMD, 1, 0);
+  syscall_return_t res = command2(BLE_DRIVER_NUMBER, BLE_ADV_STOP_CMD, 1, 0);
+  if (res.type == TOCK_SYSCALL_SUCCESS) {
+    return TOCK_SUCCESS;
+  } else {
+    return tock_error_to_rcode(res.data[0]);
+  }
 }
 
 int ble_start_passive_scan(uint8_t *data, uint8_t max_len,
@@ -26,25 +36,39 @@ int ble_start_passive_scan(uint8_t *data, uint8_t max_len,
   if (data == NULL || callback == NULL) {
     return TOCK_FAIL;
   } else {
-    int err;
 
-    err = subscribe(BLE_DRIVER_NUMBER, BLE_SCAN_SUB, callback, NULL);
-    if (err < TOCK_SUCCESS)
-      return err;
+    subscribe_return_t sub_err = subscribe2(BLE_DRIVER_NUMBER, BLE_SCAN_SUB, callback, NULL);
+    if (!sub_err.success)
+      return tock_error_to_rcode(sub_err.error);
 
-    err =
-      allow(BLE_DRIVER_NUMBER, BLE_CFG_SCAN_BUF_ALLOW, (void *)data, max_len);
-    if (err < TOCK_SUCCESS)
-      return err;
+    allow_rw_return_t allow_err =
+      allow_readwrite(BLE_DRIVER_NUMBER, BLE_CFG_SCAN_BUF_ALLOWRW, (void *)data, max_len);
+    if (!allow_err.success)
+      return tock_error_to_rcode(allow_err.error);
 
-    return command(BLE_DRIVER_NUMBER, BLE_SCAN_CMD, 1, 0);
+    syscall_return_t res = command2(BLE_DRIVER_NUMBER, BLE_SCAN_CMD, 1, 0);
+    if (res.type == TOCK_SYSCALL_SUCCESS) {
+      return TOCK_SUCCESS;
+    } else {
+      return tock_error_to_rcode(res.data[0]);
+    }
   }
 }
 
 int ble_stop_passive_scan(void) {
-  return command(BLE_DRIVER_NUMBER, BLE_ADV_STOP_CMD, 1, 0);
+  syscall_return_t res = command2(BLE_DRIVER_NUMBER, BLE_ADV_STOP_CMD, 1, 0);
+  if (res.type == TOCK_SYSCALL_SUCCESS) {
+    return TOCK_SUCCESS;
+  } else {
+    return tock_error_to_rcode(res.data[0]);
+  }
 }
 
 int ble_set_tx_power(TxPower_t power_level) {
-  return command(BLE_DRIVER_NUMBER, BLE_CFG_TX_POWER_CMD, power_level, 0);
+  syscall_return_t res = command2(BLE_DRIVER_NUMBER, BLE_CFG_TX_POWER_CMD, power_level, 0);
+  if (res.type == TOCK_SYSCALL_SUCCESS) {
+    return TOCK_SUCCESS;
+  } else {
+    return tock_error_to_rcode(res.data[0]);
+  }
 }
