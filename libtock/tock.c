tock/libtock-c

commit 7d91971d303564d9d8e0a8b7dce84cf0df7a99be

RISC-V support for yield-no-wait.

libtock/tock.c
@@ -61,6 +61,7 @@ void yield_for(bool *cond) {
 
 #if defined(__thumb__)
 
+
 void yield(void) {
   if (task_cur != task_last) {
     tock_task_t task = task_queue[task_cur];
@@ -140,18 +141,6 @@ int yield_no_wait(void) {
       );
     return (int)result;
   }
-}
-
-void yield_no_return(void) {
-  // Because yield-no-return doesn't return, we don't need to worry
-  // about clobbering.
-  register uint32_t wait asm ("r0") = 2; // yield-no-wait
-  asm volatile (
-    "svc 0       \n"
-    :
-    : "r" (wait)
-    : "memory"
-    );
 }
 
 int subscribe(uint32_t driver, uint32_t subscribe,
@@ -333,15 +322,38 @@ void yield(void) {
     task_cur = (task_cur + 1) % TASK_QUEUE_SIZE;
     task.cb(task.arg0, task.arg1, task.arg2, task.ud);
   } else {
+    register uint32_t a1  asm ("a1") = 1; // yield-wait
     asm volatile (
-      "li    a4, 0\n"
+      "li    a5, 0\n"
       "ecall\n"
       :
-      :
+      : "r" (a1)
       : "memory", "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7",
       "t0", "t1", "t2", "t3", "t4", "t5", "t6", "ra"
       );
 
+  }
+}
+
+
+int yield_no_wait(void) {
+  if (task_cur != task_last) {
+    tock_task_t task = task_queue[task_cur];
+    task_cur = (task_cur + 1) % TASK_QUEUE_SIZE;
+    task.cb(task.arg0, task.arg1, task.arg2, task.ud);
+  } else {
+    uint8_t result = 0;
+    register uint32_t a1  asm ("a1") = 0; // yield-no-wait
+    register uint8_t* a2  asm ("a2") = &result;
+    asm volatile (
+      "li    a5, 0\n"
+      "ecall\n"
+      :
+      : "r" (a1), "r" (a2)
+      : "memory", "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7",
+      "t0", "t1", "t2", "t3", "t4", "t5", "t6", "ra"
+      );
+    return (int)result;
   }
 }
 

commit 4e1391ec7001478720b26b6f977031c6f52cfbfa

rv32i: fix yield system call class argument register
On RISC-V 32bit platforms, the system call class identifier for
yield (0) must be passed in the 5th argument register, that is a4 or
x14 respectively. Passing it in the 6th argument register (a5) causes
undefined behavior.

This bug was hard to trace down, because the system call tracing
output was misleading, putting the blame on other parts of the
codebase. The LiteX simualtor (Verilator) RISC-V formal interface,
together with the Ibex instruction traces helped to hunt this one
down.

Fixes: 7d91971d303564 ("RISC-V support for yield-no-wait.")
Signed-off-by: Leon Schuermann <leon@is.currently.online>

libtock/tock.c
@@ -308,7 +308,7 @@ void yield(void) {
     register uint32_t a0  __asm__ ("a0")        = 1; // yield-wait
     register uint32_t wait_field __asm__ ("a1") = 0; // yield result ptr
     __asm__ volatile (
-      "li    a5, 0\n"
+      "li    a4, 0\n"
       "ecall\n"
       :
       : "r" (a0), "r" (wait_field)
@@ -327,7 +327,7 @@ int yield_no_wait(void) {
     register uint32_t a0  __asm__ ("a0") = 0; // yield-no-wait
     register uint8_t* a1  __asm__ ("a1") = &result;
     __asm__ volatile (
-      "li    a5, 0\n"
+      "li    a4, 0\n"
       "ecall\n"
       :
       : "r" (a0), "r" (a1)