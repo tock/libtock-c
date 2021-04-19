d5fd2011fb6862153ab21535628d06748469a9c6
Brad Campbell <bradjc5@gmail.com>
2021-04-19 16:24:38 -0400

    test: bare bones: support risc-v

--- examples/tests/bare_bones/main.c
+++ examples/tests/bare_bones/main.c
@@ -8,10 +8,22 @@ void _start(void* app_start __attribute__((unused)),
             void* memory_len __attribute__((unused)),
             void* app_heap_break __attribute__((unused))) {
   // Yield.
+#if defined(__thumb__)
   __asm__ volatile (
       "svc 0       \n"
       :
       :
       : "memory", "r0", "r1", "r2", "r3", "r12", "lr"
       );
+
+#elif defined(__riscv)
+  __asm__ volatile (
+      "li    a0, 0\n"
+      "ecall\n"
+      :
+      :
+      : "memory", "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7",
+      "t0", "t1", "t2", "t3", "t4", "t5", "t6", "ra"
+      );
+#endif
 }
