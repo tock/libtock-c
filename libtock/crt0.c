2a34097e25ecf3b4c8253927fa588e795d4aea33
Pat Pannuto <pat.pannuto@gmail.com>
2021-08-27 13:26:26 -0700

    crt0: explicitly 0 unused argc/argv

--- libtock/crt0.c
+++ libtock/crt0.c
@@ -13,7 +13,7 @@
 #error Fixed STACK_SIZE.
 #endif
 
-extern int main(void);
+extern int main(int argc, char *argv[]);
 
 // Allow _start to go undeclared
 #pragma GCC diagnostic ignored "-Wmissing-declarations"
@@ -328,7 +328,7 @@ void _c_start_pic(uint32_t app_start, uint32_t mem_start) {
     }
   }
 
-  exit(main());
+  exit(main(0, NULL));
 }
 
 // C startup routine for apps compiled with fixed addresses (i.e. no PIC).
@@ -362,5 +362,5 @@ void _c_start_nopic(uint32_t app_start, uint32_t mem_start) {
   char* bss_start = (char*)(myhdr->bss_start + mem_start);
   memset(bss_start, 0, myhdr->bss_size);
 
-  exit(main());
+  exit(main(0, NULL));
 }
