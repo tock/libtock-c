170769f8a237716ef9582a0696fb58ffa6bdf487
HOSEOK LEE <embedevlee@gmail.com>
2022-07-23 02:15:50 -0400

    app: update ota app example
    1) button version ota app and uart version ota app are integrated. For implementation test, it is convenient to use button version ota app.
    2) button version ota app is deactivated by default.
    3) At final stage, button version will be deleted.
    4) Increased the stack size of ota app
    5) This app work well for demo

--- libtock/internal/ota.c
+++ libtock/internal/ota.c
@@ -4,90 +4,166 @@
 #include "tock.h"
 #include "internal/ota.h"
 
-// Internal callback for synchronous interfaces
-// Check ota driver exist. This dirver provide interface to access the NVM(Flash Memory) in order to write data from an app.
-int ota_driver_check(void)
+/***************************** Interface of nonvolatile_storage_driver.rs (capsule) *****************************/
+// Check the nonvolateil driver existance
+int ota_nonvol_driver_check(void)
 {
   syscall_return_t res = command(DRIVER_NUM_NONVOLATILE_STORAGE_DRIVER, COMMAND_DRIVER_CHECK, 0, 0);
   return tock_command_return_novalue_to_returncode(res);
 }
 
-// Check how many bytes are in userspace(not kernel space)
-int ota_userspace_size_check(int *userspace_size_bytes)
+// Check the size of flash memory allocated to userspace(not kernel space)
+int ota_nonvol_userspace_size_check(int *userspace_size_bytes)
 {
   syscall_return_t res = command(DRIVER_NUM_NONVOLATILE_STORAGE_DRIVER, COMMAND_USERSPACE_SIZE_CHECK, 0, 0);
   return tock_command_return_u32_to_returncode(res, (uint32_t *)userspace_size_bytes);
 }
 
-// Provide grant for kernel
-int ota_internal_read_buffer(uint8_t *buffer, uint32_t len)
+// Set sharable read buffer with nonvolatile_storage_driver.rs
+int ota_flash_internal_read_buffer(uint8_t *buffer, uint32_t len)
 {
-  allow_rw_return_t aval = allow_readwrite(DRIVER_NUM_NONVOLATILE_STORAGE_DRIVER, OTA_APP_ID, (void *)buffer, len);
+  allow_rw_return_t aval = allow_readwrite(DRIVER_NUM_NONVOLATILE_STORAGE_DRIVER, IDs_FOR_NONVOL, (void *)buffer, len);
   return tock_allow_rw_return_to_returncode(aval);
 }
-// Provide grant for kernel
-int ota_internal_write_buffer(uint8_t *buffer, uint32_t len)
+
+// Set sharable write buffer with nonvolatile_storage_driver.rs
+int ota_flash_internal_write_buffer(uint8_t *buffer, uint32_t len)
 {
-  allow_ro_return_t aval = allow_readonly(DRIVER_NUM_NONVOLATILE_STORAGE_DRIVER, OTA_APP_ID, (void *)buffer, len);
+  allow_ro_return_t aval = allow_readonly(DRIVER_NUM_NONVOLATILE_STORAGE_DRIVER, IDs_FOR_NONVOL, (void *)buffer, len);
   return tock_allow_ro_return_to_returncode(aval);
 }
 
-// call back setting for read
-int ota_read_done_subscribe(subscribe_upcall cb, void *userdata)
+// Entitle callback function (when read action is done)
+int ota_flash_read_done_subscribe(subscribe_upcall cb, void *userdata)
 {
   subscribe_return_t sval = subscribe(DRIVER_NUM_NONVOLATILE_STORAGE_DRIVER, OTA_READ_DONE_CALLBACK_NUM, cb, userdata);
   return tock_subscribe_return_to_returncode(sval);
 }
 
-// call back setting for write
-int ota_write_done_subscribe(subscribe_upcall cb, void *userdata)
+// Entitle callback function (when write action is done)
+int ota_flash_write_done_subscribe(subscribe_upcall cb, void *userdata)
 {
   subscribe_return_t sval = subscribe(DRIVER_NUM_NONVOLATILE_STORAGE_DRIVER, OTA_WRITE_DONE_CALLBACK_NUM, cb, userdata);
   return tock_subscribe_return_to_returncode(sval);
 }
 
-// For direct command for data read
+// Command to let the kernel read data from flash
 int ota_data_read_execution(uint32_t offset, uint32_t length)
 {
-  syscall_return_t res = command(DRIVER_NUM_NONVOLATILE_STORAGE_DRIVER, COMMAND_READ_DATA, (int)offset, (int)length);
+  syscall_return_t res = command(DRIVER_NUM_NONVOLATILE_STORAGE_DRIVER, COMMAND_READ_DATA, (uint32_t)offset, (uint32_t)length);
   return tock_command_return_novalue_to_returncode(res);
 }
-// For direct command for data write
+// Command to let the kernel write data into flash
 int ota_data_write_execution(uint32_t offset, uint32_t length)
 {
-  syscall_return_t res = command(DRIVER_NUM_NONVOLATILE_STORAGE_DRIVER, COMMAND_WRITE_DATA, (int)offset, (int)length);
+  syscall_return_t res = command(DRIVER_NUM_NONVOLATILE_STORAGE_DRIVER, COMMAND_WRITE_DATA, (uint32_t)offset, (uint32_t)length);
   return tock_command_return_novalue_to_returncode(res);
 }
 
-// PROCLOADER KERNEL
-// For direct command for data write
+/***************************** Interface of process_load_utilities.rs (kernel) *****************************/
+// Commnad to let the kernel load a flashed app
 int ota_app_loading_request(void)
 {
   syscall_return_t res = command(DRIVER_NUM_PROCLOADER_KERNEL, COMMAND_PROC_APP_LOAD_REQ, 0, 0);
   return tock_command_return_novalue_to_returncode(res);
 }
 
-// For direct command for data write
-int ota_set_start_address_of_writable_flash(void)
+// Commnad to let the kernel set the dynamically changed writable flash start address
+int ota_set_dynamic_start_address_of_writable_flash(void)
 {
-  syscall_return_t res = command(DRIVER_NUM_PROCLOADER_KERNEL, COMMAND_PROC_SET_START_ADDRESS, 0, 0);
+  syscall_return_t res = command(DRIVER_NUM_PROCLOADER_KERNEL, COMMAND_PROC_SET_DYNAMIC_START_ADDRESS, 0, 0);
   return tock_command_return_novalue_to_returncode(res);
 }
 
-int ota_get_offset_of_start_address_of_writable_flash(uint32_t *startaddress)
+// Commnad to get the dynamically changed writable flash start address
+int ota_get_dynamic_start_address_of_writable_flash(uint32_t *startaddress)
 {
-  syscall_return_t res = command(DRIVER_NUM_PROCLOADER_KERNEL, COMMAND_PROC_GET_START_ADDRESS, 0, 0);
+  syscall_return_t res = command(DRIVER_NUM_PROCLOADER_KERNEL, COMMAND_PROC_GET_DYNAMIC_START_ADDRESS, 0, 0);
   return tock_command_return_u32_to_returncode(res, (uint32_t *)startaddress);
 }
 
-int ota_get_the_number_of_processes(uint32_t *processes_num)
+// Commnad to get FLASH end address
+int ota_get_end_address_of_writable_flash(uint32_t *endaddress)
 {
-  syscall_return_t res = command(DRIVER_NUM_PROCLOADER_KERNEL, COMMAND_PROC_GET_PROCESS_NUMS, 0, 0);
-  return tock_command_return_u32_to_returncode(res, (uint32_t *)processes_num);
+  syscall_return_t res = command(DRIVER_NUM_PROCLOADER_KERNEL, COMMAND_PROC_GET_FLASH_END_ADDRESS, 0, 0);
+  return tock_command_return_u32_to_returncode(res, (uint32_t *)endaddress);
 }
 
+// Commnad to get index (PROCESSES array at main.rs) for kernel to write the entry point of a flashed app
+int ota_get_process_index(uint32_t *processes_index)
+{
+  syscall_return_t res = command(DRIVER_NUM_PROCLOADER_KERNEL, COMMAND_PROC_GET_PROCESS_INDEX, 0, 0);
+  return tock_command_return_u32_to_returncode(res, (uint32_t *)processes_index);
+}
+
+// Commnad to get the maximum number of process that kernel can handle
+int ota_get_process_maxnum(uint32_t *processes_maxnum)
+{
+  syscall_return_t res = command(DRIVER_NUM_PROCLOADER_KERNEL, COMMAND_PROC_GET_PROCESS_MAX, 0, 0);
+  return tock_command_return_u32_to_returncode(res, (uint32_t *)processes_maxnum);
+}
+
+// Commnad to let the kernel calculate CRC32-POSIX and to return the result
 int ota_calculate_crc32(uint32_t *crc32_result)
 {
   syscall_return_t res = command(DRIVER_NUM_PROCLOADER_KERNEL, COMMAND_PROC_CAL_GET_CRC32, 0, 0);
   return tock_command_return_u32_to_returncode(res, (uint32_t *)crc32_result);
+}
+
+// Commnad to let the kernel use the end address of sram that OTA_app uses as the start address fo unused sram
+int ota_set_unused_sram_start_address_init_val(void)
+{
+  syscall_return_t res = command(DRIVER_NUM_PROCLOADER_KERNEL, COMMAND_PROC_SET_SRAM_START_ADDRESS, 0, 0);
+  return tock_command_return_novalue_to_returncode(res);
+}
+
+// Commnad to get the start address of flash memory region containing app images
+int ota_get_flash_start_address(uint32_t *rom_start_address)
+{
+  syscall_return_t res = command(DRIVER_NUM_PROCLOADER_KERNEL, COMMAND_PROC_GET_FLASH_START_ADDRESS, 0, 0);
+  return tock_command_return_u32_to_returncode(res, (uint32_t *)rom_start_address);
+}
+
+/***************************** Interface of uart console.rs (capsule) *****************************/
+// Set sharable read buffer with console.rs
+int ota_uart_internal_read_buffer(uint8_t *buffer, uint32_t len)
+{
+  allow_rw_return_t aval = allow_readwrite(DRIVER_NUM_CONSOLE, IDs_FOR_CONSOLE, (void *)buffer, len);
+  return tock_allow_rw_return_to_returncode(aval);
+}
+
+// Entitle callback function (when read action is done)
+int ota_uart_read_done_subscribe(subscribe_upcall cb, void *userdata)
+{
+  subscribe_return_t sval = subscribe(DRIVER_NUM_CONSOLE, COMMAND_CONSOLE_GET_STR, cb, userdata);
+  return tock_subscribe_return_to_returncode(sval);
+}
+
+// Commnad to specify when callback function (by ota_uart_read_done_subscribe) have to be triggered
+int ota_uart_data_read_cb_entry(uint32_t length)
+{
+  syscall_return_t res = command(DRIVER_NUM_CONSOLE, COMMAND_CONSOLE_GET_STR, length, 0);
+  return tock_command_return_novalue_to_returncode(res);
+}
+
+// Commnad to send data via UART
+int ota_putnstr_async(const char *str, size_t len, subscribe_upcall cb, void *userdata)
+{
+#pragma GCC diagnostic push
+#pragma GCC diagnostic pop
+
+  allow_ro_return_t ro = allow_readonly(DRIVER_NUM_CONSOLE, COMMAND_CONSOLE_PUT_STR, str, len);
+  if (!ro.success)
+  {
+    return tock_status_to_returncode(ro.status);
+  }
+
+  subscribe_return_t sub = subscribe(DRIVER_NUM_CONSOLE, COMMAND_CONSOLE_PUT_STR, cb, userdata);
+  if (!sub.success)
+  {
+    return tock_status_to_returncode(sub.status);
+  }
+
+  syscall_return_t com = command(DRIVER_NUM_CONSOLE, COMMAND_CONSOLE_PUT_STR, len, 0);
+  return tock_command_return_novalue_to_returncode(com);
 }
--- libtock/internal/ota.h
+++ libtock/internal/ota.h
@@ -7,42 +7,63 @@ extern "C"
 
 #include "tock.h"
 
+/* Driver number for system call */
 #define DRIVER_NUM_NONVOLATILE_STORAGE_DRIVER 0x50001
 #define DRIVER_NUM_PROCLOADER_KERNEL 0x10001
+#define DRIVER_NUM_CONSOLE 0x1
 
-#define OTA_APP_ID 0
+/* COMMAND SET OF NONVOLATILE_STORAGE_DRIVER */
+#define IDs_FOR_NONVOL 0
 
 #define OTA_READ_DONE_CALLBACK_NUM 0
 #define OTA_WRITE_DONE_CALLBACK_NUM 1
-
-#define OTA_READ_OFFSET_DONE_CALLBACK_NUM 0
 
 #define COMMAND_DRIVER_CHECK 0
 #define COMMAND_USERSPACE_SIZE_CHECK 1
 #define COMMAND_READ_DATA 2
 #define COMMAND_WRITE_DATA 3
 
+/* COMMAND SET OF PROCESS_LOAD_UTILITIES */
 #define COMMAND_PROC_APP_LOAD_REQ 1
-#define COMMAND_PROC_SET_START_ADDRESS 2
-#define COMMAND_PROC_GET_START_ADDRESS 3
-#define COMMAND_PROC_GET_PROCESS_NUMS 4
+#define COMMAND_PROC_SET_DYNAMIC_START_ADDRESS 2
+#define COMMAND_PROC_GET_DYNAMIC_START_ADDRESS 3
+#define COMMAND_PROC_SET_SRAM_START_ADDRESS 4
 #define COMMAND_PROC_CAL_GET_CRC32 5
+#define COMMAND_PROC_GET_PROCESS_MAX 6
+#define COMMAND_PROC_GET_FLASH_END_ADDRESS 7
+#define COMMAND_PROC_GET_PROCESS_INDEX 8
+#define COMMAND_PROC_GET_FLASH_START_ADDRESS 9
 
-#define COMMAND_APP_SCANNING 1
+/* COMMAND SET OF CONSOLE */
+#define IDs_FOR_CONSOLE 1
+#define COMMAND_CONSOLE_PUT_STR 1
+#define COMMAND_CONSOLE_GET_STR 2
 
-    int ota_driver_check(void);
-    int ota_userspace_size_check(int *userspace_size_bytes);
-    int ota_internal_read_buffer(uint8_t *buffer, uint32_t len);
-    int ota_internal_write_buffer(uint8_t *buffer, uint32_t len);
-    int ota_read_done_subscribe(subscribe_upcall cb, void *userdata);
-    int ota_write_done_subscribe(subscribe_upcall cb, void *userdata);
+    int ota_nonvol_driver_check(void);
+    int ota_nonvol_userspace_size_check(int *userspace_size_bytes);
+
+    int ota_flash_internal_read_buffer(uint8_t *buffer, uint32_t len);
+    int ota_flash_internal_write_buffer(uint8_t *buffer, uint32_t len);
+    int ota_flash_read_done_subscribe(subscribe_upcall cb, void *userdata);
+    int ota_flash_write_done_subscribe(subscribe_upcall cb, void *userdata);
     int ota_data_read_execution(uint32_t offset, uint32_t length);
     int ota_data_write_execution(uint32_t offset, uint32_t length);
+
     int ota_app_loading_request(void);
-    int ota_set_start_address_of_writable_flash(void);
-    int ota_get_offset_of_start_address_of_writable_flash(uint32_t *startaddress);
-    int ota_get_the_number_of_processes(uint32_t *processes_num);
+    int ota_set_dynamic_start_address_of_writable_flash(void);
+    int ota_get_dynamic_start_address_of_writable_flash(uint32_t *startaddress);
+    int ota_get_end_address_of_writable_flash(uint32_t *endaddress);
+    int ota_get_process_index(uint32_t *processes_num);
+    int ota_get_process_maxnum(uint32_t *processes_maxnum);
     int ota_calculate_crc32(uint32_t *crc32_result);
+    int ota_set_unused_sram_start_address_init_val(void);
+    int ota_get_flash_start_address(uint32_t *rom_start_address);
+
+    int ota_uart_internal_read_buffer(uint8_t *buffer, uint32_t len);
+    int ota_uart_read_done_subscribe(subscribe_upcall cb, void *userdata);
+    int ota_uart_data_read_cb_entry(uint32_t length);
+
+    int ota_putnstr_async(const char *str, size_t len, subscribe_upcall cb, void *userdata);
 
 #ifdef __cplusplus
 }
