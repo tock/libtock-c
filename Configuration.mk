3d78357db854fbc50a8b4a09a8e093130dd48f39
Brad Campbell <bradjc5@gmail.com>
2023-07-24 15:29:21 -0400

    riscv: need more header space
    Since we now also include a program header, we need more space for
    headers in TBFs. We create this space by moving the fixed address of the
    app binary further down in flash.

--- Configuration.mk
+++ Configuration.mk
@@ -74,22 +74,22 @@ OPENTITAN_TOCK_TARGETS := rv32imc|rv32imc.0x20030080.0x10005000|0x20030080|0x100
                           rv32imc|rv32imc.0x20034080.0x10008000|0x20034080|0x10008000
 
 # Specific addresses useful for the ARTY-E21 FPGA softcore hardware memory map.
-ARTY_E21_TOCK_TARGETS := rv32imac|rv32imac.0x40430060.0x80004000|0x40430060|0x80004000\
-                         rv32imac|rv32imac.0x40440060.0x80007000|0x40440060|0x80007000
+ARTY_E21_TOCK_TARGETS := rv32imac|rv32imac.0x40430080.0x80004000|0x40430080|0x80004000\
+                         rv32imac|rv32imac.0x40440080.0x80007000|0x40440080|0x80007000
 
 # Include the RISC-V targets.
-#  rv32imac|rv32imac.0x20040060.0x80002800 # RISC-V for HiFive1b
-#  rv32imac|rv32imac.0x403B0060.0x3FCC0000 # RISC-V for ESP32-C3
-#  rv32imc|rv32imc.0x41000060.0x42008000   # RISC-V for LiteX Arty-A7
-#  rv32imc|rv32imc.0x00080060.0x40008000   # RISC-V for LiteX Simulator
+#  rv32imac|rv32imac.0x20040080.0x80002800 # RISC-V for HiFive1b
+#  rv32imac|rv32imac.0x403B0080.0x3FCC0000 # RISC-V for ESP32-C3
+#  rv32imc|rv32imc.0x41000080.0x42008000   # RISC-V for LiteX Arty-A7
+#  rv32imc|rv32imc.0x00080080.0x40008000   # RISC-V for LiteX Simulator
 TOCK_TARGETS ?= cortex-m0\
                 cortex-m3\
                 cortex-m4\
                 cortex-m7\
-                rv32imac|rv32imac.0x20040060.0x80002800|0x20040060|0x80002800\
-                rv32imac|rv32imac.0x403B0060.0x3FCC0000|0x403B0060|0x3FCC0000\
-                rv32imc|rv32imc.0x41000060.0x42008000|0x41000060|0x42008000\
-                rv32imc|rv32imc.0x00080060.0x40008000|0x00080060|0x40008000\
+                rv32imac|rv32imac.0x20040080.0x80002800|0x20040080|0x80002800\
+                rv32imac|rv32imac.0x403B0080.0x3FCC0000|0x403B0080|0x3FCC0000\
+                rv32imc|rv32imc.0x41000080.0x42008000|0x41000080|0x42008000\
+                rv32imc|rv32imc.0x00080080.0x40008000|0x00080080|0x40008000\
                 $(OPENTITAN_TOCK_TARGETS) \
                 $(ARTY_E21_TOCK_TARGETS)
 endif
