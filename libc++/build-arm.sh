c4bb4475b990f3654f49f20f2257f189a415e6a1
Brad Campbell <bradjc5@gmail.com>
2023-09-12 22:27:45 -0400

    libc++: update makefile and build script
    For ARM

--- libc++/build-arm.sh
+++ libc++/build-arm.sh
@@ -15,8 +15,8 @@ if [[ ! -e $NEWLIB_INCLUDE_PATH ]]; then
   exit 1
 fi
 
-export CFLAGS_FOR_TARGET="-g -Os -ffunction-sections -fdata-sections -fPIC -msingle-pic-base -mno-pic-data-is-text-relative -mthumb -mcpu=cortex-m0 -isystem $NEWLIB_INCLUDE_PATH"
-export CXXFLAGS_FOR_TARGET="-g -Os -ffunction-sections -fdata-sections -fPIC -msingle-pic-base -mno-pic-data-is-text-relative -mthumb -mcpu=cortex-m0 -isystem $NEWLIB_INCLUDE_PATH"
+export CFLAGS_FOR_TARGET="-g -Os -ffunction-sections -fdata-sections -fPIC -msingle-pic-base -mno-pic-data-is-text-relative -mthumb -isystem $NEWLIB_INCLUDE_PATH"
+export CXXFLAGS_FOR_TARGET="-g -Os -ffunction-sections -fdata-sections -fPIC -msingle-pic-base -mno-pic-data-is-text-relative -mthumb -isystem $NEWLIB_INCLUDE_PATH"
 
 if gcc --version | grep -q clang; then
   echo "$(tput bold)System gcc is clang. Overriding with gcc-13"
@@ -35,13 +35,41 @@ else
   extra_with=""
 fi
 
+# Create our own build file for multilib compilation. This is selected with the
+# `--with-multilib-list=@t-tock` flag. Due to how the configure process works,
+# the file has to be in the gcc/config/arm folder.
+cat << EOF > $GCC_SRC_DIR/gcc/config/arm/t-tock
+For Tock!!
+
+MULTILIB_OPTIONS     =
+MULTILIB_DIRNAMES    =
+MULTILIB_EXCEPTIONS  =
+MULTILIB_MATCHES     =
+MULTILIB_REUSE       =
+MULTILIB_REQUIRED    =
+
+# Each entry in MULTILIB_OPTIONS serves as a level in the build hierarchy.
+MULTILIB_OPTIONS  += mthumb
+MULTILIB_DIRNAMES += thumb
+
+MULTILIB_OPTIONS  += march=armv6s-m/march=armv7-m/march=armv7e-m
+MULTILIB_DIRNAMES += v6-m v7-m v7e-m
+
+MULTILIB_OPTIONS  += mfloat-abi=soft
+MULTILIB_DIRNAMES += nofp
+
+# The actual targets we want to build.
+MULTILIB_REQUIRED += mthumb/march=armv6s-m/mfloat-abi=soft
+MULTILIB_REQUIRED += mthumb/march=armv7-m/mfloat-abi=soft
+MULTILIB_REQUIRED += mthumb/march=armv7e-m/mfloat-abi=soft
+EOF
+
+
 $GCC_SRC_DIR/configure \
   --build=x86_64-linux-gnu \
   --host=x86_64-linux-gnu \
   --target=arm-none-eabi \
-  --with-cpu=cortex-m0 \
-  --with-mode=thumb \
-  --disable-fpu \
+  --with-multilib-list=@t-tock \
   --with-newlib $extra_with \
   --with-headers=$NEWLIB_INCLUDE_PATH \
   --enable-languages="c c++" \
