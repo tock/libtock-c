42ab45bf9bad2485c809089913a724bd08920667
Brad Campbell <bradjc5@gmail.com>
2023-09-20 15:14:29 -0400

    newlib: arm: check for proper multilib support

--- newlib/build-arm.sh
+++ newlib/build-arm.sh
@@ -2,6 +2,23 @@
 
 NEWLIB_SRC_DIR=$1
 
+# We want to end up with newlib compiled for the `v6-m/nofp`, `v7-m/nofp`, and
+# `v7e-m/nofp` architectures. For that to happen the arm-none-eabi-gcc compiler
+# must have been compiled with multilib support for those architectures. We
+# verify that here before actually building anything.
+if ! arm-none-eabi-gcc --print-multi-lib | grep -q 'v6-m/nofp'; then
+  echo "ERROR: arm-none-eabi-gcc not configured with multilib support for v6-m/nofp"
+  exit -1
+fi
+if ! arm-none-eabi-gcc --print-multi-lib | grep -q 'v7-m/nofp'; then
+  echo "ERROR: arm-none-eabi-gcc not configured with multilib support for v7-m/nofp"
+  exit -1
+fi
+if ! arm-none-eabi-gcc --print-multi-lib | grep -q 'v7e-m/nofp'; then
+  echo "ERROR: arm-none-eabi-gcc not configured with multilib support for v7e-m/nofp"
+  exit -1
+fi
+
 $NEWLIB_SRC_DIR/configure --target=arm-none-eabi \
   --disable-newlib-supplied-syscalls \
   --disable-nls \
