e2a1604fad4700d953e5e6344cf229812f76c2ef
Brad Campbell <bradjc5@gmail.com>
2023-11-29 15:30:04 -0500

    remove tock from wm1110 library

--- wm1110/wm1110/wm1110.mk
+++ wm1110/wm1110/wm1110.mk
@@ -2,11 +2,11 @@ CSRCS += smtc_hal_gpio.c
 CSRCS += smtc_hal_spi.c
 CSRCS += lr11xx_hal.c
 CSRCS += lr11xx_system.c
-CSRCS += alarm_timer.c
-CSRCS += alarm_internal.c
-CSRCS += tock.c
+# CSRCS += alarm_timer.c
+# CSRCS += alarm_internal.c
+# CSRCS += tock.c
 CSRCS += lora_phy.c
-CSRCS += tock_gpio.c
+# CSRCS += tock_gpio.c
 
 DEPPATH += --dep-path $(WM1110_DIR)/$(WM1110_DIR_NAME)/src
 VPATH += :$(WM1110_DIR)/$(WM1110_DIR_NAME)/src
