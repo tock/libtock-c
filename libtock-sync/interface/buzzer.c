e917d8ed76321a97170e1d4bbca8a4fa6c4826af
Brad Campbell <bradjc5@gmail.com>
2024-02-16 18:50:36 -0500

    buzzer

---
+++ libtock-sync/interface/buzzer.c
@@ -0,0 +1,24 @@
+#include "buzzer.h"
+
+struct data {
+  bool fired;
+};
+
+static struct data result = { .fired = false };
+
+
+static void buzzer_cb(void) {
+  result.fired = true;
+}
+
+returncode_t libtocksync_buzzer_tone(uint32_t frequency_hz, uint32_t duration_ms) {
+  int err;
+  result.fired = false;
+
+  err = libtock_buzzer_tone(frequency_hz,  duration_ms, buzzer_cb);
+  if (err != RETURNCODE_SUCCESS) return err;
+
+  // Wait for the callback meaning the tone is finished.
+  yield_for(&result.fired);
+  return RETURNCODE_SUCCESS;
+}
---
+++ libtock/interface/syscalls/buzzer_syscalls.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "tock.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define DRIVER_NUM_BUZZER 0x90000
+
+// Check if the buzzer system call driver is available on this board.
+bool libtock_buzzer_exists(void);
+
+// Subscribe an upcall for the buzzer.
+returncode_t libtock_buzzer_set_upcall(subscribe_upcall callback, void* opaque);
+
+// Start a tone at a given frequency for a given duration.
+returncode_t libtock_buzzer_command_tone(uint32_t frequency_hz, uint32_t duration_ms);
+
+#ifdef __cplusplus
+}
+#endif
+
