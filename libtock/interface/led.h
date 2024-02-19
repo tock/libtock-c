dbfa53a7de73d1d71597a442e5c704c127748fc6
Brad Campbell <bradjc5@gmail.com>
2024-02-18 23:21:20 -0500

    update led

--- libtock/interface/led.h
+++ libtock/interface/led.h
@@ -6,14 +6,17 @@
 extern "C" {
 #endif
 
-#define DRIVER_NUM_LEDS 0x00000002
-
-int led_on(int led_num);
-int led_off(int led_num);
-int led_toggle(int led_num);
-
 // Returns the number of LEDs on the host platform.
 int led_count(int* count);
+
+// Turn on the specified LED.
+int led_on(int led_num);
+
+// Turn off the specified LED.
+int led_off(int led_num);
+
+// Toggle the specified LED.
+int led_toggle(int led_num);
 
 #ifdef __cplusplus
 }
