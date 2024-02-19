157741074eb4ad0da2343d89b7e2add49a8078c1
Brad Campbell <bradjc5@gmail.com>
2024-02-18 23:21:20 -0500

    led

--- libtock/interface/led.h
+++ libtock/interface/led.h
@@ -1,19 +1,23 @@
 #pragma once
 
 #include "tock.h"
+#include "syscalls/led_syscalls.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define DRIVER_NUM_LEDS 0x00000002
-
-int led_on(int led_num);
-int led_off(int led_num);
-int led_toggle(int led_num);
-
 // Returns the number of LEDs on the host platform.
-int led_count(int* count);
+returncode_t libtock_led_count(int* count);
+
+// Turn on the specified LED.
+returncode_t libtock_led_on(int led_num);
+
+// Turn off the specified LED.
+returncode_t libtock_led_off(int led_num);
+
+// Toggle the specified LED.
+returncode_t libtock_led_toggle(int led_num);
 
 #ifdef __cplusplus
 }
