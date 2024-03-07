fe4cd15867ffb9de1e2951265fe463b825f9b0f8
Brad Campbell <bradjc5@gmail.com>
2024-03-07 01:31:25 -0500

    spi_peripheral

---
+++ libtock-sync/peripherals/spi_peripheral.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <libtock/tock.h>
+#include <libtock/peripherals/spi_peripheral.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// As the SPI peripheral, write a buffer of data.
+returncode_t libtocksync_spi_peripheral_write(const uint8_t* write,
+                                              size_t         len);
+
+// As the SPI peripheral, write and read buffers of data.
+returncode_t libtocksync_spi_peripheral_read_write(const uint8_t* write,
+                                                   uint8_t*       read,
+                                                   size_t         len);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
