c66987da7a1b2bef18419f423720f1cf4d08190d
Brad Campbell <bradjc5@gmail.com>
2024-03-06 23:31:22 -0500

    analog_comparator

--- libtock/peripherals/analog_comparator.c
+++ libtock/peripherals/analog_comparator.c
@@ -1,31 +1,17 @@
 #include "analog_comparator.h"
-#include "tock.h"
 
-bool analog_comparator_exists(void) {
-  return driver_exists(DRIVER_NUM_ANALOG_COMPARATOR);
+returncode_t libtock_analog_comparator_count(int* count) {
+  return libtock_analog_comparator_command_count((uint32_t*) count);
 }
 
-int analog_comparator_comparison(uint8_t channel, bool* comparison) {
-  syscall_return_t com = command(DRIVER_NUM_ANALOG_COMPARATOR, 1, channel, 0);
-  return tock_command_return_u32_to_returncode(com, (uint32_t*) comparison);
+returncode_t libtock_analog_comparator_comparison(uint8_t channel, bool* comparison) {
+  return libtock_analog_comparator_command_comparison(channel, (uint32_t*) comparison);
 }
 
-int analog_comparator_start_comparing(uint8_t channel) {
-  syscall_return_t com = command(DRIVER_NUM_ANALOG_COMPARATOR, 2, channel, 0);
-  return tock_command_return_novalue_to_returncode(com);
+returncode_t libtock_analog_comparator_start_comparing(uint8_t channel) {
+  return libtock_analog_comparator_command_start_comparing(channel);
 }
 
-int analog_comparator_stop_comparing(uint8_t channel) {
-  syscall_return_t com = command(DRIVER_NUM_ANALOG_COMPARATOR, 3, channel, 0);
-  return tock_command_return_novalue_to_returncode(com);
-}
-
-int analog_comparator_count(int* count) {
-  syscall_return_t com = command(DRIVER_NUM_ANALOG_COMPARATOR, 4, 0, 0);
-  return tock_command_return_u32_to_returncode(com, (uint32_t*) count);
-}
-
-int analog_comparator_interrupt_callback(subscribe_upcall callback, void* callback_args) {
-  subscribe_return_t sub = subscribe(DRIVER_NUM_ANALOG_COMPARATOR, 0, callback, callback_args);
-  return tock_subscribe_return_to_returncode(sub);
+returncode_t libtock_analog_comparator_stop_comparing(uint8_t channel) {
+  return libtock_analog_comparator_command_stop_comparing(channel);
 }
