338c6e1e82b72aa2ee70f4ecdf3c2da9e06aaeb8
Brad Campbell <bradjc5@gmail.com>
2024-03-07 18:06:26 -0500

    humidity

--- libtock/sensors/humidity.h
+++ libtock/sensors/humidity.h
@@ -1,31 +1,20 @@
 #pragma once
 
 #include "tock.h"
+#include "syscalls/humidity_syscalls.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define DRIVER_NUM_HUMIDITY 0x60001
-
-// check if humidity sensor exists
-bool humidity_exists(void);
-
-// units: humidity in hundredths of percent.
-
-// function to be called when the humidity measurement is finished
+// Function signature for humidity data callback.
 //
-// callback       - pointer to function to be called
-// callback_args  - pointer to data provided to the callback
-int humidity_set_callback (subscribe_upcall callback, void* callback_args);
+// - `arg1` (`int`): Returncode indicating status from sampling the sensor.
+// - `arg2` (`int`): Humidity in hundredths of percent.
+typedef void (*libtock_humidity_callback)(returncode_t, int);
 
-// initiate an humidity measurement used both for synchronous and asynchronous readings
-int humidity_read(void);
-
-// initiate a synchronous humidity measurement
-//
-// humi           - pointer/address where the result of the humidity reading should be stored
-int humidity_read_sync (unsigned* humi);
+// Start a humidity measurement. The reading will be provided via the callback.
+returncode_t libtock_humidity_read(libtock_humidity_callback cb);
 
 #ifdef __cplusplus
 }
