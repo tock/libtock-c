a3b6f1f99615dd68fc7435b705e507f8771adb7f
Brad Campbell <bradjc5@gmail.com>
2024-03-07 01:12:16 -0500

    rtc

---
+++ libtock-sync/peripherals/rtc.c
@@ -0,0 +1,54 @@
+#include "rtc.h"
+
+struct rtc_date_data {
+  bool fired;
+  returncode_t ret;
+  libtock_rtc_date_t date;
+};
+struct rtc_done_data {
+  bool fired;
+  returncode_t ret;
+};
+
+static struct rtc_date_data result      = { .fired = false };
+static struct rtc_done_data result_done = { .fired = false };
+
+
+static void rtc_date_cb(returncode_t ret, libtock_rtc_date_t date) {
+  result.fired = true;
+  result.ret   = ret;
+  result.date  = date;
+}
+
+static void rtc_done_cb(returncode_t ret) {
+  result_done.fired = true;
+  result_done.ret   = ret;
+}
+
+returncode_t libtocksync_rtc_get_date(libtock_rtc_date_t* date) {
+  returncode_t ret;
+
+  result.fired = false;
+
+  ret = libtock_rtc_get_date(rtc_date_cb);
+  if (ret != RETURNCODE_SUCCESS) return ret;
+
+  yield_for(&result.fired);
+  if (result.ret != RETURNCODE_SUCCESS) return result.ret;
+
+  *date = result.date;
+  return RETURNCODE_SUCCESS;
+}
+
+returncode_t libtocksync_rtc_set_date(libtock_rtc_date_t* set_date) {
+  returncode_t ret;
+
+  result_done.fired = false;
+
+  ret = libtock_rtc_set_date(set_date, rtc_done_cb);
+  if (ret != RETURNCODE_SUCCESS) return ret;
+
+  yield_for(&result_done.fired);
+
+  return result_done.ret;
+}
---
+++ libtock/peripherals/syscalls/rtc_syscalls.c
@@ -0,0 +1,20 @@
+#include "rtc_syscalls.h"
+
+bool libtock_rtc_exists(void) {
+  return driver_exists(DRIVER_NUM_RTC);
+}
+
+returncode_t libtock_rtc_set_upcall(subscribe_upcall callback, void* opaque) {
+  subscribe_return_t sval = subscribe(DRIVER_NUM_RTC, 0, callback, opaque);
+  return tock_subscribe_return_to_returncode(sval);
+}
+
+returncode_t libtock_rtc_command_get_date(void) {
+  syscall_return_t cval = command(DRIVER_NUM_RTC, 1, 0, 0);
+  return tock_command_return_novalue_to_returncode(cval);
+}
+
+returncode_t libtock_rtc_command_set_date(uint32_t date, uint32_t time) {
+  syscall_return_t cval = command(DRIVER_NUM_RTC, 2, date, time);
+  return tock_command_return_novalue_to_returncode(cval);
+}
