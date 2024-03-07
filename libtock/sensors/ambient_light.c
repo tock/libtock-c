023f86fe89b365a34a6f2af7c6460f7da435140e
Brad Campbell <bradjc5@gmail.com>
2024-03-07 17:55:32 -0500

    ambient light

--- libtock/sensors/ambient_light.c
+++ libtock/sensors/ambient_light.c
@@ -1,52 +1,19 @@
 #include "ambient_light.h"
-#include "tock.h"
-
-typedef struct {
-  int intensity;
-  bool fired;
-} ambient_light_data_t;
 
 // callback for synchronous reads
 static void ambient_light_upcall(int intensity,
                                  __attribute__ ((unused)) int unused1,
-                                 __attribute__ ((unused)) int unused2, void* ud) {
-  ambient_light_data_t* result = (ambient_light_data_t*)ud;
-  result->intensity = intensity;
-  result->fired     = true;
+                                 __attribute__ ((unused)) int unused2, void* opaque) {
+  libtock_ambient_light_callback cb = (libtock_ambient_light_callback) opaque;
+  cb(RETURNCODE_SUCCESS, intensity);
 }
 
-bool ambient_light_exists(void) {
-  return driver_exists(DRIVER_NUM_AMBIENT_LIGHT);
-}
+returncode_t libtock_ambient_light_read_intensity(libtock_ambient_light_callback cb) {
+  returncode_t err;
 
-int ambient_light_read_intensity_sync(int* lux_value) {
-  int err;
-  ambient_light_data_t result = {0};
-  result.fired = false;
+  err = libtock_ambient_light_set_upcall(ambient_light_upcall, cb);
+  if (err != RETURNCODE_SUCCESS) return err;
 
-  err = ambient_light_subscribe(ambient_light_upcall, (void*)(&result));
-  if (err < RETURNCODE_SUCCESS) {
-    return err;
-  }
-
-  err = ambient_light_start_intensity_reading();
-  if (err < RETURNCODE_SUCCESS) {
-    return err;
-  }
-
-  yield_for(&result.fired);
-
-  *lux_value = result.intensity;
-
-  return RETURNCODE_SUCCESS;
-}
-
-int ambient_light_subscribe(subscribe_upcall callback, void* userdata) {
-  subscribe_return_t ret = subscribe(DRIVER_NUM_AMBIENT_LIGHT, 0, callback, userdata);
-  return tock_subscribe_return_to_returncode(ret);
-}
-
-int ambient_light_start_intensity_reading(void) {
-  syscall_return_t ret = command(DRIVER_NUM_AMBIENT_LIGHT, 1, 0, 0);
-  return tock_command_return_novalue_to_returncode(ret);
+  err = libtock_ambient_light_command_start_intensity_reading();
+  return err;
 }
