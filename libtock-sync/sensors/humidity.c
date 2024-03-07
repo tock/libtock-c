b4bff00ffedb864d5353144dc292440d3c2ea8e0
Brad Campbell <bradjc5@gmail.com>
2024-03-07 18:06:26 -0500

    humidity

---
+++ libtock-sync/sensors/humidity.c
@@ -0,0 +1,35 @@
+#include <sensors/humidity.h>
+#include <tock.h>
+
+#include "humidity.h"
+
+typedef struct {
+  int humidity;
+  returncode_t ret;
+  bool fired;
+} humidity_result_t;
+
+humidity_result_t result = {.fired = false};
+
+// callback for synchronous reads
+static void humidity_callback(returncode_t ret, int humidity) {
+  result.humidity = humidity;
+  result.ret      = ret;
+  result.fired    = true;
+}
+
+returncode_t libtocksync_humidity_read(int* humidity) {
+  returncode_t err;
+
+  result.fired = false;
+
+  err = libtock_humidity_read(humidity_callback);
+  if (err != RETURNCODE_SUCCESS) return err;
+
+  yield_for(&result.fired);
+  if (result.ret != RETURNCODE_SUCCESS) return result.ret;
+
+  *humidity = result.humidity;
+
+  return RETURNCODE_SUCCESS;
+}
---
+++ libtock-sync/sensors/humidity.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <tock.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Read the humidity sensor synchronously.
+//
+// ## Arguments
+//
+// - `humidity`: Set to the humidity in hundredths of a percent.
+//
+// ## Return Value
+//
+// A returncode indicating whether the sensor read was completed successfully.
+returncode_t libtocksync_humidity_read(int* humidity);
+
+#ifdef __cplusplus
+}
+#endif
