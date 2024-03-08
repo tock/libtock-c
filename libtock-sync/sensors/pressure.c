b86f340e44a4d0dcbfaa60a74b161e2d313f444a
Brad Campbell <bradjc5@gmail.com>
2024-03-07 23:35:43 -0500

    pressure

---
+++ libtock-sync/sensors/pressure.c
@@ -0,0 +1,32 @@
+#include "pressure.h"
+
+struct pressure_data {
+  bool fired;
+  int pressure;
+  returncode_t ret;
+};
+
+static struct pressure_data result = { .fired = false };
+
+static void pressure_cb(returncode_t ret, int pressure) {
+  result.pressure = pressure;
+  result.fired    = true;
+  result.ret      = ret;
+}
+
+returncode_t libtocksync_pressure_read(int* pressure) {
+  returncode_t err;
+
+  result.fired = false;
+
+  err = libtock_pressure_read(pressure_cb);
+  if (err != RETURNCODE_SUCCESS) return err;
+
+  // Wait for the callback.
+  yield_for(&result.fired);
+  if (result.ret != RETURNCODE_SUCCESS) return result.ret;
+
+  *pressure = result.pressure;
+
+  return RETURNCODE_SUCCESS;
+}
