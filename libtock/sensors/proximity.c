bdc1cb9b3d0a3110a56b1a036e83659090c183b2
Brad Campbell <bradjc5@gmail.com>
2024-03-07 23:58:37 -0500

    proximity

--- libtock/sensors/proximity.c
+++ libtock/sensors/proximity.c
@@ -1,92 +1,33 @@
 #include "proximity.h"
+#include "syscalls/proximity_syscalls.h"
 #include "tock.h"
-#include <stdio.h>
-
-struct thresholds {
-  uint8_t lower_threshold;
-  uint8_t higher_threshold;
-};
-
-// structure to store threshold values to be sent to the driver
-static struct thresholds threshes = {.lower_threshold = 0, .higher_threshold = 175};
-
-struct data {
-  bool fired;
-  int proximity;
-};
-
-static struct data result = {.fired = false};
 
 // Internal callback for faking synchronous reads
-static void cb(int                         proximity,
-               __attribute__((unused)) int unused,
-               __attribute__((unused)) int unused1,
-               void *                      ud) {
-  struct data *data = (struct data *)ud;
-  data->proximity = proximity;
-  data->fired     = true;
+static void proximity_cb(int                         proximity,
+                         __attribute__((unused)) int unused,
+                         __attribute__((unused)) int unused1,
+                         void *                      opaque) {
+  libtock_proximity_callback cb = (libtock_proximity_callback) opaque;
+  cb(RETURNCODE_SUCCESS, (uint8_t) proximity);
 }
 
-int proximity_set_callback(subscribe_upcall upcall, void *callback_args) {
-  subscribe_return_t sval = subscribe(DRIVER_NUM_PROXIMITY, 0, upcall, callback_args);
-  return tock_subscribe_return_to_returncode(sval);
+returncode_t libtock_proximity_read(libtock_proximity_callback cb) {
+  returncode_t err;
+
+  err = libtock_proximity_set_upcall(proximity_cb, cb);
+  if (err != RETURNCODE_SUCCESS) return err;
+
+  err = libtock_proximity_command_read();
+  return err;
 }
 
-int proximity_read(void) {
-  syscall_return_t com = command(DRIVER_NUM_PROXIMITY, 1, 0, 0);
-  return tock_command_return_novalue_to_returncode(com);
-}
+returncode_t libtock_proximity_read_on_interrupt(uint32_t lower_threshold, uint32_t higher_threshold,
+                                                 libtock_proximity_callback cb) {
+  returncode_t err;
 
-int proximity_read_on_interrupt(void) {
-  syscall_return_t com = command(DRIVER_NUM_PROXIMITY, 2, threshes.lower_threshold, threshes.higher_threshold);
-  return tock_command_return_novalue_to_returncode(com);
-}
+  err = libtock_proximity_set_upcall(proximity_cb, cb);
+  if (err != RETURNCODE_SUCCESS) return err;
 
-int proximity_set_interrupt_thresholds(uint8_t lower, uint8_t upper) {
-  threshes.lower_threshold  = lower;
-  threshes.higher_threshold = upper;
-  return 0;
-}
-
-int proximity_read_sync(uint8_t *proximity) {
-  int err;
-  result.fired = false;
-
-  err = proximity_set_callback(cb, (void *)&result);
-  if (err < 0) {
-    return err;
-  }
-
-  err = proximity_read();
-  if (err < 0) {
-    return err;
-  }
-
-  yield_for(&result.fired);
-
-  *proximity = result.proximity;
-
-  return RETURNCODE_SUCCESS;
-}
-
-int proximity_read_on_interrupt_sync(uint8_t *proximity) {
-
-  int err;
-  result.fired = false;
-
-  err = proximity_set_callback(cb, (void *)&result);
-  if (err < 0) {
-    return err;
-  }
-
-  err = proximity_read_on_interrupt();
-  if (err < 0) {
-    return err;
-  }
-
-  yield_for(&result.fired);
-
-  *proximity = result.proximity;
-
-  return RETURNCODE_SUCCESS;
+  err = libtock_proximity_command_read_on_interrupt(lower_threshold, higher_threshold);
+  return err;
 }
---
+++ libtock/sensors/syscalls/proximity_syscalls.c
@@ -0,0 +1,21 @@
+#include "proximity_syscalls.h"
+#include "tock.h"
+
+bool libtock_proximity_exists(void) {
+  return driver_exists(DRIVER_NUM_PROXIMITY);
+}
+
+returncode_t libtock_proximity_set_upcall(subscribe_upcall upcall, void *opaque) {
+  subscribe_return_t sval = subscribe(DRIVER_NUM_PROXIMITY, 0, upcall, opaque);
+  return tock_subscribe_return_to_returncode(sval);
+}
+
+returncode_t libtock_proximity_command_read(void) {
+  syscall_return_t cval = command(DRIVER_NUM_PROXIMITY, 1, 0, 0);
+  return tock_command_return_novalue_to_returncode(cval);
+}
+
+returncode_t libtock_proximity_command_read_on_interrupt(uint32_t lower_threshold, uint32_t higher_threshold) {
+  syscall_return_t cval = command(DRIVER_NUM_PROXIMITY, 2, lower_threshold, higher_threshold);
+  return tock_command_return_novalue_to_returncode(cval);
+}
