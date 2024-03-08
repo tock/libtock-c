01d296fbd5f896e7c19aced9106194d6cd1d0d5a
Brad Campbell <bradjc5@gmail.com>
2024-03-07 23:35:43 -0500

    pressure

---
+++ libtock-sync/sensors/pressure.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <tock.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Read the pressure synchronously.
+//
+// ## Arguments
+//
+// - `pressure`: The pressure reading in hPa.
+//
+// ## Return Value
+//
+// A returncode indicating whether the read was completed successfully.
+returncode_t libtocksync_pressure_read(int* pressure);
+
+#ifdef __cplusplus
+}
+#endif
