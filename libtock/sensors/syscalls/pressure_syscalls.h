4ee543f6063f410131ee5b7e8e6941870f505575
Brad Campbell <bradjc5@gmail.com>
2024-03-07 23:35:43 -0500

    pressure

---
+++ libtock/sensors/syscalls/pressure_syscalls.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "../../tock.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define DRIVER_NUM_PRESSURE 0x60008
+
+// Check if the pressure driver exists.
+bool libtock_pressure_exists(void);
+
+// Set the upcall for the pressure sensor.
+returncode_t libtock_pressure_set_upcall(subscribe_upcall callback, void* opaque);
+
+// Read the pressure sensor.
+returncode_t libtock_pressure_command_read(void);
+
+#ifdef __cplusplus
+}
+#endif
