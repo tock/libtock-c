1b7905ff6917666a7feea2e55e22897c32b7fe69
Brad Campbell <bradjc5@gmail.com>
2024-03-07 23:22:14 -0500

    ninedof

---
+++ libtock-sync/sensors/ninedof.c
@@ -0,0 +1,95 @@
+#include <math.h>
+#include <stdio.h>
+
+#include "ninedof.h"
+
+struct ninedof_data {
+  int x;
+  int y;
+  int z;
+  bool fired;
+  returncode_t ret;
+};
+
+static struct ninedof_data result = { .fired = false };
+
+
+
+static void ninedof_cb(returncode_t ret, int x, int y, int z) {
+  result.x     = x;
+  result.y     = y;
+  result.z     = z;
+  result.fired = true;
+  result.ret   = ret;
+}
+
+
+
+returncode_t libtocksync_ninedof_read_accelerometer(int* x, int* y, int* z) {
+  returncode_t err;
+
+  result.fired = false;
+
+  err = libtock_ninedof_read_accelerometer(ninedof_cb);
+  if (err != RETURNCODE_SUCCESS) return err;
+
+  // Wait for the callback.
+  yield_for(&result.fired);
+  if (result.ret != RETURNCODE_SUCCESS) return result.ret;
+
+  *x = result.x;
+  *y = result.y;
+  *z = result.z;
+
+  return RETURNCODE_SUCCESS;
+}
+
+returncode_t libtocksync_ninedof_read_accelerometer_magnitude(double* magnitude) {
+  returncode_t err;
+  int x, y, z;
+
+  err = libtocksync_ninedof_read_accelerometer(&x, &y, &z);
+  if (err != RETURNCODE_SUCCESS) return err;
+
+  *magnitude = sqrt(result.x * result.x + result.y * result.y + result.z * result.z);
+
+  return RETURNCODE_SUCCESS;
+}
+
+returncode_t libtocksync_ninedof_read_magnetometer(int* x, int* y, int* z) {
+  returncode_t err;
+
+  result.fired = false;
+
+  err = libtock_ninedof_read_magnetometer(ninedof_cb);
+  if (err != RETURNCODE_SUCCESS) return err;
+
+  // Wait for the callback.
+  yield_for(&result.fired);
+  if (result.ret != RETURNCODE_SUCCESS) return result.ret;
+
+  *x = result.x;
+  *y = result.y;
+  *z = result.z;
+
+  return RETURNCODE_SUCCESS;
+}
+
+returncode_t libtocksync_ninedof_read_gyroscope(int* x, int* y, int* z) {
+  returncode_t err;
+
+  result.fired = false;
+
+  err = libtock_ninedof_read_gyroscope(ninedof_cb);
+  if (err != RETURNCODE_SUCCESS) return err;
+
+  // Wait for the callback.
+  yield_for(&result.fired);
+  if (result.ret != RETURNCODE_SUCCESS) return result.ret;
+
+  *x = result.x;
+  *y = result.y;
+  *z = result.z;
+
+  return RETURNCODE_SUCCESS;
+}
---
+++ libtock/sensors/syscalls/ninedof_syscalls.c
@@ -0,0 +1,25 @@
+#include "ninedof_syscalls.h"
+
+bool libtock_ninedof_exists(void) {
+  return driver_exists(DRIVER_NUM_NINEDOF);
+}
+
+returncode_t libtock_ninedof_set_upcall(subscribe_upcall callback, void* opaque) {
+  subscribe_return_t sval = subscribe(DRIVER_NUM_NINEDOF, 0, callback, opaque);
+  return tock_subscribe_return_to_returncode(sval);
+}
+
+returncode_t libtock_ninedof_command_start_accelerometer_reading(void) {
+  syscall_return_t cval = command(DRIVER_NUM_NINEDOF, 1, 0, 0);
+  return tock_command_return_novalue_to_returncode(cval);
+}
+
+returncode_t libtock_ninedof_command_start_magnetometer_reading(void) {
+  syscall_return_t cval = command(DRIVER_NUM_NINEDOF, 100, 0, 0);
+  return tock_command_return_novalue_to_returncode(cval);
+}
+
+returncode_t libtock_ninedof_command_start_gyroscope_reading(void) {
+  syscall_return_t cval = command(DRIVER_NUM_NINEDOF, 200, 0, 0);
+  return tock_command_return_novalue_to_returncode(cval);
+}
