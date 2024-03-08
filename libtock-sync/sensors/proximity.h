2cd787bf4677b78db3970a0c039a7eec491298ce
Brad Campbell <bradjc5@gmail.com>
2024-03-07 23:58:37 -0500

    proximity

---
+++ libtock-sync/sensors/proximity.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <libtock/tock.h>
+#include <libtock/sensors/proximity.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Read proximity synchronously.
+//
+// This function queries the sensor for a proximity reading which is then
+// returned via the callback. A proximity value is in the range [0,255] where
+// '255' indicates the closest measurable distance and '0' that no object is
+// detected.
+returncode_t libtocksync_proximity_read(uint8_t *proximity);
+
+// Return proximity value on interrupt synchronously.
+//
+// This function can be used to wait for the sensor to detect a proximity
+// reading in the user-specified range. This range is determined by the
+// `proximity_set_interrupt_thresholds` arguments. A proximity value is in the
+// range [0,255] where '255' indicates the closest measurable distance and '0'
+// that no object is detected.
+returncode_t libtocksync_proximity_read_on_interrupt(uint32_t lower_threshold, uint32_t higher_threshold, uint8_t *proximity);
+
+#ifdef __cplusplus
+}
+#endif
--- libtock/sensors/proximity.h
+++ libtock/sensors/proximity.h
@@ -1,51 +1,30 @@
 #pragma once
 
-#include "tock.h"
-
-#define DRIVER_NUM_PROXIMITY 0x60005
+#include "../tock.h"
+#include "syscalls/proximity_syscalls.h"
 
 #ifdef _cplusplus
 extern "C" {
 #endif
 
-// function to be called when the system call is finished
+// Function signature for proximity data callback.
 //
-// callback       - pointer to function to be called
-// callback_args  - pointer to data provided to the callback
-int proximity_set_callback (subscribe_upcall callback, void* callback_args);
+// - `arg1` (`returncode_t`): Status from sampling the sensor.
+// - `arg2` (`uint8_t`): Proximity value in the range [0,255] where '255'
+//   indicates the closest measurable distance and '0' that no object is
+//   detected.
+typedef void (*libtock_proximity_callback)(returncode_t, uint8_t);
 
-// Read proximity asynchronously (no callback)
-int proximity_read(void);
-
-// Read proximity synchronously (via an internal callback)
-// This function queries the sensor for a proximity reading which is then returned via the callback.
-// A proximity value is of the following range [0,255] where '255' indicates the closest measurable distance and '0' that no object is detected.
+// Read proximity asynchronously.
 //
-// proximity      - pointer to byte-sized proximity data returned from the sensor.
-int proximity_read_sync(uint8_t* proximity);
+// This function queries the sensor for a proximity reading which is then
+// returned via the callback.
+returncode_t libtock_proximity_read(libtock_proximity_callback cb);
 
-// Return proximity value on interrupt asynchronously (no callback)
-// This function can be used to wait for the sensor to detect a proximity reading in the user-specified range.
-// This range is determined by the `proximity_set_interrupt_thresholds` arguments.
-int proximity_read_on_interrupt(void);
-
-// Return proximity value on interrupt synchronously (via an internal callback)
-// This function can be used to wait for the sensor to detect a proximity reading in the user-specified range.
-// This range is determined by the `proximity_set_interrupt_thresholds` arguments.
-// A proximity value is of the following range [0,255] where '255' indicates the closest measurable distance and '0' that no object is detected.
-//
-// proximity      - pointer to byte-sized proximity data returned from the sensor.
-int proximity_read_on_interrupt_sync(uint8_t* proximity);
-
-// Set thresholds for interrupts (no syscalls involved)
-//
-// The proximity sensor fires an interrupt (which executes the callback sent by the `proximity_read_on_interrupt_sync()` function)
-// The interrupt is fired when the following logic statement is true `(reading >= upper_threshold) || (reading <= lower_threshold)`
-//
-// lower          - lower interrupt threshold for sensor --> range is [0,255]
-// upper          - upper interrupt threshold for sensor --> range is [0,255]
-int proximity_set_interrupt_thresholds(uint8_t lower, uint8_t upper);
-
+// Read proximity asynchronously within a specified window. This function can be
+// used to wait for the sensor to detect a proximity reading in the
+// user-specified range.
+returncode_t libtock_proximity_read_on_interrupt(uint32_t lower_threshold, uint32_t higher_threshold, libtock_proximity_callback cb);
 
 #ifdef _cplusplus
 }
