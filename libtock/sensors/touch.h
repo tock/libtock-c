1332ebced03df9467abc5605ec46a38a0491b122
Brad Campbell <bradjc5@gmail.com>
2024-03-09 11:47:29 -0500

    touch

--- libtock/sensors/touch.h
+++ libtock/sensors/touch.h
@@ -1,15 +1,39 @@
-// Touch Panel Library
-
 #pragma once
 
 #include "tock.h"
+#include "syscalls/touch_syscalls.h"
 
-#define DRIVER_NUM_TOUCH 0x90002
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-#define TOUCH_STATUS_RELEASED 0
-#define TOUCH_STATUS_PRESSED 1
-#define TOUCH_STATUS_MOVED 2
-#define TOUCH_STATUS_UNSTARTED 3
+
+// Function signature for touch data callback.
+//
+// - `arg1` (`int`): Status from touch device.
+// - `arg2` (`uint16_t`): X coordinate of the touch event.
+// - `arg3` (`uint16_t`): Y coordinate of the touch event.
+typedef void (*libtock_touch_touch_callback)(int, uint16_t, uint16_t);
+
+// Function signature for multi touch data callback.
+//
+// - `arg1` (`returncode_t`): Status from touch device.
+// - `arg2` (`int`): Number of touch events.
+// - `arg3` (`int`): Number of dropped events.
+// - `arg4` (`int`): Number of events that did not fit in the buffer.
+typedef void (*libtock_touch_multi_touch_callback)(returncode_t, int, int, int);
+
+// Function signature for gesture callback.
+//
+// - `arg1` (`returncode_t`): Status from touch device.
+// - `arg2` (`int`): Gesture type.
+typedef void (*libtock_touch_gesture_callback)(returncode_t, int);
+
+
+#define LIBTOCK_TOUCH_STATUS_RELEASED 0
+#define LIBTOCK_TOUCH_STATUS_PRESSED 1
+#define LIBTOCK_TOUCH_STATUS_MOVED 2
+#define LIBTOCK_TOUCH_STATUS_UNSTARTED 3
 
 #define GESTURE_NO 0
 #define GESTURE_SWIPE_UP 1
@@ -19,8 +43,6 @@
 #define GESTURE_ZOOM_IN 5
 #define GESTURE_ZOOM_OUT 6
 
-typedef void (touch_callback)(int, int, int, void*);
-typedef void (gesture_callback)(int, void*);
 
 typedef struct __attribute__((__packed__)) {
     unsigned char id;
@@ -29,7 +51,7 @@ typedef struct __attribute__((__packed__)) {
     unsigned short y;
     unsigned char size;
     unsigned char pressure;
-} touch_t;
+} libtock_touch_event_t;
 
 // buffer data format
 //  0         1             2                  4                  6           7               8         ...
@@ -38,19 +60,29 @@ typedef struct __attribute__((__packed__)) {
 // +---------+-------------+------------------+------------------+-----------+---------------+--------- ...
 // | Touch 0                                                                                 | Touch 1  ...
 
-int get_number_of_touches (int* touches);
 
-int enable_single_touch(void);
-int disable_single_touch(void);
-int single_touch_set_callback (touch_callback cb, void* ud);
 
-int enable_multi_touch(void);
-int disable_multi_touch(void);
-int multi_touch_set_callback (touch_callback cb, void* ud, int max_touches);
-int gesture_set_callback (gesture_callback cb, void* ud);
+returncode_t libtock_touch_get_number_of_touches(int* touches);
 
-int read_touch (int index, unsigned char *id, unsigned char *status, unsigned short *x, unsigned short *y);
-int read_touch_full (int index, unsigned char *id, unsigned char *status, unsigned short *x, unsigned short *y, unsigned char *size, unsigned char *pressure);
+returncode_t libtock_touch_enable_single_touch(libtock_touch_touch_callback cb);
+
+returncode_t libtock_touch_disable_single_touch(void);
+
+returncode_t libtock_touch_enable_multi_touch(libtock_touch_event_t* buffer, int max_touches, libtock_touch_multi_touch_callback cb);
+
+returncode_t libtock_touch_disable_multi_touch(void);
+
+returncode_t libtock_touch_allocate_multi_touch_buffer(int max_touches, libtock_touch_event_t** buffer);
+
+returncode_t libtock_touch_get_gestures(libtock_touch_gesture_callback cb);
 
 // Every multi touch event needs to be acked
-int multi_touch_next (void);
+returncode_t libtock_touch_multi_touch_next(void);
+
+returncode_t libtock_touch_read_touch_from_buffer(libtock_touch_event_t* buffer, int index,
+  uint8_t *id, uint8_t *status, uint8_t *x, uint8_t *y,
+  uint8_t *size, uint8_t *pressure);
+
+#ifdef __cplusplus
+}
+#endif
