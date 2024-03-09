f8cb89a9d9d97988ebf64b3aa042e535cbc73cda
Brad Campbell <bradjc5@gmail.com>
2024-03-09 09:55:58 -0500

    sound_pressure

---
+++ libtock-sync/sensors/sound_pressure.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <libtock/tock.h>
+#include <libtock/sensors/sound_pressure.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Read the ambient sound pressure level synchronously.
+//
+// ## Arguments
+//
+// - `sound_pressure`: The pressure reading in dB.
+//
+// ## Return Value
+//
+// A returncode indicating whether the read was completed successfully.
+returncode_t libtocksync_sound_pressure_read(uint8_t* sound_pressure);
+
+#ifdef __cplusplus
+}
+#endif
