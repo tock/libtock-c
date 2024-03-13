2a2bdcb9a86793502642accb3d6f82f48b1802f3
Brad Campbell <bradjc5@gmail.com>
2024-03-12 23:20:17 -0400

    touch

--- libtock/sensors/touch.c
+++ libtock/sensors/touch.c
@@ -1,132 +1,129 @@
-#include "touch.h"
 #include <stdlib.h>
 
-static int touch_subscribe(int subscribe_num, subscribe_upcall callback, void* callback_args) {
-  subscribe_return_t sval = subscribe(DRIVER_NUM_TOUCH, subscribe_num, callback, callback_args);
-  return tock_subscribe_return_to_returncode(sval);
+#include "touch.h"
+#include "touch_syscalls.h"
+
+
+
+static void single_touch_upcall(int                          status,
+                                int                          xy,
+                                __attribute__ ((unused)) int unused1,
+                                void*                        opaque) {
+  libtock_touch_touch_callback cb = (libtock_touch_touch_callback) opaque;
+
+  uint16_t x = (uint16_t) (((uint32_t) xy) >> 16);
+  uint16_t y = (uint16_t) (((uint32_t) xy) & 0xFFFF);
+
+  cb(status, x, y);
 }
 
-static int touch_allow(int allow_num, void* data, int len) {
-  allow_rw_return_t aval = allow_readwrite(DRIVER_NUM_TOUCH, allow_num, data, len);
-  return tock_allow_rw_return_to_returncode(aval);
+static void multi_touch_upcall(int   num_events,
+                               int   dropped_events,
+                               int   lost_touches,
+                               void* opaque) {
+  libtock_touch_multi_touch_callback cb = (libtock_touch_multi_touch_callback) opaque;
+
+  cb(RETURNCODE_SUCCESS, num_events, dropped_events, lost_touches);
 }
 
-static touch_t *multi_touch_buffer = NULL;
-static unsigned char num_touches   = 0;
+static void gesture_upcall(int                          gesture,
+                           __attribute__ ((unused)) int unused0,
+                           __attribute__ ((unused)) int unused1,
+                           void*                        opaque) {
+  libtock_touch_gesture_callback cb = (libtock_touch_gesture_callback) opaque;
 
-static touch_callback *single_touch_upcall = NULL;
-static gesture_callback *gesture_upcall    = NULL;
-
-static void touch_single_touch_callback (int status, int xy, int data2 __attribute__((unused)), void *ud) {
-  if (single_touch_upcall) single_touch_upcall(status, ((unsigned int)xy >> 16), (unsigned int)xy & 0xFFFF, ud);
+  cb(RETURNCODE_SUCCESS, gesture);
 }
 
-static void touch_gesture_callback (int gesture, int data1 __attribute__((unused)), int data2 __attribute__(
-                                      (unused)), void *ud) {
-  if (gesture_upcall) gesture_upcall(gesture, ud);
+
+
+returncode_t libtock_touch_get_number_of_touches(int* touches) {
+  return libtock_touch_command_get_number_of_touches((uint32_t*) touches);
 }
 
-int get_number_of_touches (int* touches) {
-  syscall_return_t cval = command(DRIVER_NUM_TOUCH, 100, 0, 0);
-  return tock_command_return_u32_to_returncode(cval, (uint32_t*) touches);
+returncode_t libtock_touch_enable_single_touch(libtock_touch_touch_callback cb) {
+  returncode_t ret;
+
+  ret = libtock_touch_set_upcall_single_touch(single_touch_upcall, cb);
+  if (ret != RETURNCODE_SUCCESS) return ret;
+
+  ret = libtock_touch_command_enable_single_touch();
+  return ret;
 }
 
-int enable_single_touch(void) {
-  syscall_return_t cval = command(DRIVER_NUM_TOUCH, 1, 0, 0);
-  return tock_command_return_novalue_to_returncode(cval);
+returncode_t libtock_touch_disable_single_touch(void) {
+  returncode_t ret;
+
+  ret = libtock_touch_set_upcall_single_touch(NULL, NULL);
+  if (ret != RETURNCODE_SUCCESS) return ret;
+
+  ret = libtock_touch_command_disable_single_touch();
+  return ret;
 }
 
-int disable_single_touch(void) {
-  syscall_return_t cval = command(DRIVER_NUM_TOUCH, 2, 0, 0);
-  return tock_command_return_novalue_to_returncode(cval);
+returncode_t libtock_touch_enable_multi_touch(libtock_touch_event_t* buffer, int max_touches,
+                                              libtock_touch_multi_touch_callback cb) {
+  returncode_t ret;
+
+  ret = libtock_touch_set_allow_readwrite_multi_touch(buffer, max_touches * sizeof(libtock_touch_event_t));
+  if (ret != RETURNCODE_SUCCESS) return ret;
+
+  ret = libtock_touch_set_upcall_multi_touch(multi_touch_upcall, cb);
+  return ret;
+
 }
 
-int single_touch_set_callback (touch_callback cb, void* ud) {
-  single_touch_upcall = cb;
-  return touch_subscribe(0, cb != NULL ? touch_single_touch_callback : NULL, ud);
+returncode_t libtock_touch_disable_multi_touch(void) {
+  returncode_t ret;
+
+  ret = libtock_touch_set_allow_readwrite_multi_touch(NULL, 0);
+  if (ret != RETURNCODE_SUCCESS) return ret;
+
+  ret = libtock_touch_set_upcall_multi_touch(NULL, NULL);
+  if (ret != RETURNCODE_SUCCESS) return ret;
+
+  ret = libtock_touch_command_disable_multi_touch();
+  return ret;
 }
 
-int enable_multi_touch(void) {
-  syscall_return_t cval = command(DRIVER_NUM_TOUCH, 11, 0, 0);
-  return tock_command_return_novalue_to_returncode(cval);
-}
+returncode_t libtock_touch_allocate_multi_touch_buffer(int max_touches, libtock_touch_event_t** buffer) {
+  libtock_touch_event_t* multi_touch_buffer;
+  multi_touch_buffer = (libtock_touch_event_t*) malloc(max_touches * sizeof(libtock_touch_event_t));
 
-int disable_multi_touch(void) {
-  syscall_return_t cval = command(DRIVER_NUM_TOUCH, 12, 0, 0);
-  return tock_command_return_novalue_to_returncode(cval);
-}
-
-int multi_touch_set_callback (touch_callback cb, void* ud, int max_touches) {
-  int err = RETURNCODE_SUCCESS;
-  if (cb != NULL) {
-    if (multi_touch_buffer == NULL) {
-      multi_touch_buffer = (touch_t*)malloc(max_touches * sizeof(touch_t));
-      if (multi_touch_buffer) {
-        num_touches = max_touches;
-        err         = touch_allow(2, multi_touch_buffer, max_touches * sizeof(touch_t));
-        if (err == RETURNCODE_SUCCESS) {
-          err = touch_subscribe(2, cb, ud);
-        }
-        if (err != RETURNCODE_SUCCESS) {
-          free(multi_touch_buffer);
-          multi_touch_buffer = NULL;
-        }
-      } else {
-        err = RETURNCODE_ENOMEM;
-      }
-    } else {
-      err = RETURNCODE_EALREADY;
-    }
+  if (multi_touch_buffer == NULL) {
+    return RETURNCODE_ENOMEM;
   } else {
-    if (multi_touch_buffer != NULL) {
-      num_touches = 0;
-      touch_allow(2, NULL, 0);
-      err = touch_subscribe(2, cb, ud);
-      free(multi_touch_buffer);
-      multi_touch_buffer = NULL;
-    }
+    *buffer = multi_touch_buffer;
+    return RETURNCODE_SUCCESS;
   }
-  return err;
 }
 
-int gesture_set_callback (gesture_callback cb, void* ud) {
-  gesture_upcall = cb;
-  return touch_subscribe(1, cb != NULL ? touch_gesture_callback : NULL, ud);
+
+
+returncode_t libtock_touch_get_gestures(libtock_touch_gesture_callback cb) {
+  return libtock_touch_set_upcall_gesture(gesture_upcall, cb);
 }
+
+
+returncode_t multi_touch_next(void) {
+  return libtock_touch_command_multi_touch_next();
+}
+
 
 // get multi touch
 
-int read_touch (int index, unsigned char *id, unsigned char *status, unsigned short *x, unsigned short *y) {
-  if (index < num_touches) {
-    if (multi_touch_buffer != NULL) {
-      *id     = multi_touch_buffer[index].id;
-      *status = multi_touch_buffer[index].status;
-      *x      = multi_touch_buffer[index].x;
-      *y      = multi_touch_buffer[index].y;
-      return RETURNCODE_SUCCESS;
-    } else {
-      return RETURNCODE_ENOMEM;
-    }
-  } else {
-    return RETURNCODE_EINVAL;
-  }
-}
-
-int read_touch_full (int index, unsigned char *id, unsigned char *status, unsigned short *x, unsigned short *y,
-                     unsigned char *size, unsigned char *pressure) {
-  if (multi_touch_buffer != NULL) {
-    int err = read_touch(index, id, status, x, y);
-    if (err == RETURNCODE_SUCCESS) {
-      *size     = multi_touch_buffer[index].size;
-      *pressure = multi_touch_buffer[index].pressure;
-    }
-    return err;
+returncode_t libtock_touch_read_touch_from_buffer(libtock_touch_event_t* buffer, int index,
+                                                  uint8_t *id, uint8_t *status, uint8_t *x, uint8_t *y,
+                                                  uint8_t *size, uint8_t *pressure) {
+  if (buffer != NULL) {
+    *id       = buffer[index].id;
+    *status   = buffer[index].status;
+    *x        = buffer[index].x;
+    *y        = buffer[index].y;
+    *size     = buffer[index].size;
+    *pressure = buffer[index].pressure;
+    return RETURNCODE_SUCCESS;
   } else {
     return RETURNCODE_ENOMEM;
   }
-}
-
-int multi_touch_next (void) {
-  syscall_return_t cval = command(DRIVER_NUM_TOUCH, 10, 0, 0);
-  return tock_command_return_novalue_to_returncode(cval);
 }
