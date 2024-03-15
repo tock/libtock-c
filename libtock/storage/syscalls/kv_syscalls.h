7f964c71cccbbd4e9458425d4bc31b27c4dcd29d
Brad Campbell <bradjc5@gmail.com>
2024-03-14 22:24:52 -0400

    kv

---
+++ libtock/storage/syscalls/kv_syscalls.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "../../tock.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define DRIVER_NUM_KV 0x50003
+
+bool libtock_kv_exists(void);
+
+returncode_t libtock_kv_set_upcall(subscribe_upcall callback, void* opaque);
+
+returncode_t libtock_kv_set_readonly_allow_key_buffer(const uint8_t* buffer, uint32_t len);
+
+returncode_t libtock_kv_set_readonly_allow_input_buffer(const uint8_t* buffer, uint32_t len);
+
+returncode_t libtock_kv_set_readwrite_allow_output_buffer(uint8_t* buffer, uint32_t len);
+
+returncode_t libtock_kv_command_get(void);
+
+returncode_t libtock_kv_command_set(void);
+
+returncode_t libtock_kv_command_delete(void);
+
+returncode_t libtock_kv_command_add(void);
+
+returncode_t libtock_kv_command_update(void);
+
+#ifdef __cplusplus
+}
+#endif
+
