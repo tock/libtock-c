9d265aafab277fa7b6c4df7c97b4f0e4bc126f99
Brad Campbell <bradjc5@gmail.com>
2024-03-22 13:31:11 -0400

    sha

--- libtock/crypto/sha.c
+++ libtock/crypto/sha.c
@@ -1,48 +1,31 @@
 #include "sha.h"
 
-#define DRIVER_NUM_SHA 0x40005
-
-#define TOCK_SHA_CB       0
-
-#define TOCK_SHA_DATA_BUF     1
-#define TOCK_SHA_DEST_BUF     2
-
-#define TOCK_SHA_SET_ALGORITHM   0
-#define TOCK_SHA_RUN             1
-#define TOCK_SHA_UPDATE          2
-#define TOCK_SHA_FINISH          3
-
-int sha_set_callback (subscribe_upcall callback, void* callback_args) {
-  subscribe_return_t sval = subscribe(DRIVER_NUM_SHA, TOCK_SHA_CB, callback, callback_args);
-  return tock_subscribe_return_to_returncode(sval);
+static void sha_upcall(int ret,
+                       __attribute__ ((unused)) int unused1,
+                       __attribute__ ((unused)) int unused2, void* opaque) {
+  libtock_sha_callback_hash cb = (libtock_sha_callback_hash) opaque;
+  cb((returncode_t) ret);
 }
 
-int sha_set_data_buffer(uint8_t* buffer, uint32_t len) {
-  allow_ro_return_t aval = allow_readonly(DRIVER_NUM_SHA, TOCK_SHA_DATA_BUF, (void*) buffer, len);
-  return tock_allow_ro_return_to_returncode(aval);
-}
+returncode_t libtock_sha_simple_hash(libtock_sha_algorithm_t hash_type,
+                                     uint8_t* input_buffer, uint32_t input_length,
+                                     uint8_t* hash_buffer, uint32_t hash_length,
+                                     libtock_sha_callback_hash cb) {
 
-int sha_set_dest_buffer(uint8_t* buffer, uint32_t len) {
-  allow_rw_return_t aval = allow_readwrite(DRIVER_NUM_SHA, TOCK_SHA_DEST_BUF, (void*) buffer, len);
-  return tock_allow_rw_return_to_returncode(aval);
-}
+  returncode_t ret;
 
-int sha_set_algorithm(uint8_t hash) {
-  syscall_return_t cval = command(DRIVER_NUM_SHA, TOCK_SHA_SET_ALGORITHM, hash, 0);
-  return tock_command_return_novalue_to_returncode(cval);
-}
+  ret = libtock_sha_command_set_algorithm((uint8_t) hash_type);
+  if (ret != RETURNCODE_SUCCESS) return ret;
 
-int sha_run(void) {
-  syscall_return_t cval = command(DRIVER_NUM_SHA, TOCK_SHA_RUN, 0, 0);
-  return tock_command_return_novalue_to_returncode(cval);
-}
+  ret = libtock_sha_set_readonly_allow_data_buffer(input_buffer, input_length);
+  if (ret != RETURNCODE_SUCCESS) return ret;
 
-int sha_update(void) {
-  syscall_return_t cval = command(DRIVER_NUM_SHA, TOCK_SHA_UPDATE, 0, 0);
-  return tock_command_return_novalue_to_returncode(cval);
-}
+  ret = libtock_sha_set_readwrite_allow_destination_buffer(hash_buffer, hash_length);
+  if (ret != RETURNCODE_SUCCESS) return ret;
 
-int sha_finish(void) {
-  syscall_return_t cval = command(DRIVER_NUM_SHA, TOCK_SHA_FINISH, 0, 0);
-  return tock_command_return_novalue_to_returncode(cval);
+  ret = libtock_sha_set_upcall(sha_upcall, cb);
+  if (ret != RETURNCODE_SUCCESS) return ret;
+
+  ret = libtock_sha_command_run();
+  return ret;
 }
