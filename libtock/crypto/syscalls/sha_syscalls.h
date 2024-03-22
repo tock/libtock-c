27ddd66489fe651feb5b74d7327f2316119e1ce9
Brad Campbell <bradjc5@gmail.com>
2024-03-22 13:31:11 -0400

    sha: syscalls

---
+++ libtock/crypto/syscalls/sha_syscalls.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "tock.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define DRIVER_NUM_SHA 0x40005
+
+returncode_t libtock_sha_set_upcall(subscribe_upcall callback, void* opaque);
+
+returncode_t libtock_sha_readonly_allow_data_buffer(uint8_t* buffer, uint32_t len);
+
+returncode_t libtock_sha_readwrite_allow_destination_buffer(uint8_t* buffer, uint32_t len);
+
+returncode_t libtock_sha_command_set_algorithm(uint8_t hash);
+
+returncode_t libtock_sha_command_run(void);
+
+returncode_t libtock_sha_command_update(void);
+
+returncode_t libtock_sha_command_finish(void);
+
+#ifdef __cplusplus
+}
+#endif
