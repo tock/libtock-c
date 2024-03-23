c372c5eecc16cdbf497fbc5ad099bd0a2534c613
Brad Campbell <bradjc5@gmail.com>
2024-03-23 13:31:32 -0400

    test: udp

--- examples/tests/usb/main.c
+++ examples/tests/usb/main.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 #include <timer.h>
-#include <usb.h>
+#include <libtock-sync/peripherals/usb.h>
 
 int main(void) {
-  int r;
+  returncode_t ret;
+  printf("[TEST] UDP\n");
 
-  if (!usb_exists()) {
+  if (!libtock_usb_exists()) {
     printf("USB test: driver is not present\n");
-    exit(1);
+    return -1;
   }
 
-  r = usb_enable_and_attach();
-
-  if (r == RETURNCODE_SUCCESS) {
+  ret = libtocksync_usb_enable_and_attach();
+  if (ret == RETURNCODE_SUCCESS) {
     printf("USB test: Enabled and attached\n");
   } else {
-    printf("USB test: Attach failed with status %d\n", r);
+    printf("USB test: Attach failed with status %d\n", ret);
   }
+
+  return 0;
 }
