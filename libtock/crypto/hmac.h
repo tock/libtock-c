249a89b8e6d2dabaeec60716e847d1e576c53522
Brad Campbell <bradjc5@gmail.com>
2024-03-23 10:53:51 -0400

    hmac

--- libtock/crypto/hmac.h
+++ libtock/crypto/hmac.h
@@ -1,26 +1,40 @@
 #pragma once
 
 #include "tock.h"
+#include "syscalls/hmac_syscalls.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define TOCK_HMAC_ALG_SHA256    0
-#define TOCK_HMAC_ALG_SHA384    1
-#define TOCK_HMAC_ALG_SHA512    2
+// Function signature for HMAC callback.
+//
+// - `arg1` (`returncode_t`): Status from computing the HMAC.
+typedef void (*libtock_hmac_callback_hmac)(returncode_t);
 
-int hmac_set_callback (subscribe_upcall callback, void* callback_args);
+typedef enum {
+	LIBTOCK_HMAC_SHA256 = 0,
+	LIBTOCK_HMAC_SHA384 = 1,
+	LIBTOCK_HMAC_SHA512 = 2,
+} libtock_hmac_algorithm_t;
 
-int hmac_set_key_buffer(const uint8_t* buffer, uint32_t len);
-int hmac_set_data_buffer(const uint8_t* buffer, uint32_t len);
-int hmac_set_dest_buffer(uint8_t* buffer, uint32_t len);
 
-int hmac_set_algorithm(uint8_t hash);
-int hmac_run(void);
-int hmac_update(void);
-int hmac_finish(void);
+
+// Compute an HMAC using `keyb_buffer` over `input_buffer` and store the result
+// in `hash_buffer`.
+//
+// The callback will be called when the HMAC is available.
+returncode_t libtock_hmac_simple(libtock_hmac_algorithm_t hmac_type,
+  uint8_t* key_buffer, uint32_t key_length,
+                                     uint8_t* input_buffer, uint32_t input_length,
+                                     uint8_t* hash_buffer, uint32_t hash_length,
+                                     libtock_hmac_callback_hmac cb);
 
 #ifdef __cplusplus
 }
 #endif
+
+
+
+
+
