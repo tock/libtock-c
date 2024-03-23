1049d368caa5aaee6c661f6922b5f9a1d9eb355f
Brad Campbell <bradjc5@gmail.com>
2024-03-23 00:16:41 -0400

    lora_phy

---
+++ libtock-sync/net/lora_phy.c
@@ -0,0 +1,45 @@
+#include "lora_phy.h"
+
+
+
+struct lora_phy_spi_data {
+  bool fired;
+  returncode_t ret;
+};
+
+static struct lora_phy_spi_data result = {.fired = false};
+
+static void lora_phy_spi_cb(returncode_t ret) {
+  result.fired = true;
+  result.ret   = ret;
+}
+
+returncode_t libtocksync_lora_phy_write(const uint8_t* write,
+                                        uint32_t       len) {
+  result.fired = false;
+  returncode_t ret;
+
+  ret = libtock_lora_phy_set_master_read_buffer(NULL, 0);
+  if (ret != RETURNCODE_SUCCESS) return ret;
+
+  ret = libtock_lora_phy_write(write, len, lora_phy_spi_cb);
+  if (ret != RETURNCODE_SUCCESS) return ret;
+
+  yield_for(&result.fired);
+  return result.ret;
+}
+
+returncode_t libtocksync_lora_phy_read_write(const uint8_t* write,
+                                             uint8_t*       read,
+                                             uint32_t       len) {
+
+  result.fired = false;
+  returncode_t ret;
+
+  ret = libtock_lora_phy_read_write(write, read, len, lora_phy_spi_cb);
+  if (ret != RETURNCODE_SUCCESS) return ret;
+
+  yield_for(&result.fired);
+  return result.ret;
+
+}
