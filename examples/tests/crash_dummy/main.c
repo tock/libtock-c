ef47175c6b0cf335d1f56c7875e8a5feb75cc1c5
Brad Campbell <bradjc5@gmail.com>
2024-03-23 12:00:07 -0400

    test: crash_dummy

--- examples/tests/crash_dummy/main.c
+++ examples/tests/crash_dummy/main.c
@@ -1,19 +1,17 @@
 // Crash on button press.
 
-#include <button.h>
+#include <libtock/interface/button.h>
 
 volatile int* nullptr = 0;
 
-static void button_callback(__attribute__ ((unused)) int   btn_num,
-                            __attribute__ ((unused)) int   val,
-                            __attribute__ ((unused)) int   arg2,
-                            __attribute__ ((unused)) void *ud) {
+static void button_callback(__attribute__ ((unused)) returncode_t ret,
+                            __attribute__ ((unused)) int          btn_num,
+                            __attribute__ ((unused)) bool         val) {
   __attribute__ ((unused)) volatile int k = *nullptr;
 }
 
 int main(void) {
-  button_subscribe(button_callback, NULL);
-  button_enable_interrupt(0);
+  libtock_button_notify_on_press(0, button_callback);
 
-  return 0;
+  while (1) yield();
 }
