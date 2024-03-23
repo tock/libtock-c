ecb4b86266874be2e49994b26c3f5c65536c25a8
Brad Campbell <bradjc5@gmail.com>
2024-03-23 14:37:11 -0400

    sdcard

---
+++ libtock-sync/storage/sdcard.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <libtock/tock.h>
+#include <libtock/storage/sdcard.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Initialize the SD card.
+returncode_t libtocksync_sdcard_initialize(uint32_t* block_size, uint32_t* size_in_kB);
+
+// Read a block from the SD card.
+returncode_t libtocksync_sdcard_read_block(uint32_t sector, uint8_t* buffer, uint32_t len);
+
+// Write a block to the SD card.
+returncode_t libtocksync_sdcard_write_block(uint32_t sector, uint8_t* buffer, uint32_t len);
+
+#ifdef __cplusplus
+}
+#endif
