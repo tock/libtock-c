1493be5812408bfa73e574e74776e9844b8d6074
Brad Campbell <bradjc5@gmail.com>
2024-03-23 10:53:51 -0400

    hmac

---
+++ libtock-sync/crypto/hmac.c
@@ -0,0 +1,45 @@
+#include "hmac.h"
+
+struct hmac_data {
+  bool fired;
+  returncode_t ret;
+};
+
+static struct hmac_data result = {.fired = false};
+
+static void hmac_cb_hmac(returncode_t ret) {
+  result.fired = true;
+  result.ret   = ret;
+}
+
+
+
+returncode_t libtocksync_hmac_simple(libtock_hmac_algorithm_t hmac_type,
+                                     uint8_t* key_buffer, uint32_t key_length,
+                                     uint8_t* input_buffer, uint32_t input_length,
+                                     uint8_t* hmac_buffer, uint32_t hmac_length) {
+
+  returncode_t ret;
+
+  result.fired = false;
+
+  ret = libtock_hmac_simple(hmac_type, key_buffer, key_length, input_buffer, input_length, hmac_buffer, hmac_length,
+                            hmac_cb_hmac);
+  if (ret != RETURNCODE_SUCCESS) return ret;
+
+  // Wait for the callback.
+  yield_for(&result.fired);
+  if (result.ret != RETURNCODE_SUCCESS) return result.ret;
+
+  ret = libtock_hmac_set_readonly_allow_key_buffer(NULL, 0);
+  if (ret != RETURNCODE_SUCCESS) return ret;
+
+  ret = libtock_hmac_set_readonly_allow_data_buffer(NULL, 0);
+  if (ret != RETURNCODE_SUCCESS) return ret;
+
+  ret = libtock_hmac_set_readwrite_allow_destination_buffer(NULL, 0);
+  if (ret != RETURNCODE_SUCCESS) return ret;
+
+  return RETURNCODE_SUCCESS;
+
+}
---
+++ libtock/crypto/syscalls/hmac_syscalls.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "tock.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+bool libtock_hmac_exists(void);
+
+returncode_t libtock_hmac_set_upcall(subscribe_upcall callback, void* opaque);
+
+returncode_t libtock_hmac_set_readonly_allow_key_buffer(const uint8_t* buffer, uint32_t len);
+
+returncode_t libtock_hmac_set_readonly_allow_data_buffer(const uint8_t* buffer, uint32_t len);
+
+returncode_t libtock_hmac_set_readwrite_allow_destination_buffer(uint8_t* buffer, uint32_t len);
+
+returncode_t libtock_hmac_command_set_algorithm(uint32_t hash);
+
+returncode_t libtock_hmac_command_run(void);
+
+returncode_t libtock_hmac_command_update(void);
+
+returncode_t libtock_hmac_command_finish(void);
+
+#ifdef __cplusplus
+}
+#endif
