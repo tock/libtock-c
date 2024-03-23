c97354a13e347fa3f6618f0e0d9931548d843a92
Brad Campbell <bradjc5@gmail.com>
2024-03-23 14:37:11 -0400

    sdcard

--- libtock/storage/sdcard.h
+++ libtock/storage/sdcard.h
@@ -1,96 +1,58 @@
 #pragma once
 
-#include <stdint.h>
-
 #include "tock.h"
+#include "syscalls/sdcard_syscalls.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define DRIVER_NUM_SDCARD 0x50002
-
-// set a callback function for SD card commands
-// Callback is called upon command completion or when an error occurs. See
-// `sdcard_cb` in sdcard.c for callback documentation
+// Function signature for SD card initialization callback.
 //
-// returns 0 if successful, < 0 if an error occurrs
-int sdcard_set_callback (subscribe_upcall callback, void* callback_args);
+// - `arg1` (`returncode_t`): Status of the init operation.
+// - `arg2` (`uint32_t`): block_size, block size of SD card in bytes.
+// - `arg3` (`uint32_t`): size_in_kB, total size of SD card in kilobytes.
+typedef void (*libtock_sdcard_callback_initialized)(returncode_t, uint32_t, uint32_t);
 
-// set a buffer that data read from the SD card will be placed into
-// this buffer can be reused across multiple read calls
+// Function signature for SD card general callback.
 //
-// returns 0 if successful, < 0 if an error occurrs
-int sdcard_set_read_buffer (uint8_t* buffer, uint32_t len);
+// - `arg1` (`returncode_t`): Status of the init operation.
+typedef void (*libtock_sdcard_callback_operations)(returncode_t);
 
-// set a buffer containing data to write to the SD card
-// this buffer can be reused across multiple write calls by changing its
-// contents. No need to call this function a second time
+// Initialize an SD card asynchronously.
 //
-// returns 0 if successful, < 0 if an error occurrs
-int sdcard_set_write_buffer (uint8_t* buffer, uint32_t len);
+// Note that for a newly powered-on SD card, initialization can take over 100 ms
+// to complete. Expects a callback to have already been set. Callback will be
+// called when either initialization is complete or an error occurs.
+returncode_t libtock_sdcard_initialize(libtock_sdcard_callback_initialized cb);
 
-// check if an SD card is installed
-// Completes synchronously
+// Read a single block from an SD card asynchronously.
 //
-// returns 1 if installed, 0 if not installed, < 0 if an error occurrs
-int sdcard_is_installed (void);
-
-// initialize an SD card asynchronously
-// Note that for a newly powered-on SD card, initialization can take over
-// 100 ms to complete. Expects a callback to have already been set. Callback
-// will be called when either initialization is complete or an error occurs
-//
-// returns 0 if started successfully, < 0 if an error occurrs
-int sdcard_initialize (void);
-
-// initialize an SD card synchronously
-// Note that for a newly powered-on SD card, initialization can take over
-// 100 ms to complete
-//
-// block_size - set to the block size of the SD card, if not NULL
-// size_in_kB - set to the size of the SD card in kilobytes, if not NULL
-//
-// returns 0 if SD card is now initialized, < 0 if an error occurrs
-int sdcard_initialize_sync (uint32_t* block_size, uint32_t* size_in_kB);
-
-// read a single block from an SD card asynchronously
 // Expects a read_buffer and a callback to already have been set up. Callback
 // will be called when either the block has been read or an error occurs. When
-// the callback is successful, data will be in the read_buffer
+// the callback is successful, data will be in the read_buffer.
 //
-// sector - sector address of the block to be read
+// ## Arguments
 //
-// returns 0 if started successfully, < 0 if an error occurrs
-int sdcard_read_block (uint32_t sector);
+// - `sector`: sector address of the block to be read.
+// - `buffer`: buffer to read into.
+// - `len`: len of buffer.
+// - `cb`: callback.
+returncode_t libtock_sdcard_read_block(uint32_t sector, uint8_t* buffer, uint32_t len, libtock_sdcard_callback_operations cb);
 
-// read a single block from an SD card synchronously
-// Expects a read_buffer to already have been set up. When the command
-// completes successfully, data will be in the read_buffer
+// Write a single block to an SD card asynchronously.
 //
-// sector - sector address of the block to be read
-//
-// returns 0 if the block has been read, < 0 if an error occurrs
-int sdcard_read_block_sync (uint32_t sector);
-
-// write a single block to an SD card asynchronously
 // Expects a write_buffer and a callback to already have been set up. Data in
-// the write_buffer will be written to the SD card. Callback will be called
-// when either the block has been written or an error occurs
+// the write_buffer will be written to the SD card. Callback will be called when
+// either the block has been written or an error occurs.
 //
-// sector - sector address of the block to be written
+// ## Arguments
 //
-// returns 0 if started successfully, < 0 if an error occurrs
-int sdcard_write_block (uint32_t sector);
-
-// write a single block to an SD card synchronously
-// Expects a write_buffer to already have been set up. Data in the write_buffer
-// will be written to the SD card
-//
-// sector - sector address of the block to be written
-//
-// returns 0 if the block has been written, < 0 if an error occurrs
-int sdcard_write_block_sync (uint32_t sector);
+// - `sector`: sector address of the block to be read.
+// - `buffer`: buffer to write from.
+// - `len`: len of buffer.
+// - `cb`: callback.
+returncode_t libtock_sdcard_write_block(uint32_t sector, uint8_t* buffer, uint32_t len, libtock_sdcard_callback_operations cb);
 
 #ifdef __cplusplus
 }
