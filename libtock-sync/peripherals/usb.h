e9ecdd7d72d1f097d18cfe3fe963c19b14c8d836
Brad Campbell <bradjc5@gmail.com>
2024-03-23 10:22:13 -0400

    usb

---
+++ libtock-sync/peripherals/usb.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <tock.h>
+#include <peripherals/usb.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Enable and attach the USB and wait until the USB attaches.
+returncode_t libtocksync_usb_enable_and_attach(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
--- libtock/peripherals/usb.c
+++ libtock/peripherals/usb.c
@@ -1,47 +1,19 @@
 #include "usb.h"
 
-int usb_exists(void) {
-  return driver_exists(DRIVER_NUM_USB);
+static void usb_upcall(int                         status,
+                       __attribute__((unused)) int v1,
+                       __attribute__((unused)) int v2,
+                       void*                       opaque) {
+  libtock_usb_callback_attached cb = (libtock_usb_callback_attached) opaque;
+  cb(tock_status_to_returncode(status));
 }
 
-int usb_subscribe(subscribe_upcall upcall, void *ud) {
-  subscribe_return_t sval = subscribe(DRIVER_NUM_USB, 0, upcall, ud);
-  return tock_subscribe_return_to_returncode(sval);
-}
+returncode_t libtock_usb_enable_and_attach(libtock_usb_callback_attached cb) {
+  returncode_t err;
 
-int usb_enable_and_attach_async(void) {
-  syscall_return_t cval = command(DRIVER_NUM_USB, 1, 0, 0);
-  return tock_command_return_novalue_to_returncode(cval);
-}
+  err = libtock_usb_set_upcall(usb_upcall, cb);
+  if (err != RETURNCODE_SUCCESS) return err;
 
-struct data {
-  bool fired;
-  int rcode;
-};
-
-static void callback(int                         status,
-                     __attribute__((unused)) int v1,
-                     __attribute__((unused)) int v2,
-                     void *                      data)
-{
-  struct data *d = data;
-  d->fired = true;
-  d->rcode = tock_status_to_returncode(status);
-}
-
-int usb_enable_and_attach(void)
-{
-  int err;
-
-  struct data d = { .fired = false };
-
-  err = usb_subscribe(callback, (void *) &d);
-  if (err < 0) return err;
-
-  err = usb_enable_and_attach_async();
-  if (err < 0) return err;
-
-  yield_for(&d.fired);
-
-  return d.rcode;
+  err = libtock_usb_command_enable_and_attach();
+  return err;
 }
