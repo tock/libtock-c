29faeb205e6433383aef09c2b9d97fbed2cdf40d
Brad Campbell <bradjc5@gmail.com>
2024-03-23 10:53:51 -0400

    hmac

--- libtock/crypto/hmac.c
+++ libtock/crypto/hmac.c
@@ -1,54 +1,37 @@
 #include "hmac.h"
 
-#define DRIVER_NUM_HMAC 0x40003
-
-#define TOCK_HMAC_CB       0
-
-#define TOCK_HMAC_KEY_BUF      0
-#define TOCK_HMAC_DATA_BUF     1
-#define TOCK_HMAC_DEST_BUF     2
-
-#define TOCK_HMAC_SET_ALGORITHM   0
-#define TOCK_HMAC_RUN             1
-#define TOCK_HMAC_UPDATE          2
-#define TOCK_HMAC_FINISH          3
-
-int hmac_set_callback (subscribe_upcall callback, void* callback_args) {
-  subscribe_return_t sval = subscribe(DRIVER_NUM_HMAC, TOCK_HMAC_CB, callback, callback_args);
-  return tock_subscribe_return_to_returncode(sval);
+static void hmac_upcall(int ret,
+                        __attribute__ ((unused)) int unused1,
+                        __attribute__ ((unused)) int unused2, void* opaque) {
+  libtock_hmac_callback_hmac cb = (libtock_hmac_callback_hmac) opaque;
+  cb((returncode_t) ret);
 }
 
-int hmac_set_key_buffer(const uint8_t* buffer, uint32_t len) {
-  allow_ro_return_t aval = allow_readonly(DRIVER_NUM_HMAC, TOCK_HMAC_KEY_BUF, (void*) buffer, len);
-  return tock_allow_ro_return_to_returncode(aval);
-}
 
-int hmac_set_data_buffer(const uint8_t* buffer, uint32_t len) {
-  allow_ro_return_t aval = allow_readonly(DRIVER_NUM_HMAC, TOCK_HMAC_DATA_BUF, (void*) buffer, len);
-  return tock_allow_ro_return_to_returncode(aval);
-}
+returncode_t libtock_hmac_simple(libtock_hmac_algorithm_t hmac_type,
+                                 uint8_t* key_buffer, uint32_t key_length,
+                                 uint8_t* input_buffer, uint32_t input_length,
+                                 uint8_t* hash_buffer, uint32_t hash_length,
+                                 libtock_hmac_callback_hmac cb) {
 
-int hmac_set_dest_buffer(uint8_t* buffer, uint32_t len) {
-  allow_rw_return_t aval = allow_readwrite(DRIVER_NUM_HMAC, TOCK_HMAC_DEST_BUF, (void*) buffer, len);
-  return tock_allow_rw_return_to_returncode(aval);
-}
+  returncode_t ret;
 
-int hmac_set_algorithm(uint8_t hash) {
-  syscall_return_t cval = command(DRIVER_NUM_HMAC, TOCK_HMAC_SET_ALGORITHM, hash, 0);
-  return tock_command_return_novalue_to_returncode(cval);
-}
+  ret = libtock_hmac_command_set_algorithm((uint32_t) hmac_type);
+  if (ret != RETURNCODE_SUCCESS) return ret;
 
-int hmac_run(void) {
-  syscall_return_t cval = command(DRIVER_NUM_HMAC, TOCK_HMAC_RUN, 0, 0);
-  return tock_command_return_novalue_to_returncode(cval);
-}
+  ret = libtock_hmac_set_readonly_allow_key_buffer(key_buffer, key_length);
+  if (ret != RETURNCODE_SUCCESS) return ret;
 
-int hmac_update(void) {
-  syscall_return_t cval = command(DRIVER_NUM_HMAC, TOCK_HMAC_UPDATE, 0, 0);
-  return tock_command_return_novalue_to_returncode(cval);
-}
+  ret = libtock_hmac_set_readonly_allow_data_buffer(input_buffer, input_length);
+  if (ret != RETURNCODE_SUCCESS) return ret;
 
-int hmac_finish(void) {
-  syscall_return_t cval = command(DRIVER_NUM_HMAC, TOCK_HMAC_FINISH, 0, 0);
-  return tock_command_return_novalue_to_returncode(cval);
+  ret = libtock_hmac_set_readwrite_allow_destination_buffer(hash_buffer, hash_length);
+  if (ret != RETURNCODE_SUCCESS) return ret;
+
+  ret = libtock_hmac_set_upcall(hmac_upcall, cb);
+  if (ret != RETURNCODE_SUCCESS) return ret;
+
+  ret = libtock_hmac_command_run();
+  return ret;
+
 }
