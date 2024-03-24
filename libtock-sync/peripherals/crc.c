2e35c00683ceee44ba2ea8fa42ffa7b6f3fb9774
Brad Campbell <bradjc5@gmail.com>
2024-03-23 20:21:01 -0400

    crc

---
+++ libtock-sync/peripherals/crc.c
@@ -0,0 +1,33 @@
+#include "crc.h"
+
+struct crc_data {
+  bool fired;
+  int status;
+  uint32_t crc;
+};
+
+static struct crc_data result = {.fired = false};
+
+static void crc_callback(returncode_t ret, uint32_t crc) {
+  result.fired  = true;
+  result.status = ret;
+  result.crc    = crc;
+}
+
+returncode_t libtocksync_crc_compute(const uint8_t* buf, size_t buflen, libtock_crc_alg_t algorithm, uint32_t* crc) {
+  returncode_t ret;
+  result.fired = false;
+
+  ret = libtock_crc_compute(buf, buflen, algorithm, crc_callback);
+  if (ret != RETURNCODE_SUCCESS) return ret;
+
+  yield_for(&result.fired);
+  if (result.status != RETURNCODE_SUCCESS) return result.status;
+
+  ret = libtock_crc_set_readonly_allow(NULL, 0);
+  if (ret != RETURNCODE_SUCCESS) return ret;
+
+  *crc = result.crc;
+
+  return RETURNCODE_SUCCESS;
+}
