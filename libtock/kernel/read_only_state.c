955106009be6e2d7277ac02991ae94f7295b95c6
Brad Campbell <bradjc5@gmail.com>
2024-03-25 21:50:59 -0400

    read_only_state

--- libtock/kernel/read_only_state.c
+++ libtock/kernel/read_only_state.c
@@ -1,41 +1,21 @@
 #include "read_only_state.h"
 
-bool read_only_state_exists(void) {
-  return driver_exists(READ_ONLY_STATEDRIVER_NUM);
-}
 
-int read_only_state_get_version(void) {
-  syscall_return_t res = command(READ_ONLY_STATEDRIVER_NUM, 1, 0, 0);
-
-  if (res.type != TOCK_SYSCALL_SUCCESS_U32) {
-    return RETURNCODE_ENOSUPPORT;
-  } else {
-    return res.data[0];
-  }
-}
-
-int read_only_state_allocate_region(void* base, int len) {
-  allow_userspace_r_return_t buf;
-
-  if (len < READ_ONLY_STATEBUFFER_LEN) {
+returncode_t libtock_read_only_state_allocate_region(uint8_t* base, int len) {
+  if (len < LIBTOCK_READ_ONLY_STATE_BUFFER_LEN) {
     // The buffer is not long enough
     return RETURNCODE_ESIZE;
   }
 
-  buf = allow_userspace_read(READ_ONLY_STATEDRIVER_NUM, 0, base, len);
-
-  if (!buf.success) {
-    return tock_status_to_returncode(buf.status);
-  }
-  return RETURNCODE_SUCCESS;
+  return libtock_read_only_state_set_userspace_read_allow_allocate_region(base, len);
 }
 
-uint32_t read_only_state_get_pending_tasks(void* base) {
+uint32_t libtock_read_only_state_get_pending_tasks(void* base) {
   uint32_t* ptr = base;
   return ptr[1];
 }
 
-uint64_t read_only_state_get_ticks(void* base) {
+uint64_t libtock_read_only_state_get_ticks(void* base) {
   uint32_t* ptr = base;
 
   // Start with the high bytes set to 0
@@ -53,17 +33,19 @@ uint64_t read_only_state_get_ticks(void* base) {
   return ((uint64_t)high << 32) | low;
 }
 
-int read_only_state_quick_yield(void* base) {
+returncode_t libtock_read_only_state_quick_yield(void* base, int* pending_tasks) {
   if (yield_check_tasks()) {
-    return 1;
+    *pending_tasks = 1;
+    return RETURNCODE_SUCCESS;
   } else {
-    uint32_t tasks = read_only_state_get_pending_tasks(base);
+    uint32_t tasks = libtock_read_only_state_get_pending_tasks(base);
 
     if (tasks > 0) {
       // Waiting tasks, call yield
       yield();
     }
 
-    return tasks;
+    *pending_tasks = tasks;
+    return RETURNCODE_SUCCESS;
   }
 }
