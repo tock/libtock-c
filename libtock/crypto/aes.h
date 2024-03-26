4d84021352e5d6d2f5e9f8e667434daaa8163169
Brad Campbell <bradjc5@gmail.com>
2024-03-25 22:34:46 -0400

    aes

--- libtock/crypto/aes.h
+++ libtock/crypto/aes.h
@@ -1,103 +1,67 @@
 #pragma once
 
-#include "tock.h"
+#include "../tock.h"
+#include "syscalls/aes_syscalls.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int aes_set_callback(subscribe_upcall callback, void* callback_args);
+typedef enum {
+  LIBTOCK_AES128Ctr = 0,
+  LIBTOCK_AES128CBC = 1,
+  LIBTOCK_AES128ECB = 2,
+  LIBTOCK_AES128CCM = 3
+} libtock_aes_algorithm_t;
 
-int aes_set_key_buffer(const uint8_t* buffer, uint32_t len);
-/*
- * Contains the IV buffer, if applicable.
- */
-int aes_set_iv_buffer(const uint8_t* buffer, uint32_t len);
+// Set the AES algorithm
+// operation:
+//    * 0 -> AES128Ctr
+//    * 1 -> AES128CBC
+//    * 2 -> AES128ECB
+//    * 3 -> AES128CCM
+// encrypting:
+//    * true -> Encrypt the source data
+//    * false -> Decrypt the source data
+returncode_t libtock_aes_set_algorithm(libtock_aes_algorithm_t operation, bool encrypting);
 
-/*
- * Contains the nonce for CCM buffer.
- */
-int aes_set_nonce_buffer(const uint8_t* buffer, uint32_t len);
+// Setup the platform and run the first encryption. This must be called before
+// `aes_crypt()` and `aes_finish()` but must be called after
+// `aes_set_algorithm()` and all relevant buffers are set up.
+//
+// This will load in the specified key, IV and then run the
+// encryption/decryption operation on the data in the source buffer and save the
+// output in the destination buffer.
+//
+// This will trigger a callback.
+returncode_t libtock_aes_setup(void);
 
-/*
- * Contains the source buffer.
- * If doing a AES CCM operation this sets the mlen value to `len`.
- */
-int aes_set_source_buffer(const uint8_t* buffer, uint32_t len);
-int aes_set_dest_buffer(uint8_t* buffer, uint32_t len);
+// This must be called after aes_setup(). It will run the encryption/decryption
+// operation on the source data and store it in the destination buffer, based on
+// the key and IV that was loaded by `aes_setup()`.
+//
+// Between a call of `aes_setup()` and `aes_finish()` this can be called any
+// number of times (from 0 to infinite).
+//
+// Each call to this will trigger a callback.
+returncode_t libtock_aes_crypt(void);
 
-/*
- * Check that the AES system exists
- */
-int aes_check_status(void);
+// This must be called when the operation is complete. This indicates that the
+// app has finished the operation. Any future operations need to call
+// `aes_set_algorithm()` and `aes_setup()` as the key and IV will be cleared.
+returncode_t libtock_aes_finish(void);
 
-/*
- * Set the AES algorithm
- * operation:
- *    * 0 -> AES128Ctr
- *    * 1 -> AES128CBC
- *    * 2 -> AES128ECB
- *    * 3 -> AES128CCM
- * encrypting:
- *    * true -> Encrypt the source data
- *    * false -> Decrypt the source data
- */
-int aes_set_algorithm(uint8_t operation, bool encrypting);
+// This sets the A offset for AES CCM.
+returncode_t libtock_aes_ccm_set_a_off(uint32_t value);
 
-/*
- * Setup the platform and run the first encryption.
- * This must be called before `aes_crypt()` and `aes_finish()`
- * but must be called after `aes_set_algorithm()` and all relevent
- * buffers are set up.
- *
- * This will load in the specified key, IV and then run the
- * encryption/decryption operation on the data in the source
- * buffer and save the output in the destination buffer.
- *
- * This will trigger a callback.
- *
- */
-int aes_setup(void);
+// This sets the M offset for AES CCM.
+returncode_t libtock_aes_ccm_set_m_off(uint32_t value);
 
-/*
- * This must be called after aes_setup(). It will run the
- * encryption/decryption operation on the source data and store
- * it in the destination buffer, based on thte key and IV that was
- * loaded by `aes_setup()`.
- *
- * Between a call of `aes_setup()` and `aes_finish()` this can be called
- * any number of times (from 0 to infinite).
- *
- * Each call to this will trigger a callback
- */
-int aes_crypt(void);
+// This sets the mic length for AES CCM.
+returncode_t libtock_aes_ccm_set_mic_len(uint32_t value);
 
-/*
- * This must be called when the operation is complete. This indicates that the
- * app has finished the operation. Any future operations need to call
- * `aes_set_algorithm()` and `aes_setup()` as the key and IV will be cleared.
- */
-int aes_finish(void);
-
-/*
- * This sets the A offset for AES CCM.
- */
-int aes_ccm_set_a_off(uint32_t value);
-
-/*
- * This sets the M offset for AES CCM.
- */
-int aes_ccm_set_m_off(uint32_t value);
-
-/*
- * This sets the mic length for AES CCM.
- */
-int aes_ccm_set_mic_len(uint32_t value);
-
-/*
- * This sets the confidential bool for AES CCM.
- */
-int aes_ccm_set_confidential(bool value);
+// This sets the confidential bool for AES CCM.
+returncode_t libtock_aes_ccm_set_confidential(bool value);
 
 #ifdef __cplusplus
 }
