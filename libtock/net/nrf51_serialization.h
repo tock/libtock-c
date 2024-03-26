c9cc32c15b5d64c8f6a1d45f5188bb6d3cf1c251
Brad Campbell <bradjc5@gmail.com>
2024-03-25 22:14:07 -0400

    nrf51_serialization

--- libtock/net/nrf51_serialization.h
+++ libtock/net/nrf51_serialization.h
@@ -1,35 +1,22 @@
 #pragma once
 
-#include "tock.h"
+#include "../tock.h"
+#include "syscalls/nrf51_serialization_syscalls.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define DRIVER_NUM_NRF_SERIALIZATION 0x80004
-
 // Toggle the reset line to the nRF51 chip to reset the BLE MCU.
-__attribute__ ((warn_unused_result))
-int nrf51_serialization_reset (void);
-
-// Give the BLE Serialization / UART layer a callback to call when
-// a packet is received and when a TX is finished.
-__attribute__ ((warn_unused_result))
-int nrf51_serialization_subscribe (subscribe_upcall cb);
-
-// Pass a buffer for the driver to write received UART bytes to.
-__attribute__ ((warn_unused_result))
-int nrf51_serialization_setup_receive_buffer (char* rx, int rx_len);
+returncode_t libtock_nrf51_serialization_reset(void);
 
 // Write a packet to the BLE Serialization connectivity processor.
-__attribute__ ((warn_unused_result))
-int nrf51_serialization_write(char* tx, int tx_len);
+returncode_t libtock_nrf51_serialization_write(uint8_t* tx, int tx_len);
 
-// Receive into the buffer passed in to `setup_receive_buffer` previously
-__attribute__ ((warn_unused_result))
-int nrf51_serialization_read(int rx_len);
+// Receive into the buffer passed in to
+// `libtock_nrf51_serialization_set_readwrite_allow_receive_buffer` previously.
+returncode_t libtock_nrf51_serialization_read(int rx_len, int* bytes_read);
 
-  
 #ifdef __cplusplus
 }
 #endif
