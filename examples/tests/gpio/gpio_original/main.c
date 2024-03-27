c08990abc60e63d742aec50b15bec687e37c2865
Brad Campbell <bradjc5@gmail.com>
2024-03-26 23:52:37 -0400

    test: gpio

--- examples/tests/gpio/gpio_original/main.c
+++ examples/tests/gpio/gpio_original/main.c
@@ -15,20 +15,24 @@
 #include <string.h>
 #include <unistd.h>
 
-#include <console.h>
-#include <gpio.h>
-#include <led.h>
-#include <timer.h>
-#include <tock.h>
+#include <libtock-sync/services/alarm.h>
+#include <libtock/interface/console.h>
+#include <libtock/interface/led.h>
+#include <libtock/peripherals/gpio.h>
+#include <libtock/tock.h>
 
-int callback_count = 0;
-// callback for timers
-static void timer_cb (__attribute__ ((unused)) int   arg0,
-                      __attribute__ ((unused)) int   arg1,
-                      __attribute__ ((unused)) int   arg2,
-                      __attribute__ ((unused)) void* userdata) {
-  callback_count     = callback_count + 1;
-  *((bool*)userdata) = 1;
+struct alarm_cb_data {
+  bool fired;
+  int callback_count;
+};
+
+static struct alarm_cb_data data = { .fired = false, .callback_count = 0 };
+
+static void alarm_cb(__attribute__ ((unused)) uint32_t now,
+                     __attribute__ ((unused)) uint32_t scheduled,
+                     __attribute__ ((unused)) void*    opaque) {
+  data.callback_count = data.callback_count + 1;
+  data.fired = true;
 }
 
 // **************************************************
@@ -37,16 +41,16 @@ static void timer_cb (__attribute__ ((unused)) int   arg0,
 static void gpio_output(void) {
   printf("Periodically toggling pin\n");
 
-  gpio_enable_output(0);
-  // Start repeating timer
-  static bool resume = 0;
-  tock_timer_t timer;
-  timer_every(1000, timer_cb, &resume, &timer);
+  libtock_gpio_enable_output(0);
+  // Start repeating alarm
+  data.fired = false;
+  libtock_alarm_repeating_t alarm_repeating;
+  libtock_alarm_repeating_every(1000, alarm_cb, NULL, &alarm_repeating);
 
   while (1) {
-    yield_for(&resume);
-    resume = 0;
-    gpio_toggle(0);
+    yield_for(&data.fired);
+    data.fired = false;
+    libtock_gpio_toggle(0);
   }
 }
 
@@ -57,31 +61,30 @@ static void gpio_input(void) {
   printf("Periodically reading value of the GPIO 0 pin\n");
   printf("Jump pin high to test (defaults to low)\n");
 
-  // set userspace pin 0 as input and start repeating timer
+  // set userspace pin 0 as input and start repeating alarm
   // pin is configured with a pull-down resistor, so it should read 0 as default
-  gpio_enable_input(0, PullDown);
-  tock_timer_t timer;
-  static bool resume = 0;
-  timer_every(500, timer_cb, &resume, &timer);
+  libtock_gpio_enable_input(0, libtock_pull_down);
+  libtock_alarm_repeating_t alarm_repeating;
+  data.fired = false;
+  libtock_alarm_repeating_every(500, alarm_cb, NULL, &alarm_repeating);
 
   while (1) {
     // print pin value
     int pin_val;
-    gpio_read(0, &pin_val);
+    libtock_gpio_read(0, &pin_val);
     printf("\tValue(%d)\n", pin_val);
-    yield_for(&resume);
-    resume = 0;
+    yield_for(&data.fired);
+    data.fired = 0;
   }
 }
 
 // **************************************************
 // GPIO interrupt example
 // **************************************************
-static void gpio_cb (__attribute__ ((unused)) int pin_num,
-                     __attribute__ ((unused)) int arg2,
-                     __attribute__ ((unused)) int arg3,
-                     void*                        userdata) {
-  *((bool*) userdata) = true;
+static bool gpio_interrupt_fired = false;
+static void gpio_cb (__attribute__ ((unused)) uint32_t pin_num,
+                     __attribute__ ((unused)) bool     arg2) {
+  gpio_interrupt_fired = true;
 }
 
 static void gpio_interrupt(void) {
@@ -89,16 +92,16 @@ static void gpio_interrupt(void) {
   printf("Jump pin high to test\n");
 
   // set callback for GPIO interrupts
-  static bool resume = 0;
-  gpio_interrupt_callback(gpio_cb, &resume);
+  libtock_gpio_set_interrupt_callback(gpio_cb);
 
   // set userspace pin 0 as input and enable interrupts on it
-  gpio_enable_input(0, PullDown);
-  gpio_enable_interrupt(0, Change);
+  libtock_gpio_enable_input(0, libtock_pull_down);
+  libtock_gpio_enable_interrupt(0, libtock_change);
 
+  gpio_interrupt_fired = false;
   while (1) {
-    yield_for(&resume);
-    resume = 0;
+    yield_for(&gpio_interrupt_fired);
+    gpio_interrupt_fired = false;
     printf("\tGPIO Interrupt!\n");
   }
 }
