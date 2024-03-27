46218d3761bd19af181db79a7ae1110491a4a2b6
Brad Campbell <bradjc5@gmail.com>
2024-03-26 23:52:37 -0400

    test: gpio

---
+++ examples/tests/gpio/gpio_interrupt/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#include <console.h>
+#include <libtock/peripherals/gpio.h>
+#include <libtock/tock.h>
+
+static void gpio_cb (__attribute__ ((unused)) uint32_t   pin_num,
+                     __attribute__ ((unused)) bool   val) {}
+
+int main(void) {
+  printf("[Test] GPIO Interrupt\n");
+  printf("Jump GPIO pin 0 high to test.\n");
+
+  // set callback for GPIO interrupts
+  libtock_gpio_set_interrupt_callback(gpio_cb);
+
+  // set GPIO 0 as input and enable interrupts on it
+  libtock_gpio_enable_input(0, libtock_pull_down);
+  libtock_gpio_enable_interrupt(0, libtock_change);
+
+  while (1) {
+    yield();
+    printf("\tGPIO Interrupt! (App: %p)\n", tock_app_flash_begins_at());
+  }
+
+  return 0;
+}
