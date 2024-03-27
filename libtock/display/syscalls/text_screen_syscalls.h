41c95d46d962607c5ca7966b8e4bf434cd15b69a
Brad Campbell <bradjc5@gmail.com>
2024-03-26 21:56:20 -0400

    text_screen

---
+++ libtock/display/syscalls/text_screen_syscalls.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "tock.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define DRIVER_NUM_TEXT_SCREEN 0x90003
+
+returncode_t libtock_text_screen_set_upcall(subscribe_upcall callback, void *opaque);
+
+returncode_t libtock_text_screen_set_readonly_allow(const uint8_t* ptr, uint32_t size);
+
+returncode_t libtock_text_screen_command_get_size(void);
+
+returncode_t libtock_text_screen_command_on(void);
+
+returncode_t libtock_text_screen_command_off(void);
+
+returncode_t libtock_text_screen_command_blink_on(void);
+
+returncode_t libtock_text_screen_command_blink_off(void);
+
+returncode_t libtock_text_screen_command_show_cursor(void);
+
+returncode_t libtock_text_screen_command_hide_cursor(void);
+
+returncode_t libtock_text_screen_command_write(uint32_t len);
+
+returncode_t libtock_text_screen_command_clear(void);
+
+returncode_t libtock_text_screen_command_home(void);
+
+returncode_t libtock_text_screen_command_set_cursor(uint32_t col, uint32_t row);
+
+#ifdef __cplusplus
+}
+#endif
--- libtock/display/text_screen.h
+++ libtock/display/text_screen.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "tock.h"
+#include "syscalls/text_screen_syscalls.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -8,22 +9,40 @@ extern "C" {
 
 #define DRIVER_NUM_TEXT_SCREEN 0x90003
 
-int text_screen_init (size_t len);
-uint8_t* text_screen_buffer (void);
+// Callback when an operation has completed.
+//
+// This callback is used for several operations, but is generic as it only
+// returns the returncode for the operation as provided in the upcall.
+typedef void (*libtock_text_screen_callback_done)(returncode_t);
 
-int text_screen_display_on (void);
-int text_screen_display_off (void);
-int text_screen_blink_on (void);
-int text_screen_blink_off (void);
-int text_screen_show_cursor (void);
-int text_screen_hide_cursor (void);
-int text_screen_clear (void);
-int text_screen_home (void);
+// Callback with size.
+//
+// - `arg1` (`returncode_t`): Status of getting the size.
+// - `arg2` (`uint32_t`): Width of display.
+// - `arg3` (`uint32_t`): Height of display.
+typedef void (*libtock_text_screen_callback_size)(returncode_t, uint32_t, uint32_t);
 
-int text_screen_get_size (size_t* width, size_t* height);
+returncode_t libtock_text_screen_display_on (libtock_text_screen_callback_done cb);
 
-int text_screen_set_cursor (uint8_t col, uint8_t row);
-int text_screen_write (size_t len);
+returncode_t libtock_text_screen_display_off (libtock_text_screen_callback_done cb);
+
+returncode_t libtock_text_screen_blink_on (libtock_text_screen_callback_done cb);
+
+returncode_t libtock_text_screen_blink_off (libtock_text_screen_callback_done cb);
+
+returncode_t libtock_text_screen_show_cursor (libtock_text_screen_callback_done cb);
+
+returncode_t libtock_text_screen_hide_cursor (libtock_text_screen_callback_done cb);
+
+returncode_t libtock_text_screen_clear (libtock_text_screen_callback_done cb);
+
+returncode_t libtock_text_screen_home (libtock_text_screen_callback_done cb);
+
+returncode_t libtock_text_screen_set_cursor (uint8_t col, uint8_t row, libtock_text_screen_callback_done cb);
+
+returncode_t libtock_text_screen_write (uint8_t* buffer, uint32_t buffer_len, uint32_t write_len, libtock_text_screen_callback_done cb);
+
+returncode_t libtock_text_screen_get_size (libtock_text_screen_callback_size cb);
 
 #ifdef __cplusplus
 }
