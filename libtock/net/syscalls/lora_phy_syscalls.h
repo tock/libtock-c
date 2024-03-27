d148d8dc587b3257529c7c0b83f739a9a3e33295
Brad Campbell <bradjc5@gmail.com>
2024-03-26 22:50:26 -0400

    lora phy

--- libtock/net/syscalls/lora_phy_syscalls.h
+++ libtock/net/syscalls/lora_phy_syscalls.h
@@ -6,16 +6,10 @@
 extern "C" {
 #endif
 
-
 #define DRIVER_NUM_LORA_PHY_SPI  0x30003
 #define DRIVER_NUM_LORA_PHY_GPIO 0x30004
 
-
-
-
 bool libtock_lora_phy_exists(void);
-
-
 
 returncode_t libtock_lora_phy_set_upcall_spi(subscribe_upcall callback, void* opaque);
 
@@ -24,8 +18,6 @@ returncode_t libtock_lora_phy_set_readonly_allow_master_write_buffer(const uint8
 returncode_t libtock_lora_phy_set_readwrite_allow_master_read_buffer(uint8_t* buffer, uint32_t len);
 
 returncode_t libtock_lora_phy_command_read_write(uint32_t length);
-
-
 
 returncode_t libtock_lora_phy_gpio_command_enable_output(uint32_t pin);
 
@@ -45,10 +37,6 @@ returncode_t libtock_lora_phy_gpio_command_disable_interrupt(uint32_t pin);
 
 returncode_t libtock_lora_phy_gpio_command_interrupt_callback(subscribe_upcall callback, void* opaque);
 
-
-
-
 #ifdef __cplusplus
 }
 #endif
-
