bd0bc9509ad0ca1a4e703c9a01c3270149caab9a
Brad Campbell <bradjc5@gmail.com>
2024-03-26 21:56:20 -0400

    text_screen

---
+++ libtock-sync/display/text_screen.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <libtock/tock.h>
+#include <libtock/display/text_screen.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+returncode_t libtocksync_text_screen_display_on(void);
+
+returncode_t libtocksync_text_screen_display_off(void);
+
+returncode_t libtocksync_text_screen_blink_on(void);
+
+returncode_t libtocksync_text_screen_blink_off(void);
+
+returncode_t libtocksync_text_screen_show_cursor(void);
+
+returncode_t libtocksync_text_screen_hide_cursor(void);
+
+returncode_t libtocksync_text_screen_clear(void);
+
+returncode_t libtocksync_text_screen_home(void);
+
+returncode_t libtocksync_text_screen_set_cursor(uint8_t col, uint8_t row);
+
+returncode_t libtocksync_text_screen_write(uint8_t* buffer, uint32_t buffer_len, uint32_t write_len);
+
+returncode_t libtocksync_text_screen_get_size(uint32_t* width, uint32_t* height);
+
+#ifdef __cplusplus
+}
+#endif
