8ba5955aed30427e2e61b0a8680a3e5fc5939e70
Brad Campbell <bradjc5@gmail.com>
2024-03-27 22:39:21 -0400

    test: text_scren

--- examples/tests/text_screen/main.c
+++ examples/tests/text_screen/main.c
@@ -1,31 +1,29 @@
-/*
-    Example for text_screen library
-    Tested on NUCLEO-F429ZI
- */
-
-#include <screen.h>
+#include <stdlib.h>
 #include <string.h>
-#include <text_screen.h>
+
+#include <libtock-sync/display/text_screen.h>
 #include <timer.h>
 #include <tock.h>
 
 int main(void) {
-  int ret         = text_screen_init(15);
-  uint8_t *buffer = text_screen_buffer();
-  ret = text_screen_display_on();
+  returncode_t ret;
+
+  uint8_t* buffer = calloc(15, 1);
+
+  ret = libtocksync_text_screen_display_on();
   if (ret == RETURNCODE_SUCCESS) {
-    ret = text_screen_set_cursor(0, 0);
+    ret = libtocksync_text_screen_set_cursor(0, 0);
     memcpy(buffer, "Hello!", 6);
-    ret = text_screen_write(6);
+    ret = libtocksync_text_screen_write(buffer, 15, 6);
 
     delay_ms(5000);
 
-    ret = text_screen_set_cursor(0, 1);
+    ret = libtocksync_text_screen_set_cursor(0, 1);
     memcpy(buffer, "Goodbyee!", 9);
-    ret = text_screen_write(9);
+    ret = libtocksync_text_screen_write(buffer, 15, 9);
 
     delay_ms(2000);
-    ret = text_screen_clear();
+    ret = libtocksync_text_screen_clear();
   }
   return 0;
 }
