becbef990516df6bbc006d47f5fe05794efe7b59
viswajith-g <gyx4bw@virginia.edu>
2024-03-28 20:23:40 -0400

    rebase and add examples/test/app_loader. New app binaries are located in internal/binaries.c.
    cleaned up and formatted
    
    removed old code
    
    removed userspace code from a previous implementation that was giving rise to confusion.
    
    added comments to app_loader.h
    
    added binaries headers and updated code
    
    added binaries in external files to make the app file more readable. Added a new subscribe and callback to support the async setup
    
    update readme

--- libtock/internal/app_loader.c
+++ libtock/internal/app_loader.c
@@ -1,15 +1,17 @@
-#include <stdio.h>
-#include <string.h>
-
-#include "tock.h"
 #include "internal/app_loader.h"
+#include "tock.h"
 
 int app_loader_exists(void) {
   syscall_return_t res = command(DRIVER_NUM_APP_LOADER, 0, 0, 0);
   return tock_command_return_novalue_to_returncode(res);
 }
 
-int app_loader_subscribe(subscribe_upcall cb, void *userdata) {
+int app_loader_setup_subscribe(subscribe_upcall cb, void *userdata) {
+  subscribe_return_t sval = subscribe(DRIVER_NUM_APP_LOADER, 0, cb, userdata);
+  return tock_subscribe_return_to_returncode(sval);
+}
+
+int app_loader_write_subscribe(subscribe_upcall cb, void *userdata) {
   subscribe_return_t sval = subscribe(DRIVER_NUM_APP_LOADER, 1, cb, userdata);
   return tock_subscribe_return_to_returncode(sval);
 }
@@ -20,7 +22,7 @@ int app_loader_write_buffer(uint8_t *buffer, uint32_t len) {
 }
 
 int app_loader_command_setup(uint32_t app_length) {
-  syscall_return_t res = command(DRIVER_NUM_APP_LOADER, 1, app_length, 0); 
+  syscall_return_t res = command(DRIVER_NUM_APP_LOADER, 1, app_length, 0);
   return tock_command_return_novalue_to_returncode(res);
 }
 
@@ -32,29 +34,4 @@ int app_loader_command_write(uint32_t flash_offset, uint32_t write_length) {
 int app_loader_command_load(void) {
   syscall_return_t res = command(DRIVER_NUM_APP_LOADER, 3, 0, 0);
   return tock_command_return_novalue_to_returncode(res);
-}
-
-int button_subscribe(subscribe_upcall callback, void *ud) {
-  subscribe_return_t res = subscribe(DRIVER_NUM_BUTTON, 0, callback, ud);
-  return tock_subscribe_return_to_returncode(res);
-}
-
-int button_count(int* count) {
-  syscall_return_t res = command(DRIVER_NUM_BUTTON, 0, 0, 0);
-  return tock_command_return_u32_to_returncode(res, (uint32_t*) count);
-}
-
-int button_enable_interrupt(int button_num) {
-  syscall_return_t res = command(DRIVER_NUM_BUTTON, 1, button_num, 0);
-  return tock_command_return_novalue_to_returncode(res);
-}
-
-int button_disable_interrupt(int button_num) {
-  syscall_return_t res = command(DRIVER_NUM_BUTTON, 2, button_num, 0);
-  return tock_command_return_novalue_to_returncode(res);
-}
-
-int button_read(int button_num, int* button_value) {
-  syscall_return_t res = command(DRIVER_NUM_BUTTON, 3, button_num, 0);
-  return tock_command_return_u32_to_returncode(res, (uint32_t*) button_value);
 }
