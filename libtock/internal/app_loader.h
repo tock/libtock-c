200b8749465a5c3e514be18616ab56b32881ecc3
viswajith-g <gyx4bw@virginia.edu>
2024-03-28 20:23:40 -0400

    rebase and add examples/test/app_loader. New app binaries are located in internal/binaries.c.
    cleaned up and formatted
    
    removed old code
    
    removed userspace code from a previous implementation that was giving rise to confusion.
    
    added comments to app_loader.h
    
    added binaries headers and updated code
    
    added binaries in external files to make the app file more readable. Added a new subscribe and callback to support the async setup
    
    update readme

--- libtock/internal/app_loader.h
+++ libtock/internal/app_loader.h
@@ -8,31 +8,62 @@ extern "C"
 #include "tock.h"
 
 // Driver number for system call
-#define DRIVER_NUM_APP_LOADER   0x10001
-#define DRIVER_NUM_BUTTON       0x3
+#define DRIVER_NUM_APP_LOADER   0x50004
 
-#define BUTTON_1 0
-#define BUTTON_2 1
-#define BUTTON_3 2
-#define BUTTON_4 3
+#define BUTTON1 0
+#define BUTTON2 1
 
-#define BUTTON_PRESSED 1
-
+/*
+ * Command to check if the `app_loader` capsule exists.
+ */
 int app_loader_exists(void);
-int app_loader_subscribe(subscribe_upcall cb, void *userdata);
-int app_loader_write_buffer(uint8_t *buffer, uint32_t len);
-int app_loader_command_setup(uint32_t app_length);
-int app_loader_command_write(uint32_t flash_offset, uint32_t write_length);
-int app_loader_command_load(void);
-int app_loader_callback_subscribe_init(void);
-int write_app(void);
 
-int button_init(void);
-int button_subscribe(subscribe_upcall callback, void *ud);
-int button_enable_interrupt(int button_num);
-int button_disable_interrupt(int button_num);
-int button_read(int button_num, int* button_value);
-int button_count(int* count);
+/*
+ * Function to setup the callback from capsule.
+ * This function takes in the function that will be executed
+ * when the callback is triggered.
+ */
+int app_loader_setup_subscribe(subscribe_upcall cb, void *userdata);
+
+/*
+ * Command to request the kernel to set up for a new app.
+ * This functions takes the size of the new app as the argument. 
+ */
+int app_loader_command_setup(uint32_t app_length);
+
+/*
+ * Function to setup the callback from capsule.
+ * This function takes in the function that will be executed
+ * when the callback is triggered.
+ */
+int app_loader_write_subscribe(subscribe_upcall cb, void *userdata);
+
+/*
+ * Function to set up a shared buffer with the capsule.
+ * This buffer is used to write data to the flash. 
+ */
+int app_loader_write_buffer(uint8_t *buffer, uint32_t len);
+
+/*
+ * Command to write new app binary to the capsule.
+ * Specify the offset at which the data segment should be written.
+ * Also specify how long the data segment will be in bytes.
+ * 
+ * After writing data, the userspace app should yield for 
+ * the write done callback from the capsule.
+ */
+int app_loader_command_write(uint32_t flash_offset, uint32_t write_length);
+
+/*
+ * Command to request the kernel to load the newly flashed app. 
+ */
+int app_loader_command_load(void);
+
+/*
+ * Internal function to write new app to flash. Takes app size as argument.
+ */
+int write_app(double size, uint8_t binary[]);
+
 
 #ifdef __cplusplus
 }
---
+++ tools/dynamic_app_loader/tab_to_binary_array.py
@@ -0,0 +1,30 @@
+import os
+
+def read_binary_file(file_name):
+    parent_directory = os.path.dirname(os.getcwd())
+    grandparent_directory = os.path.dirname(parent_directory)
+    file_path = os.path.join(grandparent_directory, file_name)
+    with open(file_path, 'rb') as file:
+        binary_data = file.read()
+    return binary_data
+
+def binary_to_array(binary_data):
+    hex_array = [hex(byte) for byte in binary_data]
+    return hex_array
+
+def print_hex_array(hex_array):
+    print("Hexadecimal Array:")
+    count = 0
+    for hex_byte in hex_array:
+        print(f"0x{hex_byte[2:].zfill(2)}", end=", ")  # Add prefix '0x' and leading zero padding
+        count += 1
+        if count % 14 == 0:
+            print()  # Add a new line after every 14 elements
+    if count % 14 != 0:
+        print()  # Add a new line if the last line is not complete
+
+
+file_name = 'examples/adc/build/cortex-m4/cortex-m4.tbf'
+binary_data = read_binary_file(file_name)
+hex_array = binary_to_array(binary_data)
+print_hex_array(hex_array)
