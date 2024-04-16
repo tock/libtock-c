c8c93fa8aea972a15b106fa9816b8a0f80a2e52e
Brad Campbell <bradjc5@gmail.com>
2024-04-16 12:31:47 -0400

    temperature

---
+++ libtock/sensors/syscalls/temperature_syscalls.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "../../tock.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define DRIVER_NUM_TEMPERATURE 0x60000
+
+// Check if temperature sensor exists.
+bool libtock_temperature_exists(void);
+
+// units: temperature in hundredths of degrees centigrade.
+
+// Set the callback function to be called when the temperature measurement is
+// finished.
+//
+// callback  - pointer to function to be called
+// opaque    - pointer to data provided to the callback
+returncode_t libtock_temperature_set_upcall(subscribe_upcall callback, void* opaque);
+
+// Initiate an ambient temperature measurement.
+returncode_t libtock_temperature_command_read(void);
+
+#ifdef __cplusplus
+}
+#endif
