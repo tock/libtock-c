61a449091bd98750fff42505a11e114a866613e7
Brad Campbell <bradjc5@gmail.com>
2024-04-16 13:05:47 -0400

    screen

--- libtock/display/screen.h
+++ libtock/display/screen.h
@@ -1,21 +1,7 @@
-// Screen API
-
 #pragma once
 
 #include "tock.h"
-
-#define SCREEN_ROTATION_NORMAL 0
-#define SCREEN_ROTATION_90 1
-#define SCREEN_ROTATION_180 2
-#define SCREEN_ROTATION_270 3
-
-#define SCREEN_PIXEL_FORMAT_MONO 0
-#define SCREEN_PIXEL_FORMAT_RGB_233 1
-#define SCREEN_PIXEL_FORMAT_RGB_565 2
-#define SCREEN_PIXEL_FORMAT_RGB_888 3
-#define SCREEN_PIXEL_FORMAT_ARGB_8888 4
-
-#define SCREEN_PIXEL_FORMAT_ERROR -1
+#include "syscalls/screen_syscalls.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -23,43 +9,128 @@ extern "C" {
 
 #define DRIVER_NUM_SCREEN 0x90001
 
-// init
-int screen_init (size_t len);
-uint8_t * screen_buffer (void);
+// Supported pixel formats.
+typedef enum {
+  MONO=0,
+  RGB_233=1,
+  RGB_565=2,
+  RGB_888=3,
+  ARGB_8888=4,
+} libtock_screen_format_t;
 
-// syscalls
-int screen_subscribe (subscribe_upcall cb, void *userdata);
-int screen_allow (const void* ptr, size_t size);
+// Supported screen rotations.
+typedef enum {
+  ROTATION_NORMAL=0,
+  ROTATION_90=1,
+  ROTATION_180=2,
+  ROTATION_270=3,
+} libtock_screen_rotation_t;
 
-// query
-bool screen_setup_enabled (void);
-int screen_get_supported_resolutions (int* resolutions);
-int screen_get_supported_resolution (size_t index, size_t *width, size_t *height);
-int screen_get_supported_pixel_formats (int* formats);
-int screen_get_supported_pixel_format (size_t index, int* format);
+// Callback when an operation has completed.
+//
+// This callback is used for several operations, but is generic as it only
+// returns the returncode for the operation as provided in the upcall.
+typedef void (*libtock_screen_callback_done)(returncode_t);
 
-// power
-int screen_set_brightness (size_t brightness);
-int screen_invert_on (void);
-int screen_invert_off (void);
+// Callback when retrieving the screen pixel format.
+//
+// The callback is provided the screen format when called.
+typedef void (*libtock_screen_callback_format)(returncode_t, libtock_screen_format_t);
 
-// pixel format
-int screen_get_bits_per_pixel (size_t format);
-int screen_get_pixel_format (int* format);
-int screen_set_pixel_format (size_t format);
+// Callback when retrieving the screen rotation.
+//
+// The callback includes the rotation angle as an int.
+typedef void (*libtock_screen_callback_rotation)(returncode_t, libtock_screen_rotation_t);
 
-// resolution and rotation
-int screen_get_resolution (size_t *width, size_t *height);
-int screen_set_resolution (size_t width, size_t height);
 
-int screen_get_rotation (int* rotation);
-int screen_set_rotation (size_t rotation);
+// INIT
 
-// drawing
-int screen_set_color (size_t position, size_t color);
-int screen_set_frame (uint16_t x, uint16_t y, uint16_t width, uint16_t height);
-int screen_fill (size_t color);
-int screen_write (size_t length);
+// Allocate and setup `buffer` with size `len`.
+//
+// This will allocate len bytes and assign buffer to the array. This buffer can
+// be used for drawing the screen.
+statuscode_t libtock_screen_buffer_init(size_t len, uint8_t** buffer);
+
+// QUERY
+
+// Check if the screen is setup. Returns `true` if the screen is enabled, or
+// `false` otherwise.
+bool libtock_screen_setup_enabled(void);
+
+// Get the number of supported resolutions for the screen.
+returncode_t libtock_screen_get_supported_resolutions(uint32_t* resolutions);
+
+// Get a particular resolution based on `index`. `width` and `height` will be
+// set with the resolution corresponding to that index.
+returncode_t libtock_screen_get_supported_resolution(size_t index, uint32_t* width, uint32_t* height);
+
+// Get the number or supported pixel formats.
+returncode_t libtock_screen_get_supported_pixel_formats(uint32_t* formats);
+
+// Get the particular format based on `index`. `format` will be set with the
+// correct pixel format for that index.
+returncode_t libtock_screen_get_supported_pixel_format(size_t index, libtock_screen_format_t* format);
+
+// POWER
+
+// Set the brightness. The callback will be called when the brightness is set.
+returncode_t libtock_screen_set_brightness(uint32_t brightness, libtock_screen_callback_done cb);
+
+// Turn on color inversion. The callback will be called when the operation is
+// complete.
+returncode_t libtock_screen_invert_on(libtock_screen_callback_done cb);
+
+// Turn off color inversion. The callback will be called when the operation is
+// complete.
+returncode_t libtock_screen_invert_off(libtock_screen_callback_done cb);
+
+// PIXEL FORMAT
+
+// Calculate and return the number of bits per pixel for the given pixel format.
+int libtock_screen_get_bits_per_pixel(libtock_screen_format_t format);
+
+// Get the pixel format. The format will be returned in the callback.
+returncode_t libtock_screen_get_pixel_format(libtock_screen_callback_format cb);
+
+// Set the pixel format. The callback will be called when the format is set.
+returncode_t libtock_screen_set_pixel_format(libtock_screen_format_t format, libtock_screen_callback_done cb);
+
+// RESOLUTION AND ROTATION
+
+// Get the current resolution of the screen.
+returncode_t libtock_screen_get_resolution(uint32_t* width, uint32_t* height);
+
+// Set the resolution of the screen to the specified width and height. The
+// callback will be called when the resolution is set.
+returncode_t libtock_screen_set_resolution(uint32_t width, uint32_t height, libtock_screen_callback_done cb);
+
+// Get the current screen rotation. The rotation will be specified in the
+// callback function.
+returncode_t libtock_screen_get_rotation(libtock_screen_callback_rotation cb);
+
+// Set the screen rotation. The callback will be called when the operation
+// completes.
+returncode_t libtock_screen_set_rotation(libtock_screen_rotation_t rotation, libtock_screen_callback_done cb);
+
+// DRAWING
+
+// Set the current drawing frame on the screen.
+//
+// The frame is specified by the X,Y coordinates as the upper left, and the
+// width and height of the frame box. The callback will be called when setting
+// the frame is complete.
+returncode_t libtock_screen_set_frame(uint16_t x, uint16_t y, uint16_t width, uint16_t height, libtock_screen_callback_done cb);
+
+// Fill the screen to a given color.
+//
+// The screen data is in buffer. The callback will be called when the screen is
+// finished being filled.
+returncode_t libtock_screen_fill(uint8_t* buffer, int buffer_len, size_t color, libtock_screen_callback_done cb);
+
+// Write the data in buffer to the screen.
+//
+// The callback will be called when the screen is finished being updated.
+returncode_t libtock_screen_write(uint8_t* buffer, int buffer_len, size_t length, libtock_screen_callback_done cb);
 
 #ifdef __cplusplus
 }
---
+++ libtock/display/syscalls/screen_syscalls.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include "tock.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define DRIVER_NUM_SCREEN 0x90001
+
+// Check if the screen driver exists.
+bool libtock_screen_exists(void);
+
+// Set the upcall for screen upcalls.
+returncode_t libtock_screen_set_upcall(subscribe_upcall cb, void* opaque);
+
+// Allow a buffer used to write the screen.
+returncode_t libtock_screen_set_readonly_allow(const void* ptr, size_t size);
+
+// Get the number of supported resolutions.
+returncode_t libtock_screen_command_get_supported_resolutions(uint32_t* resolutions);
+
+// Get the supported resolution at the given index.
+returncode_t libtock_screen_command_get_supported_resolution(uint32_t index, uint32_t* width, uint32_t* height);
+
+// Get the current screen resolution.
+returncode_t libtock_screen_command_get_resolution(uint32_t* width, uint32_t* height);
+
+// Set the screen resolution.
+returncode_t libtock_screen_command_set_resolution(uint32_t width, uint32_t height);
+
+// Get the number of supported pixel formats.
+returncode_t libtock_screen_command_get_supported_pixel_formats(uint32_t* formats);
+
+// Get the specific supported pixel format.
+returncode_t libtock_screen_command_get_supported_pixel_format(uint32_t index, uint32_t* format);
+
+// Check if screen is enabled.
+returncode_t libtock_screen_command_enabled(uint32_t* enabled);
+
+// Set the brightness.
+returncode_t libtock_screen_command_set_brightness(uint32_t brightness);
+
+// Invert the screen.
+returncode_t libtock_screen_command_invert_on(void);
+
+// Disable the screen inversion.
+returncode_t libtock_screen_command_invert_off(void);
+
+// Get the current pixel format.
+returncode_t libtock_screen_command_get_pixel_format(void);
+
+// Set the pixel format.
+returncode_t libtock_screen_command_set_pixel_format(uint32_t format);
+
+// Get the current screen rotation.
+returncode_t libtock_screen_command_get_rotation(void);
+
+// Set the screen rotation.
+returncode_t libtock_screen_command_set_rotation(uint32_t rotation);
+
+// Set the current active screen frame.
+returncode_t libtock_screen_command_set_frame(uint16_t x, uint16_t y, uint16_t width, uint16_t height);
+
+// Write the allowed buffer to the screen.
+returncode_t libtock_screen_command_write(uint32_t length);
+
+// Fill the screen from the allowed buffer.
+returncode_t libtock_screen_command_fill(void);
+
+#ifdef __cplusplus
+}
+#endif
+
