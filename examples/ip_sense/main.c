e7ecc928e6cad86befc58fd27216de889c0a6e6d
Brad Campbell <bradjc5@gmail.com>
2024-04-16 12:43:48 -0400

    examples: ip_sense

--- examples/ip_sense/main.c
+++ examples/ip_sense/main.c
@@ -1,13 +1,13 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-#include <ambient_light.h>
-#include <humidity.h>
-#include <temperature.h>
-#include <timer.h>
+#include <libtock-sync/sensors/ambient_light.h>
+#include <libtock-sync/sensors/humidity.h>
+#include <libtock-sync/sensors/temperature.h>
+#include <libtock-sync/services/alarm.h>
 
-#include <ieee802154.h>
-#include <udp.h>
+#include <libtock-sync/net/ieee802154.h>
+#include <libtock-sync/net/udp.h>
 
 static unsigned char BUF_BIND_CFG[2 * sizeof(sock_addr_t)];
 
@@ -24,17 +24,17 @@ int main(void) {
   printf("[IPv6_Sense] Starting IPv6 Sensors App.\n");
   printf("[IPv6_Sense] Sensors will be sampled and transmitted.\n");
 
-  unsigned int humi = 0;
+  int humi = 0;
   int temp = 0;
   int lux  = 0;
   char packet[64];
 
-  ieee802154_set_pan(0xABCD);
-  ieee802154_config_commit();
-  ieee802154_up();
+  libtock_ieee802154_set_pan(0xABCD);
+  libtock_ieee802154_config_commit();
+  libtocksync_ieee802154_up();
 
   ipv6_addr_t ifaces[10];
-  udp_list_ifaces(ifaces, 10);
+  libtock_udp_list_ifaces(ifaces, 10);
 
   sock_handle_t handle;
   sock_addr_t addr = {
@@ -45,7 +45,7 @@ int main(void) {
   printf("Opening socket on ");
   print_ipv6(&ifaces[0]);
   printf(" : %d, and binding to that socket.\n", addr.port);
-  int bind_return = udp_bind(&handle, &addr, BUF_BIND_CFG);
+  int bind_return = libtock_udp_bind(&handle, &addr, BUF_BIND_CFG);
 
   if (bind_return < 0) {
     printf("Failed to bind to port: failure=%d\n", bind_return);
@@ -58,14 +58,14 @@ int main(void) {
   };
 
   while (1) {
-    temperature_read_sync(&temp);
-    humidity_read_sync(&humi);
-    ambient_light_read_intensity_sync(&lux);
+    libtocksync_temperature_read(&temp);
+    libtocksync_humidity_read(&humi);
+    libtocksync_ambient_light_read_intensity(&lux);
 
     int len = snprintf(packet, sizeof(packet), "%d deg C; %d%% humidity; %d lux;\n",
                        temp / 100, humi / 100, lux);
     int max_tx_len;
-    udp_get_max_tx_len(&max_tx_len);
+    libtock_udp_get_max_tx_len(&max_tx_len);
     if (len > max_tx_len) {
       printf("Cannot send packets longer than %d bytes without changing"
              " constants in kernel\n", max_tx_len);
@@ -74,7 +74,7 @@ int main(void) {
     printf("Sending packet (length %d) --> ", len);
     print_ipv6(&(destination.addr));
     printf(" : %d\n", destination.port);
-    ssize_t result = udp_send_to(packet, len, &destination);
+    ssize_t result = libtocksync_udp_send(packet, len, &destination);
 
     switch (result) {
       case RETURNCODE_SUCCESS:
@@ -83,6 +83,6 @@ int main(void) {
       default:
         printf("Error sending packet %d\n\n", result);
     }
-    delay_ms(4000);
+    libtocksync_alarm_delay_ms(4000);
   }
 }
