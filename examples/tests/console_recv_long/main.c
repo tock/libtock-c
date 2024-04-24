33ad6459cdb3956b88fa8556dcbdedff30e8cf05
Brad Campbell <bradjc5@gmail.com>
2024-04-24 17:01:31 -0400

    test: console_recv_long

--- examples/tests/console_recv_long/main.c
+++ examples/tests/console_recv_long/main.c
@@ -1,30 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 
-#include <console.h>
+#include <libtock-sync/interface/console.h>
 
-char buf[100];
+uint8_t buf[100];
 
-static void getnstr_cb(int   result __attribute__ ((unused)),
-                       int   len,
-                       int   _z __attribute__ ((unused)),
-                       void* ud __attribute__ ((unused))) {
+int main(void) {
+  int number_read;
+  int ret = libtocksync_console_read(buf, 61, &number_read);
+  if (ret != RETURNCODE_SUCCESS) {
+    printf("[SHORT] Error doing UART receive: %i\n", ret);
+    return -1;
+  }
+
   printf("\n\nconsole_recv_long: ");
-  for (int i = 0; i < len; i++) {
+  for (int i = 0; i < number_read; i++) {
     printf("%c", buf[i]);
   }
   printf("\n");
-}
-
-
-int main(void) {
-  int ret = getnstr_async(buf, 61, getnstr_cb, NULL);
-  if (ret != RETURNCODE_SUCCESS) {
-    printf("[LONG] Error doing UART receive: %i\n", ret);
-    return -1;
-  }
 
   return 0;
 }
