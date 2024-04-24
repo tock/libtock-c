bc681a50e1c6c4bb3d53b8b19cea9a6413f3e65b
tylerp <potyondyt@gmail.com>
2024-04-24 15:03:44 -0700

    ieee802154

---
+++ libtock-sync/net/ieee802154.c
@@ -0,0 +1,93 @@
+#include <libtock/timer.h>
+
+#include "ieee802154.h"
+
+struct ieee802154_receive_data {
+  bool fired;
+  int pan;
+  int src_addr;
+  int dest_addr;
+};
+
+static struct ieee802154_receive_data receive_result = { .fired = false };
+
+
+struct ieee802154_send_data {
+  bool fired;
+  bool acked;
+  statuscode_t status;
+};
+
+static struct ieee802154_send_data send_result = { .fired = false };
+
+static void ieee802154_receive_done_cb(int pan, int src_addr, int dest_addr) {
+  receive_result.fired     = true;
+  receive_result.pan       = pan;
+  receive_result.src_addr  = src_addr;
+  receive_result.dest_addr = dest_addr;
+}
+
+static void ieee802154_send_done_cb(statuscode_t status, bool acked) {
+  send_result.fired  = true;
+  send_result.acked  = acked;
+  send_result.status = status;
+}
+
+
+returncode_t libtocksync_ieee802154_send(uint16_t         addr,
+                                         security_level_t level,
+                                         key_id_mode_t    key_id_mode,
+                                         uint8_t*         key_id,
+                                         const uint8_t*   payload,
+                                         uint8_t          len) {
+  send_result.fired = false;
+
+  returncode_t ret = libtock_ieee802154_send(addr, level, key_id_mode, key_id, payload, len, ieee802154_send_done_cb);
+  if (ret != RETURNCODE_SUCCESS) return ret;
+
+  // Wait for the frame to be sent
+  yield_for(&send_result.fired);
+
+  return tock_status_to_returncode(send_result.status);
+}
+
+
+returncode_t libtocksync_ieee802154_send_raw(
+  const uint8_t *payload,
+  uint8_t        len) {
+  send_result.fired = false;
+
+  returncode_t ret = libtock_ieee802154_send_raw(payload, len, ieee802154_send_done_cb);
+  if (ret != RETURNCODE_SUCCESS) return ret;
+
+  // Wait for the frame to be sent
+  yield_for(&send_result.fired);
+
+  return tock_status_to_returncode(send_result.status);
+}
+
+returncode_t libtocksync_ieee802154_receive(const libtock_ieee802154_rxbuf *frame) {
+  receive_result.fired = false;
+
+  returncode_t ret = libtock_ieee802154_receive(frame, ieee802154_receive_done_cb);
+  if (ret != RETURNCODE_SUCCESS) return ret;
+
+  // Wait for a frame
+  yield_for(&receive_result.fired);
+
+  // receive upcall is only scheduled by the kernel if a frame is successfully received
+  return RETURNCODE_SUCCESS;
+}
+
+returncode_t libtocksync_ieee802154_up(void) {
+  // Spin until radio is on.
+  bool status;
+  libtock_ieee802154_is_up(&status);
+
+  while (!status) {
+    delay_ms(10);
+    libtock_ieee802154_is_up(&status);
+  }
+  delay_ms(10); // without this delay, immediate calls to send can still fail.
+  return RETURNCODE_SUCCESS;
+}
