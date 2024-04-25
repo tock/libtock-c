eb347c17d81b94e4d2b5a12a925beda5f3c05bb6
Anthony Tarbinian <atarbini@ucsd.edu>
2024-04-25 13:18:43 -0700

    Non-volatile storage flash implementation
    Co-authored-by: Samir Rashid <s3rashid@ucsd.edu>

--- libopenthread/platform/flash.c
+++ libopenthread/platform/flash.c
@@ -1,62 +1,101 @@
+#include <internal/nonvolatile_storage.h>
 #include <openthread/platform/flash.h>
-#include <string.h>
+#include <stdio.h>
 
-const uint32_t SWAP_SIZE = 512;
-uint8_t swapPart0[512];
-uint8_t swapPart1[512];
+// Force flash operations to be synchronous
+static bool done = false;
 
-// NOTE: This is just a temporary version of this file. OpenThread will
-// not operate properly if it is unable to "save" the state. This provides
-// an in memory representation of "flash" that will be lost across power
-// cycles.
+const uint32_t SWAP_SIZE = 1024;
+const uint8_t SWAP_NUM = 2;
+
+uint32_t otPlatFlashGetSwapSize(otInstance *aInstance){
+	OT_UNUSED_VARIABLE(aInstance);
+	return SWAP_SIZE;
+}
+
+static void read_done(__attribute((unused)) int length, __attribute__((unused)) int arg1,
+                      __attribute__((unused)) int arg2, __attribute__((unused)) void *ud) {
+    done = true;
+}
+
+static void write_done(__attribute((unused)) int length, __attribute__((unused)) int arg1,
+                       __attribute__((unused)) int arg2, __attribute__((unused)) void *ud) {
+    done = true;
+}
 
 void otPlatFlashInit(otInstance *aInstance) {
-  OT_UNUSED_VARIABLE(aInstance);
-
-  // initialize swapPart0 and swapPart1
-  memset(swapPart0, 0, SWAP_SIZE);
-  memset(swapPart1, 0, SWAP_SIZE);
-
+    OT_UNUSED_VARIABLE(aInstance);
 }
 
 void otPlatFlashErase(otInstance *aInstance, uint8_t aSwapIndex) {
-  OT_UNUSED_VARIABLE(aInstance);
+    OT_UNUSED_VARIABLE(aInstance);
 
-  if (aSwapIndex == 0) {
-    memset(swapPart0, 0, SWAP_SIZE);
-  } else {
-    memset(swapPart1, 0, SWAP_SIZE);
-  }
-}
+    int num_bytes;
+    nonvolatile_storage_internal_get_number_bytes(&num_bytes);
 
-void otPlatFlashWrite(otInstance *aInstance, uint8_t aSwapIndex, uint32_t aOffset, const void *aData, uint32_t aSize) {
-  OT_UNUSED_VARIABLE(aInstance);
+    // Write all zeroes to flash
+    uint8_t zeroes[SWAP_SIZE];
 
-  if (aSwapIndex == 0) {
-    memcpy(swapPart0 + aOffset, aData, aSize);
-  } else {
-    memcpy(swapPart1 + aOffset, aData, aSize);
-  }
+    otPlatFlashWrite(aInstance, aSwapIndex, 0, zeroes, num_bytes);
 
 }
 
-void otPlatFlashRead(otInstance *aInstance, uint8_t aSwapIndex, uint32_t aOffset, void *aData, uint32_t aSize) {
-  OT_UNUSED_VARIABLE(aInstance);
-  OT_UNUSED_VARIABLE(aSwapIndex);
-  OT_UNUSED_VARIABLE(aOffset);
-  OT_UNUSED_VARIABLE(aData);
-  OT_UNUSED_VARIABLE(aSize);
+void otPlatFlashWrite(otInstance *aInstance, uint8_t aSwapIndex, uint32_t aOffset,
+                      const void *aData, uint32_t aSize) {
+    OT_UNUSED_VARIABLE(aInstance);
+    OT_UNUSED_VARIABLE(aSwapIndex);
+    int ret;
 
-  if (aSwapIndex == 0) {
-    memcpy(aData, swapPart0 + aOffset, aSize);
-  } else {
-    memcpy(aData, swapPart1 + aOffset, aSize);
-  }
+    assert(aSwapIndex < SWAP_NUM);
+    uint32_t offset = aSwapIndex ? SWAP_SIZE : 0;
+    offset += aOffset;
+
+    // Sets up the buffer to store the output of the read
+    ret = nonvolatile_storage_internal_write_buffer((void *)aData, aSize);
+    if (ret != RETURNCODE_SUCCESS) {
+        return;
+    }
+
+    ret = nonvolatile_storage_internal_write_done_subscribe(write_done, NULL);
+    if (ret != RETURNCODE_SUCCESS) {
+        return;
+    }
+
+    done = false;
+    ret = nonvolatile_storage_internal_write(offset, aSize);
+    if (ret != 0) {
+        return;
+    }
+    // wait until callback fires
+    yield_for(&done);
 }
 
-uint32_t otPlatFlashGetSwapSize(otInstance *aInstance)
-{
-  OT_UNUSED_VARIABLE(aInstance);
+void otPlatFlashRead(otInstance *aInstance, uint8_t aSwapIndex, uint32_t aOffset, void *aData,
+                     uint32_t aSize) {
+    OT_UNUSED_VARIABLE(aInstance);
+    OT_UNUSED_VARIABLE(aSwapIndex);
+    int ret;
 
-  return SWAP_SIZE;
+    assert(aSwapIndex < SWAP_NUM);
+    uint32_t offset = aSwapIndex ? SWAP_SIZE : 0;
+    offset += aOffset;
+
+    // Sets up the buffer to store the output of the read
+    ret = nonvolatile_storage_internal_read_buffer(aData, aSize);
+    if (ret != RETURNCODE_SUCCESS) {
+   	return;
+    }
+
+    ret = nonvolatile_storage_internal_read_done_subscribe(read_done, NULL);
+    if (ret != RETURNCODE_SUCCESS) {
+        return;
+    }
+
+    done = false;
+    ret = nonvolatile_storage_internal_read(offset, aSize);
+    if (ret != RETURNCODE_SUCCESS) {
+        return;
+    }
+    // wait until callback fires
+    yield_for(&done);
 }
