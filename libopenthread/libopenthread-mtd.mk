1faa45f6c5b4704c2b134091b579c4353703d4f4
tylerp <potyondyt@gmail.com>
2024-04-25 11:28:17 -0700

    Modify Openthread app build script
    add libopenthread `.mk` file to "bundle" the needed EXTERN_LIB
    includes. This allows for an openthread app to now only include this
    file.

--- libopenthread/libopenthread-mtd.mk
+++ libopenthread/libopenthread-mtd.mk
@@ -1,7 +1,10 @@
 # Include this Makefile in the app's makefile to use the OpenThread Minimal
 # Thread Device (MTD) library.
 
-EXTERN_LIBS += $(OT_TOCK_LIB_DIR)/libopenthread-mtd
-EXTERN_LIBS += $(OT_TOCK_LIB_DIR)/libtcplp
-EXTERN_LIBS += $(OT_TOCK_LIB_DIR)/libmbedtls
-EXTERN_LIBS += $(OT_TOCK_LIB_DIR)/platform
+TOCK_USERLAND_BASE_DIR ?= ..
+LIBOPENTHREAD_DIR := $(TOCK_USERLAND_BASE_DIR)/libopenthread
+
+EXTERN_LIBS += $(LIBOPENTHREAD_DIR)/libopenthread-mtd
+EXTERN_LIBS += $(LIBOPENTHREAD_DIR)/libtcplp
+EXTERN_LIBS += $(LIBOPENTHREAD_DIR)/libmbedtls
+EXTERN_LIBS += $(LIBOPENTHREAD_DIR)/platform
