58f0aeb8ca948bc1b4bcaf2f30e32a03b44e9b29
Brad Campbell <bradjc5@gmail.com>
2024-04-29 11:51:57 -0400

    test: screen-tock

--- examples/tests/screen-tock/main.c
+++ examples/tests/screen-tock/main.c
@@ -1,28 +1,23 @@
-#include <screen.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <timer.h>
+
+#include <libtock-sync/display/screen.h>
+#include <libtock/timer.h>
 
 #define BUFFER_SIZE 10 * 1024
 
+uint8_t screen_buffer[BUFFER_SIZE];
+
 int main(void) {
-  int err;
+  printf("[SCREEN-TOCK] Test Screen Display\n");
 
-  err = screen_init(BUFFER_SIZE);
-  if (err < 0) {
-    printf("buffer allocation failed\n");
-    return -1;
-  }
+  uint32_t width, height;
+  libtock_screen_get_resolution(&width, &height);
 
-  printf("screen init\n");
+  libtocksync_screen_set_brightness(32000);
 
-  size_t width, height;
-  screen_get_resolution(&width, &height);
-
-  screen_set_brightness(32000);
-
-  screen_set_frame(0, 0, width, height);
-  screen_fill(0);
+  libtocksync_screen_set_frame(0, 0, width, height);
+  libtocksync_screen_fill(screen_buffer, BUFFER_SIZE, 0);
 
   size_t char_width = width / 4;
   size_t x, y, w, h;
@@ -34,15 +29,15 @@ int main(void) {
   y = 0;
   w = char_width - 2;
   h = height / 5;
-  screen_set_frame(x, y, w, h);
-  screen_fill(0xFFFF);
+  libtocksync_screen_set_frame(x, y, w, h);
+  libtocksync_screen_fill(screen_buffer, BUFFER_SIZE, 0xFFFF);
 
   x = x_offset + char_width / 3;
   y = 0;
   w = char_width / 3;
   h = height;
-  screen_set_frame(x, y, w, h);
-  screen_fill(0xFFFF);
+  libtocksync_screen_set_frame(x, y, w, h);
+  libtocksync_screen_fill(screen_buffer, BUFFER_SIZE, 0xFFFF);
 
   // O
   x_offset = char_width;
@@ -51,15 +46,15 @@ int main(void) {
   y = 0;
   w = char_width - 2;
   h = height;
-  screen_set_frame(x, y, w, h);
-  screen_fill(0xFFFF);
+  libtocksync_screen_set_frame(x, y, w, h);
+  libtocksync_screen_fill(screen_buffer, BUFFER_SIZE, 0xFFFF);
 
   x = x_offset + (char_width / 3);
   y = height / 5;
   w = char_width / 3;
   h = (height / 5) * 4;
-  screen_set_frame(x, y, w, h);
-  screen_fill(0);
+  libtocksync_screen_set_frame(x, y, w, h);
+  libtocksync_screen_fill(screen_buffer, BUFFER_SIZE, 0);
 
   // C
   x_offset = char_width * 2;
@@ -68,15 +63,15 @@ int main(void) {
   y = 0;
   w = char_width - 2;
   h = height;
-  screen_set_frame(x, y, w, h);
-  screen_fill(0xFFFF);
+  libtocksync_screen_set_frame(x, y, w, h);
+  libtocksync_screen_fill(screen_buffer, BUFFER_SIZE, 0xFFFF);
 
   x = x_offset + (char_width / 3);
   y = height / 5;
   w = 2 * (char_width / 3) + 1;
   h = (height / 5) * 4;
-  screen_set_frame(x, y, w, h);
-  screen_fill(0);
+  libtocksync_screen_set_frame(x, y, w, h);
+  libtocksync_screen_fill(screen_buffer, BUFFER_SIZE, 0);
 
   // K
   x_offset = char_width * 3;
@@ -85,29 +80,29 @@ int main(void) {
   y = 0;
   w = char_width / 3;
   h = height;
-  screen_set_frame(x, y, w, h);
-  screen_fill(0xFFFF);
+  libtocksync_screen_set_frame(x, y, w, h);
+  libtocksync_screen_fill(screen_buffer, BUFFER_SIZE, 0xFFFF);
 
   x = x_offset + 1;
   y = (height / 5) * 2;
   w = char_width - 2;
   h = height / 5;
-  screen_set_frame(x, y, w, h);
-  screen_fill(0xFFFF);
+  libtocksync_screen_set_frame(x, y, w, h);
+  libtocksync_screen_fill(screen_buffer, BUFFER_SIZE, 0xFFFF);
 
   x = x_offset + ((char_width / 3) * 2);
   y = 16;
   w = char_width / 3;
   h = 16;
-  screen_set_frame(x, y, w, h);
-  screen_fill(0xFFFF);
+  libtocksync_screen_set_frame(x, y, w, h);
+  libtocksync_screen_fill(screen_buffer, BUFFER_SIZE, 0xFFFF);
 
   x = x_offset + ((char_width / 3) * 2);
   y = 32;
   w = char_width / 3;
   h = 16;
-  screen_set_frame(x, y, w, h);
-  screen_fill(0xFFFF);
+  libtocksync_screen_set_frame(x, y, w, h);
+  libtocksync_screen_fill(screen_buffer, BUFFER_SIZE, 0xFFFF);
 
   printf("TOCK on screen!\n");
 
