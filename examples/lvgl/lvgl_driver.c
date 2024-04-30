4b7d93ad1c22c5780c087edccbcddec5ff9ed39d
Brad Campbell <bradjc5@gmail.com>
2024-04-29 21:54:13 -0400

    example: lvgl

--- examples/lvgl/lvgl_driver.c
+++ examples/lvgl/lvgl_driver.c
@@ -1,3 +1,8 @@
+#include <libtock-sync/display/screen.h>
+#include <libtock/sensors/touch.h>
+#include <libtock/tock.h>
+#include <lvgl/lvgl.h>
+
 #include "lvgl_driver.h"
 
 static lv_disp_draw_buf_t disp_buf;
@@ -7,8 +12,11 @@ static lv_disp_t *display_device;
 static lv_indev_drv_t indev_drv;
 static lv_indev_t *touch_input_device;
 
-static int touch_status = TOUCH_STATUS_UNSTARTED;
-static int touch_x = 0, touch_y = 0;
+static int touch_status = LIBTOCK_TOUCH_STATUS_UNSTARTED;
+static uint16_t touch_x = 0, touch_y = 0;
+
+static int buffer_size = 0;
+static uint8_t* buffer;
 
 /* screen driver */
 static void screen_lvgl_driver(lv_disp_drv_t * disp, const lv_area_t * area,
@@ -19,13 +27,13 @@ static void screen_lvgl_driver(lv_disp_drv_t * disp, const lv_area_t * area,
   y = area->y1;
   int w = area->x2 - area->x1 + 1;
   int h = area->y2 - area->y1 + 1;
-  screen_set_frame(x, y, w, h);
-  screen_write((w * h) * sizeof(lv_color_t));
+  libtocksync_screen_set_frame(x, y, w, h);
+  libtocksync_screen_write(buffer, buffer_size, (w * h) * sizeof(lv_color_t));
 
   lv_disp_flush_ready(disp);           /* Indicate you are ready with the flushing*/
 }
 
-static void touch_event (int status, int x, int y, __attribute__((unused)) void* ud) {
+static void touch_event (int status, uint16_t x, uint16_t y) {
   touch_status = status;
   touch_x      = x;
   touch_y      = y;
@@ -33,7 +41,7 @@ static void touch_event (int status, int x, int y, __attribute__((unused)) void*
 
 static void my_input_read(__attribute__((unused)) lv_indev_drv_t * drv, lv_indev_data_t*data)
 {
-  if (touch_status == TOUCH_STATUS_PRESSED || touch_status == TOUCH_STATUS_MOVED) {
+  if (touch_status == LIBTOCK_TOUCH_STATUS_PRESSED || touch_status == LIBTOCK_TOUCH_STATUS_MOVED) {
     data->point.x = touch_x;
     data->point.y = touch_y;
     data->state   = LV_INDEV_STATE_PRESSED;
@@ -42,38 +50,41 @@ static void my_input_read(__attribute__((unused)) lv_indev_drv_t * drv, lv_indev
   }
 }
 
+
+
 int lvgl_driver_init (int buffer_lines)
 {
-  size_t width, height;
-  int error = screen_get_resolution(&width, &height);
-  if (error == RETURNCODE_SUCCESS) {
-    error = screen_init(width * buffer_lines * sizeof(lv_color_t));
-    if (error == RETURNCODE_SUCCESS) {
-      /* share the frame buffer with littlevgl */
-      lv_color_t *buf = (lv_color_t*)screen_buffer();
+  uint32_t width, height;
+  int error = libtock_screen_get_resolution(&width, &height);
+  if (error != RETURNCODE_SUCCESS) return error;
 
-      /* initialize littlevgl */
-      lv_init();
-      lv_disp_drv_init(&disp_drv);
-      disp_drv.flush_cb = screen_lvgl_driver;
-      disp_drv.hor_res  = width;
-      disp_drv.ver_res  = height;
-      lv_disp_draw_buf_init(&disp_buf, buf, NULL, width * buffer_lines);
-      disp_drv.draw_buf = &disp_buf;
-      display_device    = lv_disp_drv_register(&disp_drv);
+  buffer_size = width * buffer_lines * sizeof(lv_color_t);
+  error       = libtock_screen_buffer_init(buffer_size, &buffer);
+  if (error != RETURNCODE_SUCCESS) return error;
 
-      int touches;
-      if (get_number_of_touches(&touches) == RETURNCODE_SUCCESS && touches >= 1) {
-        enable_single_touch();
-        single_touch_set_callback(touch_event, NULL);
-        lv_indev_drv_init(&indev_drv);
-        indev_drv.type     = LV_INDEV_TYPE_POINTER;
-        indev_drv.read_cb  = my_input_read;
-        touch_input_device = lv_indev_drv_register(&indev_drv);
-      }
-    }
+  /* share the frame buffer with littlevgl */
+  lv_color_t *buf = (lv_color_t*) buffer;
+
+  /* initialize littlevgl */
+  lv_init();
+  lv_disp_drv_init(&disp_drv);
+  disp_drv.flush_cb = screen_lvgl_driver;
+  disp_drv.hor_res  = width;
+  disp_drv.ver_res  = height;
+  lv_disp_draw_buf_init(&disp_buf, buf, NULL, width * buffer_lines);
+  disp_drv.draw_buf = &disp_buf;
+  display_device    = lv_disp_drv_register(&disp_drv);
+
+  int touches;
+  if (libtock_touch_get_number_of_touches(&touches) == RETURNCODE_SUCCESS && touches >= 1) {
+    libtock_touch_enable_single_touch(touch_event);
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type     = LV_INDEV_TYPE_POINTER;
+    indev_drv.read_cb  = my_input_read;
+    touch_input_device = lv_indev_drv_register(&indev_drv);
   }
-  return error;
+
+  return RETURNCODE_SUCCESS;
 }
 
 void lvgl_driver_event (int millis) {
--- examples/lvgl/lvgl_driver.h
+++ examples/lvgl/lvgl_driver.h
@@ -1,9 +1,4 @@
 #pragma once
 
-#include <tock.h>
-#include <screen.h>
-#include <touch.h>
-#include <lvgl/lvgl.h>
-
 int lvgl_driver_init (int buffer_size);
 void lvgl_driver_event (int mills);
