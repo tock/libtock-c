76b079ae14b1502312934f00d2daa2de69aa6973
Brad Campbell <bradjc5@gmail.com>
2024-05-02 09:50:20 -0700

    test: fault

--- examples/tests/fault/main.c
+++ examples/tests/fault/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-#include <timer.h>
-#include <tock.h>
+#include <libtock-sync/services/alarm.h>
+#include <libtock/tock.h>
 
 int main(void) {
   printf("Testing fault behavior by faulting.\n");
-  delay_ms(1000);
+  libtocksync_alarm_delay_ms(1000);
   int* x = (int*)(0xffffff00);
   *x = 1;
 }
