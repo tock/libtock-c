a1751af954dc48c54cbfcf34a6455cc8d75e4b80
Brad Campbell <bradjc5@gmail.com>
2024-05-08 11:35:45 -0400

    services: ble-env: tests

--- examples/services/ble-env-sense/test/main.c
+++ examples/services/ble-env-sense/test/main.c
@@ -10,8 +10,8 @@ char buf[64] __attribute__((aligned(64)));
 
 typedef enum {
   SENSOR_TEMPERATURE = 0,
-  SENSOR_IRRADIANCE = 1,
-  SENSOR_HUMIDITY = 2,
+  SENSOR_IRRADIANCE  = 1,
+  SENSOR_HUMIDITY    = 2,
 } sensor_type_e;
 
 typedef struct {
