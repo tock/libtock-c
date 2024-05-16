32567dfab62d4a57e259a40abb4e07ac8acd0741
YanchengZhou <yanchengzhou020830@gmail.com>
2024-05-16 18:16:41 -0400

    fix comments

--- lr1110/lr1110/inc_changed/smtc_hal.h
+++ lr1110/lr1110/inc_changed/smtc_hal.h
@@ -7,14 +7,11 @@
 #include "smtc_hal_config.h"
 #include "smtc_hal_mcu.h"
 #include "smtc_hal_gpio.h"
-#include "smtc_hal_uart.h"
 #include "smtc_hal_spi.h"
-#include "smtc_hal_i2c.h"
 #include "smtc_hal_lp_time.h"
 #include "smtc_hal_rtc.h"
 #include "smtc_hal_rng.h"
 #include "smtc_hal_flash.h"
-#include "smtc_hal_watchdog.h"
 #include "smtc_hal_dbg_trace.h"
 
 #endif
