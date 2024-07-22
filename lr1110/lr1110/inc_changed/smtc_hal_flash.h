b611f9495ec79f00b3f0354fb3a43b37664215c2
Brad Campbell <bradjc5@gmail.com>
2024-07-22 17:28:11 -0400

    lr1110: simplify modified headers

--- lr1110/lr1110/inc_changed/smtc_hal_flash.h
+++ lr1110/lr1110/inc_changed/smtc_hal_flash.h
@@ -2,12 +2,38 @@
 #ifndef __SMTC_HAL_FLASH_H
 #define __SMTC_HAL_FLASH_H
 
-
+// We just use the beginning of our flash region and not a specific address.
 #define ADDR_FLASH_LORAWAN_CONTEXT 0
 #define ADDR_FLASH_MODEM_CONTEXT 512
 #define ADDR_FLASH_DEVNONCE_CONTEXT 1024
 #define ADDR_FLASH_SECURE_ELEMENT_CONTEXT 1536
 
+// #define ADDR_FLASH_PAGE_SIZE (( uint32_t )0x00001000 ) // Size of Page = 4 KBytes
+// #define ADDR_FLASH_PAGE_0 (( uint32_t )0x00000000 ) // Base @ of Page 0, 4 KBytes */
+// #define ADDR_FLASH_PAGE( page ) ( ADDR_FLASH_PAGE_0 + ( page ) *ADDR_FLASH_PAGE_SIZE )
+
+// #ifdef SOFTDEVICE_PRESENT
+//     #define APP_FLASH_ADDR_START	0xEA000
+//     #define APP_FLASH_ADDR_END          0xEDFFF
+// #else
+//     #define APP_FLASH_ADDR_START	0xFC000
+//     #define APP_FLASH_ADDR_END      0xFFFFF
+// #endif
+
+// #define APP_FLASH_SIZE_MAX	0x4000
+// #define APP_FLASH_PAGE_MAX	0x04
+
+// #ifdef SOFTDEVICE_PRESENT
+//     #define ADDR_FLASH_LORAWAN_CONTEXT ADDR_FLASH_PAGE(237)
+//     #define ADDR_FLASH_MODEM_CONTEXT ADDR_FLASH_PAGE(236)
+//     #define ADDR_FLASH_DEVNONCE_CONTEXT ADDR_FLASH_PAGE(235)
+//     #define ADDR_FLASH_SECURE_ELEMENT_CONTEXT ADDR_FLASH_PAGE(234)
+// #else
+//     #define ADDR_FLASH_LORAWAN_CONTEXT ADDR_FLASH_PAGE(255)
+//     #define ADDR_FLASH_MODEM_CONTEXT ADDR_FLASH_PAGE(254)
+//     #define ADDR_FLASH_DEVNONCE_CONTEXT ADDR_FLASH_PAGE(253)
+//     #define ADDR_FLASH_SECURE_ELEMENT_CONTEXT ADDR_FLASH_PAGE(252)
+// #endif
 
 #ifdef __cplusplus
 extern "C" {
