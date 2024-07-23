d62f5bb04439fab988752b1c4fb3edc59ddb45e1
Brad Campbell <bradjc5@gmail.com>
2024-07-23 12:15:24 -0400

    mcu hal cleanup

--- lr1110/lr1110/src_changed/smtc_hal_mcu.c
+++ lr1110/lr1110/src_changed/smtc_hal_mcu.c
@@ -1,20 +1,14 @@
 
-//#include "nrf_nvic.h"
-//#include "nrf52840.h"
 #include "smtc_hal.h"
-//#include "nrf_pwr_mgmt.h"
 #include "smtc_hal_rtc.h"
 
 #include <stdarg.h>
 #include <string.h>
 #include <stdio.h>
 
-//#include "hardfault.h"
 
 void hal_clock_init(void)
 {
-    //nrf_drv_clock_init();
-    //nrf_drv_clock_lfclk_request( NULL );
 }
 
 void hal_pwr_init(void)
@@ -28,26 +22,11 @@ void hal_mcu_init( void )
     hal_pwr_init( );
     hal_flash_init( );
     hal_gpio_init( );
-    //hal_rtc_init( );
-    //hal_lp_timer_init( );
-    //hal_debug_init( );
     hal_spi_init( );
-    //hal_rng_init( );
 }
-
-// void hal_mcu_disable_irq( void )
-// {
-//     //__disable_irq();
-// }
-
-// void hal_mcu_enable_irq( void )
-// {
-//     //__enable_irq();
-// }
 
 void hal_mcu_reset( void )
 {
-    // sd_nvic_SystemReset( );
 }
 
 void __attribute__( ( optimize( "O0" ) ) ) hal_mcu_wait_us( const int32_t microseconds )
@@ -65,11 +44,6 @@ void hal_mcu_wait_ms( const int32_t ms )
     libtocksync_alarm_delay_ms(ms);
 }
 
-// void hal_mcu_partial_sleep_enable( bool enable )
-// {
-	
-// }
-
 void hal_mcu_set_sleep_for_ms( const int32_t milliseconds )
 {
     bool last_sleep_loop = false;
@@ -78,48 +52,7 @@ void hal_mcu_set_sleep_for_ms( const int32_t milliseconds )
     if( milliseconds <= 0 ) return;
 
     hal_rtc_wakeup_timer_set_ms( milliseconds );
-
-    // do
-    // {
-    //     int32_t time_sleep = 0;
-    //     float time_sleep_max = RTC_2_PER_TICK * RTC_2_MAX_TICKS;
-    //     if( time_counter > time_sleep_max )
-    //     {
-    //         time_sleep = time_sleep_max;
-    //         time_counter -= time_sleep;
-    //     }
-    //     else
-    //     {
-    //         time_sleep = time_counter;
-    //         last_sleep_loop = true;
-    //     }
-
-    //     if( time_sleep > 50 )
-    //     {
-    //         hal_rtc_wakeup_timer_set_ms( time_sleep );
-    //         //nrf_pwr_mgmt_run( );
-    //     }
-    // } while( last_sleep_loop == false );
 }
-
-// static char string[HAL_PRINT_BUFFER_SIZE];
-// static void vprint( const char* fmt, va_list argp )
-// {
-//     if( 0 < vsprintf( string, fmt, argp ) )  // build string
-//     {
-//         hal_debug_init( );
-//         hal_uart_tx(( uint8_t* ) string, strlen( string ) );
-//         hal_debug_deinit( );
-//     }
-// }
-
-// void hal_mcu_trace_print( const char* fmt, ... )
-// {
-//     va_list argp;
-//     va_start( argp, fmt );
-//     vprint( fmt, argp );
-//     va_end( argp );
-// }
 
 void hal_hex_to_bin( char *input, uint8_t *dst, int len )
 {
@@ -134,12 +67,3 @@ void hal_hex_to_bin( char *input, uint8_t *dst, int len )
 		if (i >= (2 * len )) break;
 	}
 }
-
-// void hal_print_bin_to_hex( uint8_t *buf, uint16_t len )
-// {
-//     for( uint16_t i = 0; i < len; i++ )
-//     {
-//         PRINTF( "%02X", buf[i] );
-//     }
-//     PRINTF( "\r\n" );
-// }
