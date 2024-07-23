84eff7d564fcc5d631d8a5c3c0ce0ce5f5dfaf24
Brad Campbell <bradjc5@gmail.com>
2024-07-23 12:15:55 -0400

    trace hal cleanup

--- lr1110/lr1110/src_changed/smtc_hal_trace.c
+++ lr1110/lr1110/src_changed/smtc_hal_trace.c
@@ -2,7 +2,7 @@
 #include <stdint.h>   // C99 types
 #include <stdbool.h>  // bool type
 #include <string.h>
-// #include "smtc_hal_uart.h"
+
 #include "smtc_hal_trace.h"
 
 #define PRINT_BUFFER_SIZE 256
@@ -18,11 +18,5 @@ void hal_trace_print_var( const char* fmt, ... )
 char string[PRINT_BUFFER_SIZE];
 void hal_trace_print( const char* fmt, va_list argp )
 {
-    // if( 0 < vsprintf( string, fmt, argp ) )  // build string
-    // {
-    //     hal_debug_init( );
-    //     hal_uart_tx(( uint8_t* ) string, strlen( string ));
-    //     hal_debug_deinit( );
-    // }
     vprintf(fmt, argp); // directly prints formatted string
 }
