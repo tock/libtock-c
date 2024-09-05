0d1e1d2fc6ae21a471f954ff4819106373fcc999
Amit Aryeh Levy <amit@amitlevy.com>
2024-09-05 16:02:08 -0700

    Upgrade lvgl, modify lvgl example

--- examples/lvgl/lvgl_driver.c
+++ examples/lvgl/lvgl_driver.c
@@ -5,21 +5,19 @@
 
 #include "lvgl_driver.h"
 
-static lv_disp_draw_buf_t disp_buf;
-static lv_disp_drv_t disp_drv;
-static lv_disp_t* display_device;
+static lv_display_t* display_device;
 
-static lv_indev_drv_t indev_drv;
-static lv_indev_t* touch_input_device;
+//static lv_indev_drv_t indev_drv;
+//static lv_indev_t* touch_input_device;
 
-static int touch_status = LIBTOCK_TOUCH_STATUS_UNSTARTED;
-static uint16_t touch_x = 0, touch_y = 0;
+/*static int touch_status = LIBTOCK_TOUCH_STATUS_UNSTARTED;
+static uint16_t touch_x = 0, touch_y = 0;*/
 
 static int buffer_size = 0;
 static uint8_t* buffer;
 
 /* screen driver */
-static void screen_lvgl_driver(lv_disp_drv_t* disp, const lv_area_t* area,
+static void screen_lvgl_driver(lv_display_t* disp, const lv_area_t* area,
                                __attribute__ ((unused)) lv_color_t* color_p) {
   int32_t x, y;
   x = area->x1;
@@ -29,10 +27,10 @@ static void screen_lvgl_driver(lv_disp_drv_t* disp, const lv_area_t* area,
   libtocksync_screen_set_frame(x, y, w, h);
   libtocksync_screen_write(buffer, buffer_size, (w * h) * sizeof(lv_color_t));
 
-  lv_disp_flush_ready(disp);           /* Indicate you are ready with the flushing*/
+  lv_display_flush_ready(disp);           /* Indicate you are ready with the flushing*/
 }
 
-static void touch_event(int status, uint16_t x, uint16_t y) {
+/*static void touch_event(int status, uint16_t x, uint16_t y) {
   touch_status = status;
   touch_x      = x;
   touch_y      = y;
@@ -46,9 +44,38 @@ static void my_input_read(__attribute__((unused)) lv_indev_drv_t* drv, lv_indev_
   } else {
     data->state = LV_INDEV_STATE_RELEASED;
   }
+}*/
+
+static uint32_t tick_cb(void) {
+  uint32_t ticks;
+  libtock_alarm_command_read(&ticks);
+
+  uint32_t frequency;
+  libtock_alarm_command_get_frequency(&frequency);
+
+  uint64_t seconds = (ticks / frequency);
+  uint64_t milliseconds_per_second = 1000;
+
+  // Calculate the conversion of full seconds to ticks.
+  uint64_t milliseconds = seconds * milliseconds_per_second;
+
+  // To get conversion accuracy within 1 millisecond, the conversion
+  // must also convert partial seconds.
+  uint64_t leftover_ticks = ticks % frequency;
+
+  // This calculation is mathematically equivalent to doing:
+  //
+  //   `leftover_ticks` / (`frequency` / `milliseconds_per_second`)
+  //
+  // This is taking the remaining unconverted ticks (leftover_ticks)
+  // and dividing by the number of ticks per millisecond
+  // (`frequency` (ticks per second) / `1000` milliseconds per second)
+  // The division is done this way because of the same argument in
+  // `ms_to_ticks`.
+  milliseconds += (leftover_ticks * milliseconds_per_second) / frequency;
+
+  return milliseconds;
 }
-
-
 
 int lvgl_driver_init(int buffer_lines) {
   uint32_t width, height;
@@ -64,27 +91,23 @@ int lvgl_driver_init(int buffer_lines) {
 
   /* initialize littlevgl */
   lv_init();
-  lv_disp_drv_init(&disp_drv);
-  disp_drv.flush_cb = screen_lvgl_driver;
-  disp_drv.hor_res  = width;
-  disp_drv.ver_res  = height;
-  lv_disp_draw_buf_init(&disp_buf, buf, NULL, width * buffer_lines);
-  disp_drv.draw_buf = &disp_buf;
-  display_device    = lv_disp_drv_register(&disp_drv);
 
-  int touches;
+  display_device    = lv_display_create(width, height);
+  lv_display_set_flush_cb(display_device, screen_lvgl_driver);
+  lv_display_set_antialiasing(display_device, false);
+
+  lv_display_set_buffers(display_device, buf, NULL, buffer_size, LV_DISPLAY_RENDER_MODE_PARTIAL);
+
+  lv_tick_set_cb(tick_cb);
+
+  /*int touches;
   if (libtock_touch_get_number_of_touches(&touches) == RETURNCODE_SUCCESS && touches >= 1) {
     libtock_touch_enable_single_touch(touch_event);
     lv_indev_drv_init(&indev_drv);
     indev_drv.type     = LV_INDEV_TYPE_POINTER;
     indev_drv.read_cb  = my_input_read;
     touch_input_device = lv_indev_drv_register(&indev_drv);
-  }
+  }*/
 
   return RETURNCODE_SUCCESS;
-}
-
-void lvgl_driver_event(int millis) {
-  lv_tick_inc(millis);
-  lv_task_handler();
 }
--- examples/lvgl/main.c
+++ examples/lvgl/main.c
@@ -7,7 +7,7 @@
 
 #include "lvgl_driver.h"
 
-static void event_handler(lv_event_t* e) {
+/*static void event_handler(lv_event_t* e) {
   lv_event_code_t code  = lv_event_get_code(e);
   unsigned int* seconds = (unsigned int*)lv_event_get_user_data(e);
 
@@ -17,48 +17,47 @@ static void event_handler(lv_event_t* e) {
   } else if (code == LV_EVENT_VALUE_CHANGED) {
     LV_LOG_USER("Toggled");
   }
-}
+}*/
 
 int main(void) {
-  unsigned int seconds = 0;
-
   libtocksync_screen_set_brightness(100);
   int status = lvgl_driver_init(5);
   if (status == RETURNCODE_SUCCESS) {
     /* LittlevGL's Hello World tutorial example */
 
-    lv_obj_t* scr = lv_disp_get_scr_act(NULL);          /*Get the current screen*/
+    lv_obj_t* scr = lv_screen_active();          /*Get the current screen*/
 
-    /*Create a Label on the currently active screen*/
+    lv_obj_t * obj1 = lv_obj_create(lv_screen_active());
+    lv_obj_set_height(obj1, lv_pct(100));
+    lv_obj_set_width(obj1, lv_pct(100));
+
     lv_obj_t* label1 = lv_label_create(scr);
 
-    /*Modify the Label's text*/
     lv_label_set_text(label1, "Hello world!");
 
-    /* Align the Label to the center
-     * NULL means align on parent (which is the screen now)
-     * 0, 0 at the end means an x, y offset after alignment*/
     lv_obj_align(label1, LV_ALIGN_CENTER, 0, 0);
 
     lv_obj_t* btn1 = lv_btn_create(scr);
-    lv_obj_add_event_cb(btn1, event_handler, LV_EVENT_ALL, &seconds);
+    //lv_obj_add_event_cb(btn1, event_handler, LV_EVENT_ALL, &seconds);
     lv_obj_align(btn1, LV_ALIGN_CENTER, 0, -40);
 
     lv_obj_t* label = lv_label_create(btn1);
     lv_label_set_text(label, "Reset");
     lv_obj_center(label);
 
-    /* main loop */
+    lv_task_handler();
+
+    int seconds = 0;
     while (1) {
+      libtocksync_alarm_delay_ms(1000);
       seconds++;
-      if (seconds % 200 == 0) {
-        char buffer[100];
-        snprintf(buffer, 99, "Seconds: %d", seconds / 200);
-        lv_label_set_text(label1, buffer);
-      }
-      libtocksync_alarm_delay_ms(5);
-      lvgl_driver_event(5);
+      char buffer[100];
+      int written = snprintf(buffer, sizeof(buffer), "%d", seconds);
+      lv_label_set_text(label1, buffer);
+      lv_task_handler();
+      lv_tick_inc(LV_DEF_REFR_PERIOD);
     }
+
   } else {
     printf("lvgl init error: %s\n", tock_strrcode(status));
   }
