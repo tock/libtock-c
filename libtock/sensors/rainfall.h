5c2dc37cc1893fbbfd2343f6368664e6e15d18c1
Alistair Francis <alistair@alistair23.me>
2024-11-18 10:09:23 +1000

    libtock: sensors: Support rainfall sensor
    Signed-off-by: Alistair Francis <alistair@alistair23.me>

---
+++ libtock/sensors/rainfall.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "../tock.h"
+#include "syscalls/rainfall_syscalls.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Function signature for rainfall data callback.
+//
+// - `arg1` (`int`): Returncode indicating status from sampling the sensor.
+// - `arg2` (`uint32_t`): the number of mm of rain in the time period specified
+typedef void (*libtock_rainfall_callback)(returncode_t, uint32_t);
+
+// Start a rainfall measurement. The reading will be provided via the callback.
+returncode_t libtock_rainfall_read(libtock_rainfall_callback cb, int hours);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ libtock/sensors/syscalls/rainfall_syscalls.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "../../tock.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define DRIVER_NUM_RAINFALL 0x6000B
+
+// Check if the rainfall driver is installed.
+bool libtock_rainfall_exists(void);
+
+// Configure the upcall for when the reading is ready.
+returncode_t libtock_rainfall_set_upcall(subscribe_upcall callback, void* opaque);
+
+// Read the sensor.
+returncode_t libtock_rainfall_command_read(int hours);
+
+#ifdef __cplusplus
+}
+#endif
