6fc82313732df15e230d860069856d3babe5ba6f
Tyler Potyondy <potyondyt@gmail.com>
2024-12-05 12:10:36 -0800

    [Thread Tutorial] Update sensor app checkpoints.

--- examples/tutorials/thread_network/02_sensor_final/main.c
+++ examples/tutorials/thread_network/02_sensor_final/main.c
@@ -12,9 +12,14 @@ int main(void) {
   // We measure the temperature in the main loop and
   //print this value to the console.
   while (1) {
-    // Measure temperature.
+    // Measure temperature -- returned in the form 2200 => 22C
     libtocksync_temperature_read(&current_temperature);
-    printf("Current temperature: %d\r\n", current_temperature);
+
+    // Convert temperature
+    int whole_degree = current_temperature / 100;
+    int decimal_degree = current_temperature % 100;
+    
+    printf("Current temperature: %i.%i\r\n", whole_degree, decimal_degree);
     
     // Delay 1000 ms (1 second). 
     libtocksync_alarm_delay_ms(1000);
