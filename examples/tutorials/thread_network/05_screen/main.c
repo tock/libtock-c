d03ed9b0b818207f03e139702c7ff912506bd963
Tyler Potyondy <potyondyt@gmail.com>
2024-12-05 17:00:56 -0800

    [Thread tutorial] Screen start and checkpoint 6

---
+++ examples/tutorials/thread_network/05_screen/main.c
@@ -0,0 +1,8 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+
+}
---
+++ examples/tutorials/thread_network/06_screen_button/main.c
@@ -0,0 +1,25 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <libtock/interface/button.h>
+
+static void button_callback(returncode_t ret,
+                            int          btn_num,
+                            bool         pressed) {
+
+  if (pressed) {
+    printf("Button %i pressed!\r\n", btn_num);
+  }
+}
+
+int main(void) {
+  for (int i = 0; i < 4; i++){
+    libtock_button_notify_on_press(i, button_callback);
+  }
+
+  for(;;) {
+    yield();
+  }  
+}
