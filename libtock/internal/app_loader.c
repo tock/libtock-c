6587756048da50cdfdc582a699b2967b56b8ea92
viswajith-g <gyx4bw@virginia.edu>
2025-02-02 18:24:13 -0500

    Switched to newer libtock interface for app_loader apps.

--- libtock/internal/app_loader.c
+++ libtock/internal/app_loader.c
@@ -1,27 +1,27 @@
-#include "internal/app_loader.h"
-#include "tock.h"
+#include "libtock/internal/app_loader.h"
+#include "libtock/tock.h"
 
 int app_loader_exists(void) {
   syscall_return_t res = command(DRIVER_NUM_APP_LOADER, 0, 0, 0);
   return tock_command_return_novalue_to_returncode(res);
 }
 
-int app_loader_setup_subscribe(subscribe_upcall cb, void *userdata) {
+int app_loader_setup_subscribe(subscribe_upcall cb, void* userdata) {
   subscribe_return_t sval = subscribe(DRIVER_NUM_APP_LOADER, 0, cb, userdata);
   return tock_subscribe_return_to_returncode(sval);
 }
 
-int app_loader_write_subscribe(subscribe_upcall cb, void *userdata) {
+int app_loader_write_subscribe(subscribe_upcall cb, void* userdata) {
   subscribe_return_t sval = subscribe(DRIVER_NUM_APP_LOADER, 1, cb, userdata);
   return tock_subscribe_return_to_returncode(sval);
 }
 
-int app_loader_load_subscribe(subscribe_upcall cb, void *userdata) {
+int app_loader_load_subscribe(subscribe_upcall cb, void* userdata) {
   subscribe_return_t sval = subscribe(DRIVER_NUM_APP_LOADER, 2, cb, userdata);
   return tock_subscribe_return_to_returncode(sval);
 }
 
-int app_loader_write_buffer(uint8_t *buffer, uint32_t len) {
+int app_loader_write_buffer(uint8_t* buffer, uint32_t len) {
   allow_ro_return_t aval = allow_readonly(DRIVER_NUM_APP_LOADER, 0, (void*) buffer, len);
   return tock_allow_ro_return_to_returncode(aval);
 }
