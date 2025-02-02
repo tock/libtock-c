aec0d4c6c21e43182106464ad4f17894eb9615d6
viswajith-g <gyx4bw@virginia.edu>
2025-02-02 18:24:13 -0500

    Switched to newer libtock interface for app_loader apps.

--- libtock/internal/app_loader.h
+++ libtock/internal/app_loader.h
@@ -5,7 +5,7 @@ extern "C"
 {
 #endif
 
-#include "tock.h"
+#include "libtock/tock.h"
 
 // Driver number for system call
 #define DRIVER_NUM_APP_LOADER   0x10001
@@ -23,11 +23,11 @@ int app_loader_exists(void);
  * This function takes in the function that will be executed
  * when the callback is triggered.
  */
-int app_loader_setup_subscribe(subscribe_upcall cb, void *userdata);
+int app_loader_setup_subscribe(subscribe_upcall cb, void* userdata);
 
 /*
  * Command to request the kernel to set up for a new app.
- * This functions takes the size of the new app as the argument. 
+ * This functions takes the size of the new app as the argument.
  */
 int app_loader_command_setup(uint32_t app_length);
 
@@ -36,26 +36,26 @@ int app_loader_command_setup(uint32_t app_length);
  * This function takes in the function that will be executed
  * when the callback is triggered.
  */
-int app_loader_write_subscribe(subscribe_upcall cb, void *userdata);
+int app_loader_write_subscribe(subscribe_upcall cb, void* userdata);
 
 /*
  * Function to set up a shared buffer with the capsule.
- * This buffer is used to write data to the flash. 
+ * This buffer is used to write data to the flash.
  */
-int app_loader_write_buffer(uint8_t *buffer, uint32_t len);
+int app_loader_write_buffer(uint8_t* buffer, uint32_t len);
 
 /*
  * Command to write new app binary to the capsule.
  * Specify the offset at which the data segment should be written.
  * Also specify how long the data segment will be in bytes.
- * 
- * After writing data, the userspace app should yield for 
+ *
+ * After writing data, the userspace app should yield for
  * the write done callback from the capsule.
  */
 int app_loader_command_write(uint32_t flash_offset, uint32_t write_length);
 
 /*
- * Command to request the kernel to load the newly flashed app. 
+ * Command to request the kernel to load the newly flashed app.
  */
 int app_loader_command_load(void);
 
@@ -64,7 +64,7 @@ int app_loader_command_load(void);
  * This function takes in the function that will be executed
  * when the callback is triggered.
  */
-int app_loader_load_subscribe(subscribe_upcall cb, void *userdata);
+int app_loader_load_subscribe(subscribe_upcall cb, void* userdata);
 
 
 /*
