c5a1ce2f070615a598f0257eadfa7f4d2a80acbb
viswajith-g <gyx4bw@virginia.edu>
2025-02-12 15:26:05 -0500

    moved syscall wrappers to their own file and moved binaries.h and python tool to tests.

--- libtock/internal/app_loader/app_loader.h
+++ libtock/internal/app_loader/app_loader.h
@@ -5,44 +5,45 @@ extern "C"
 {
 #endif
 
+#include "libtock/internal/app_loader/syscalls/app_loader_syscalls.h"
 #include "libtock/tock.h"
-
-// Driver number for system call
-#define DRIVER_NUM_APP_LOADER   0x10001
 
 #define BUTTON1 0
 #define BUTTON2 1
 
 /*
- * Command to check if the `app_loader` capsule exists.
+ * Function to setup the callback from capsule.
+ * This function takes in the function that will be executed
+ * when the callback is triggered.
  */
-int app_loader_exists(void);
+returncode_t libtock_app_loader_set_setup_upcall(subscribe_upcall cb, void* userdata);
 
 /*
  * Function to setup the callback from capsule.
  * This function takes in the function that will be executed
  * when the callback is triggered.
  */
-int app_loader_setup_subscribe(subscribe_upcall cb, void* userdata);
+returncode_t libtock_app_loader_set_write_upcall(subscribe_upcall cb, void* userdata);
+
+/*
+ * Function to setup the callback from capsule.
+ * This function takes in the function that will be executed
+ * when the callback is triggered.
+ */
+returncode_t libtock_app_loader_set_load_upcall(subscribe_upcall cb, void* userdata);
 
 /*
  * Command to request the kernel to set up for a new app.
  * This functions takes the size of the new app as the argument.
  */
-int app_loader_command_setup(uint32_t app_length);
+returncode_t libtock_app_loader_setup(uint32_t app_length);
 
-/*
- * Function to setup the callback from capsule.
- * This function takes in the function that will be executed
- * when the callback is triggered.
- */
-int app_loader_write_subscribe(subscribe_upcall cb, void* userdata);
 
 /*
  * Function to set up a shared buffer with the capsule.
  * This buffer is used to write data to the flash.
  */
-int app_loader_write_buffer(uint8_t* buffer, uint32_t len);
+returncode_t libtock_app_loader_set_buffer(uint8_t* buffer, uint32_t len);
 
 /*
  * Command to write new app binary to the capsule.
@@ -52,25 +53,19 @@ int app_loader_write_buffer(uint8_t* buffer, uint32_t len);
  * After writing data, the userspace app should yield for
  * the write done callback from the capsule.
  */
-int app_loader_command_write(uint32_t flash_offset, uint32_t write_length);
+returncode_t libtock_app_loader_write(uint32_t flash_offset, uint32_t write_length);
 
 /*
  * Command to request the kernel to load the newly flashed app.
  */
-int app_loader_command_load(void);
-
-/*
- * Function to setup the callback from capsule.
- * This function takes in the function that will be executed
- * when the callback is triggered.
- */
-int app_loader_load_subscribe(subscribe_upcall cb, void* userdata);
+returncode_t libtock_app_loader_load(void);
 
 
 /*
  * Internal function to write new app to flash. Takes app size as argument.
  */
 int write_app(double size, uint8_t binary[]);
+
 
 
 #ifdef __cplusplus
