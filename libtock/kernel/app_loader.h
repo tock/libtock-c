e8170d61ec40d96f5d0aab41d5c9a141b6bc5b72
viswajith-g <gyx4bw@virginia.edu>
2025-02-13 13:46:50 -0500

    clean up and revert lvgl version change

--- libtock/kernel/app_loader.h
+++ libtock/kernel/app_loader.h
@@ -1,48 +1,49 @@
-#include "libtock/internal/app_loader/app_loader.h"
+#pragma once
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "libtock/kernel/syscalls/app_loader_syscalls.h"
+#include "libtock/tock.h"
+
+#define BUTTON1 0
+#define BUTTON2 1
 
 /*
  * Function to setup the callback from capsule.
  * This function takes in the function that will be executed
  * when the callback is triggered.
  */
-returncode_t libtock_app_loader_set_setup_upcall(subscribe_upcall cb, void* userdata) {
-  return libtock_app_loader_setup_subscribe(cb, userdata);
-}
+returncode_t libtock_app_loader_set_setup_upcall(subscribe_upcall cb, void* userdata);
 
 /*
  * Function to setup the callback from capsule.
  * This function takes in the function that will be executed
  * when the callback is triggered.
  */
-returncode_t libtock_app_loader_set_write_upcall(subscribe_upcall cb, void* userdata) {
-  return libtock_app_loader_write_subscribe(cb, userdata);
-}
+returncode_t libtock_app_loader_set_write_upcall(subscribe_upcall cb, void* userdata);
 
 /*
  * Function to setup the callback from capsule.
  * This function takes in the function that will be executed
  * when the callback is triggered.
  */
-returncode_t libtock_app_loader_set_load_upcall(subscribe_upcall cb, void* userdata) {
-  return libtock_app_loader_load_subscribe(cb, userdata);
-}
+returncode_t libtock_app_loader_set_load_upcall(subscribe_upcall cb, void* userdata);
 
 /*
  * Command to request the kernel to set up for a new app.
  * This functions takes the size of the new app as the argument.
  */
-returncode_t libtock_app_loader_setup(uint32_t app_length) {
-  return libtock_app_loader_command_setup(app_length);
-}
+returncode_t libtock_app_loader_setup(uint32_t app_length);
 
 
 /*
  * Function to set up a shared buffer with the capsule.
  * This buffer is used to write data to the flash.
  */
-returncode_t libtock_app_loader_set_buffer(uint8_t* buffer, uint32_t len) {
-  return libtock_app_loader_write_buffer(buffer, len);
-}
+returncode_t libtock_app_loader_set_buffer(uint8_t* buffer, uint32_t len);
 
 /*
  * Command to write new app binary to the capsule.
@@ -52,13 +53,21 @@ returncode_t libtock_app_loader_set_buffer(uint8_t* buffer, uint32_t len) {
  * After writing data, the userspace app should yield for
  * the write done callback from the capsule.
  */
-returncode_t libtock_app_loader_write(uint32_t flash_offset, uint32_t write_length) {
-  return libtock_app_loader_command_write(flash_offset, write_length);
-}
+returncode_t libtock_app_loader_write(uint32_t flash_offset, uint32_t write_length);
 
 /*
  * Command to request the kernel to load the newly flashed app.
  */
-returncode_t libtock_app_loader_load(void) {
-  return libtock_app_loader_command_load();
+returncode_t libtock_app_loader_load(void);
+
+
+/*
+ * Internal function to write new app to flash. Takes app size as argument.
+ */
+int write_app(double size, uint8_t binary[]);
+
+
+
+#ifdef __cplusplus
 }
+#endif
