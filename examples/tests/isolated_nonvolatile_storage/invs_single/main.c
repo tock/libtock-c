45a442a23460eab697d6925d82743e4bdf7187f4
Brad Campbell <bradjc5@gmail.com>
2025-02-25 12:34:34 -0500

    tests: invs: add write tests

--- examples/tests/isolated_nonvolatile_storage/invs_single/main.c
+++ examples/tests/isolated_nonvolatile_storage/invs_single/main.c
@@ -6,11 +6,9 @@
 
 
 
-
 struct nv_data {
   bool fired;
   returncode_t ret;
-  int length;
   int storage_size;
 };
 
@@ -22,31 +20,37 @@ static struct nv_data result = {.fired = false};
 //   result.storage_size = number_bytes;
 // }
 
-// static void write_cb(returncode_t ret, int length) {
-//   result.fired  = true;
-//   result.ret    = ret;
-//   result.length = length;
-// }
+static void write_cb(returncode_t ret) {
+  result.fired = true;
+  result.ret   = ret;
+}
 
-static void read_cb(returncode_t ret, int length) {
-  result.fired  = true;
-  result.ret    = ret;
-  result.length = length;
+static void read_cb(returncode_t ret) {
+  result.fired = true;
+  result.ret   = ret;
+}
+
+static void write_done(int                          ret,
+                       __attribute__ ((unused)) int arg1,
+                       __attribute__ ((unused)) int arg2,
+                       void*                        opaque) {
+  libtock_isolated_nonvolatile_storage_callback_write cb = (libtock_isolated_nonvolatile_storage_callback_write) opaque;
+  cb(tock_status_to_returncode(ret));
 }
 
 static void read_done(int                          ret,
-                      int                          length,
+                      __attribute__ ((unused)) int arg1,
                       __attribute__ ((unused)) int arg2,
                       void*                        opaque) {
   libtock_isolated_nonvolatile_storage_callback_read cb = (libtock_isolated_nonvolatile_storage_callback_read) opaque;
-  cb(tock_status_to_returncode(ret), length);
+  cb(tock_status_to_returncode(ret));
 }
 
 
-static uint32_t get_region_size(void) {
-   uint32_t num_bytes;
-    libtocksync_isolated_nonvolatile_storage_get_number_bytes(&num_bytes);
-    return  num_bytes;
+static uint64_t get_region_size(void) {
+  uint64_t num_bytes;
+  libtocksync_isolated_nonvolatile_storage_get_number_bytes(&num_bytes);
+  return num_bytes;
 }
 
 
@@ -61,7 +65,7 @@ static bool test_exists(void) {
 }
 
 static bool test_size(void) {
-  uint32_t num_bytes;
+  uint64_t num_bytes;
   returncode_t ret;
 
   ret = libtocksync_isolated_nonvolatile_storage_get_number_bytes(&num_bytes);
@@ -81,13 +85,10 @@ static bool test_read(void) {
 
   uint32_t offset = 0;
   uint32_t length = 10;
-  uint32_t length_read;
 
-  ret = libtocksync_isolated_nonvolatile_storage_read(offset, length, readbuf, sizeof(readbuf), &length_read);
+  ret = libtocksync_isolated_nonvolatile_storage_read(offset, readbuf, length);
   if (ret == RETURNCODE_SUCCESS) {
-    if (length_read == length) {
-      return true;
-    }
+    return true;
   }
   return false;
 }
@@ -98,30 +99,10 @@ static bool test_read_zerobuffer(void) {
   uint8_t readbuf[512];
 
   uint32_t offset = 0;
-  uint32_t length = 10;
-  uint32_t length_read;
 
-  ret = libtocksync_isolated_nonvolatile_storage_read(offset, length, readbuf, 0, &length_read);
+  ret = libtocksync_isolated_nonvolatile_storage_read(offset, readbuf, 0);
   if (ret == RETURNCODE_ERESERVE) {
-      return true;
-  }
-  return false;
-}
-
-static bool test_read_buffertoosmall(void) {
-  returncode_t ret;
-
-  uint8_t readbuf[512];
-
-  uint32_t offset = 0;
-  uint32_t length = 10;
-  uint32_t length_read;
-
-  ret = libtocksync_isolated_nonvolatile_storage_read(offset, length, readbuf, 5, &length_read);
-  if (ret == RETURNCODE_SUCCESS) {
-    if (length_read == 5) {
-      return true;
-    }
+    return true;
   }
   return false;
 }
@@ -130,7 +111,6 @@ static bool test_read_fail_noallow(void) {
   returncode_t ret;
 
   uint32_t offset = 0;
-  uint32_t length = 10;
 
   result.fired = false;
 
@@ -140,7 +120,7 @@ static bool test_read_fail_noallow(void) {
   ret = libtock_isolated_nonvolatile_storage_set_allow_readwrite_read_buffer(NULL, 0);
   if (ret != RETURNCODE_SUCCESS) return false;
 
-  ret = libtock_isolated_nonvolatile_storage_command_read(offset, length);
+  ret = libtock_isolated_nonvolatile_storage_command_read(offset);
   if (ret != RETURNCODE_SUCCESS) return false;
 
   yield_for(&result.fired);
@@ -156,33 +136,111 @@ static bool test_read_fail_notwithinregion(void) {
 
   uint8_t readbuf[512];
 
-  uint32_t offset;
-  uint32_t length;
-  uint32_t length_read;
+  uint64_t offset;
+  size_t length;
 
-
-  offset  = get_region_size() -5;
+  offset = get_region_size() - 5;
   length = 10;
-  ret = libtocksync_isolated_nonvolatile_storage_read(offset, length, readbuf, sizeof(readbuf), &length_read);
-  printf("returncode %d\n",ret);
+  ret    = libtocksync_isolated_nonvolatile_storage_read(offset, readbuf, length);
   if (ret != RETURNCODE_EINVAL) return false;
 
   offset = get_region_size() + 10;
   length = 10;
-  ret = libtocksync_isolated_nonvolatile_storage_read(offset, length, readbuf, sizeof(readbuf), &length_read);
-  printf("returncode %d\n",ret);
+  ret    = libtocksync_isolated_nonvolatile_storage_read(offset, readbuf, length);
   if (ret != RETURNCODE_EINVAL) return false;
 
   offset = 0;
   length = get_region_size() + 1;
-  ret = libtocksync_isolated_nonvolatile_storage_read(offset, length, readbuf, sizeof(readbuf), &length_read);
-  printf("returncode %d\n",ret);
+  ret    = libtocksync_isolated_nonvolatile_storage_read(offset, readbuf, length);
   if (ret != RETURNCODE_EINVAL) return false;
 
   offset = get_region_size() - 1;
   length = 2;
-  ret = libtocksync_isolated_nonvolatile_storage_read(offset, length, readbuf, sizeof(readbuf), &length_read);
-  printf("returncode %d\n",ret);
+  ret    = libtocksync_isolated_nonvolatile_storage_read(offset, readbuf, length);
+  if (ret != RETURNCODE_EINVAL) return false;
+
+  return true;
+}
+
+static bool test_write(void) {
+  returncode_t ret;
+
+  uint8_t writebuf[512] = {0x3f};
+
+  uint32_t offset = 0;
+  uint32_t length = 10;
+
+  ret = libtocksync_isolated_nonvolatile_storage_write(offset, writebuf, length);
+  if (ret == RETURNCODE_SUCCESS) {
+    return true;
+  }
+  return false;
+}
+
+static bool test_write_zerobuffer(void) {
+  returncode_t ret;
+
+  uint8_t writebuf[512];
+
+  uint32_t offset = 0;
+
+  ret = libtocksync_isolated_nonvolatile_storage_read(offset, writebuf, 0);
+  if (ret == RETURNCODE_ERESERVE) {
+    return true;
+  }
+  return false;
+}
+
+static bool test_write_fail_noallow(void) {
+  returncode_t ret;
+
+  uint32_t offset = 0;
+
+  result.fired = false;
+
+  ret = libtock_isolated_nonvolatile_storage_set_upcall_write_done(write_done, write_cb);
+  if (ret != RETURNCODE_SUCCESS) return false;
+
+  ret = libtock_isolated_nonvolatile_storage_set_allow_readonly_write_buffer(NULL, 0);
+  if (ret != RETURNCODE_SUCCESS) return false;
+
+  ret = libtock_isolated_nonvolatile_storage_command_write(offset);
+  if (ret != RETURNCODE_SUCCESS) return false;
+
+  yield_for(&result.fired);
+
+  if (result.ret == RETURNCODE_ERESERVE) {
+    return true;
+  }
+  return false;
+}
+
+static bool test_write_fail_notwithinregion(void) {
+  returncode_t ret;
+
+  uint8_t writebuf[512] = {0x99};
+
+  uint64_t offset;
+  size_t length;
+
+  offset = get_region_size() - 5;
+  length = 10;
+  ret    = libtocksync_isolated_nonvolatile_storage_read(offset, writebuf, length);
+  if (ret != RETURNCODE_EINVAL) return false;
+
+  offset = get_region_size() + 10;
+  length = 10;
+  ret    = libtocksync_isolated_nonvolatile_storage_read(offset, writebuf, length);
+  if (ret != RETURNCODE_EINVAL) return false;
+
+  offset = 0;
+  length = get_region_size() + 1;
+  ret    = libtocksync_isolated_nonvolatile_storage_read(offset, writebuf, length);
+  if (ret != RETURNCODE_EINVAL) return false;
+
+  offset = get_region_size() - 1;
+  length = 2;
+  ret    = libtocksync_isolated_nonvolatile_storage_read(offset, writebuf, length);
   if (ret != RETURNCODE_EINVAL) return false;
 
   return true;
@@ -194,33 +252,74 @@ int main(void) {
   printf("[TEST] Isolated Nonvolatile Storage - Single\n");
 
   printf("[INVS TEST] Exists: ");
-  if (test_exists()) printf("Success\n");
-  else printf("Fail\n");
+  if (test_exists()) {
+    printf("Success\n");
+  } else {
+    printf("Fail\n");
+  }
 
   printf("[INVS TEST] Size: ");
-  if (test_size()) printf("Success\n");
-  else printf("Fail\n");
+  if (test_size()) {
+    printf("Success\n");
+  } else {
+    printf("Fail\n");
+  }
 
   printf("[INVS TEST] Read: ");
-  if (test_read()) printf("Success\n");
-  else printf("Fail\n");
+  if (test_read()) {
+    printf("Success\n");
+  } else {
+    printf("Fail\n");
+  }
 
   printf("[INVS TEST] Read Zero Buffer: ");
-  if (test_read_zerobuffer()) printf("Success\n");
-  else printf("Fail\n");
-
-  printf("[INVS TEST] Read Buffer Too Small: ");
-  if (test_read_buffertoosmall()) printf("Success\n");
-  else printf("Fail\n");
+  if (test_read_zerobuffer()) {
+    printf("Success\n");
+  } else {
+    printf("Fail\n");
+  }
 
   printf("[INVS TEST] Read Fail No Allow: ");
-  if (test_read_fail_noallow()) printf("Success\n");
-  else printf("Fail\n");
+  if (test_read_fail_noallow()) {
+    printf("Success\n");
+  } else {
+    printf("Fail\n");
+  }
 
   printf("[INVS TEST] Read Fail Not Within Region: ");
-  if (test_read_fail_notwithinregion()) printf("Success\n");
-  else printf("Fail\n");
+  if (test_read_fail_notwithinregion()) {
+    printf("Success\n");
+  } else {
+    printf("Fail\n");
+  }
 
+  printf("[INVS TEST] Write: ");
+  if (test_write()) {
+    printf("Success\n");
+  } else {
+    printf("Fail\n");
+  }
+
+  printf("[INVS TEST] Write Zero Buffer: ");
+  if (test_write_zerobuffer()) {
+    printf("Success\n");
+  } else {
+    printf("Fail\n");
+  }
+
+  printf("[INVS TEST] Write Fail No Allow: ");
+  if (test_write_fail_noallow()) {
+    printf("Success\n");
+  } else {
+    printf("Fail\n");
+  }
+
+  printf("[INVS TEST] Write Fail Not Within Region: ");
+  if (test_write_fail_notwithinregion()) {
+    printf("Success\n");
+  } else {
+    printf("Fail\n");
+  }
 
   return 0;
 }
