ba6eb1f1011ab2693554e1e1671f007fe61ca513
Brad Campbell <bradjc5@gmail.com>
2025-02-25 12:34:34 -0500

    tests: invs: add write tests

---
+++ examples/tests/app_loader/button-press-loading/app_binaries.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdint.h>
+
+// app binaries
+extern uint8_t blink[2048];
+extern uint8_t adc[16384];
+
+#ifdef __cplusplus
+}
+#endif
