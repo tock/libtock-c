3552552f12ab01225f3f3203f53a2f462269e4c7
Pat Pannuto <pat.pannuto@gmail.com>
2025-03-05 16:31:35 -0800

    lwip: resolve conflicting BYTE_ORDER

--- lwip/include/arch/cc.h
+++ lwip/include/arch/cc.h
@@ -4,6 +4,11 @@
 #define SYS_MBOX_NULL   NULL
 #define SYS_SEM_NULL    NULL
 
+// `newlib` also uses a macro named BYTE_ORDER, but very differently
+// so we need to remove its definition first if it happened to be in use.
+#ifdef BYTE_ORDER
+#undef BYTE_ORDER
+#endif
 #define BYTE_ORDER LITTLE_ENDIAN
 
 typedef void * sys_prot_t;
