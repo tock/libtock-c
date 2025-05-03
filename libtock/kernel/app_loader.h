aa5400df54a44fcbaf2ab37a688ecd3b4056308c
viswajith-g <gyx4bw@virginia.edu>
2025-05-02 20:28:25 -0400

    changed app_binaries directory structure

--- libtock/kernel/app_loader.h
+++ libtock/kernel/app_loader.h
@@ -17,35 +17,35 @@ extern "C"
  * This function takes in the function that will be executed
  * when the callback is triggered.
  */
-returncode_t libtock_app_loader_set_setup_upcall(subscribe_upcall cb, void* userdata);
+returncode_t libtock_app_loader_setup_set_upcall(subscribe_upcall cb, void* userdata);
 
 /*
  * Function to setup the callback from capsule.
  * This function takes in the function that will be executed
  * when the callback is triggered.
  */
-returncode_t libtock_app_loader_set_write_upcall(subscribe_upcall cb, void* userdata);
+returncode_t libtock_app_loader_write_set_upcall(subscribe_upcall cb, void* userdata);
 
 /*
  * Function to setup the callback from capsule.
  * This function takes in the function that will be executed
  * when the callback is triggered.
  */
-returncode_t libtock_app_loader_set_finalize_upcall(subscribe_upcall cb, void* userdata);
+returncode_t libtock_app_loader_finalize_set_upcall(subscribe_upcall cb, void* userdata);
 
 /*
  * Function to setup the callback from capsule.
  * This function takes in the function that will be executed
  * when the callback is triggered.
  */
-returncode_t libtock_app_loader_set_load_upcall(subscribe_upcall cb, void* userdata);
+returncode_t libtock_app_loader_load_set_upcall(subscribe_upcall cb, void* userdata);
 
 /*
  * Function to setup the callback from capsule.
  * This function takes in the function that will be executed
  * when the callback is triggered.
  */
-returncode_t libtock_app_loader_set_abort_upcall(subscribe_upcall cb, void* userdata);
+returncode_t libtock_app_loader_abort_set_upcall(subscribe_upcall cb, void* userdata);
 
 /*
  * Command to request the kernel to set up for a new app.
