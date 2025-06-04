79cae3a6f4cde95d49f567081e18bf6102450ee1
viswajith-g <gyx4bw@virginia.edu>
2025-06-04 16:19:27 -0400

    process_control: upkeep to match app_loader

--- examples/tutorials/dynamic-apps-and-policies/process_manager/main.c
+++ examples/tutorials/dynamic-apps-and-policies/process_manager/main.c
@@ -297,7 +297,6 @@ static void get_binary_names(void) {
 
     offset += binary_name_len + 1;  // 1 for null terminator
   }
-  // printf("app name: %s\n", binary_names[i]);
 }
 
 // Uses the App Load service to install requested binary.
@@ -390,15 +389,13 @@ muif_t muif_list[] = {
   MUIF_U8G2_U16_LIST("ID", &selection, NULL, process_menu_get_item, process_menu_get_item_count,
                      mui_u8g2_u16_list_goto_w1_pi),
   MUIF_U8G2_U16_LIST("DE", &det_selection, NULL, details_get_str, details_get_cnt, mui_u8g2_u16_list_goto_w1_pi),
-  MUIF_U8G2_U16_LIST("LA", &binary_selection, NULL, binaries_get_str, binaries_get_cnt, mui_u8g2_u16_list_goto_w1_pi),
+  MUIF_U8G2_U16_LIST("BS", &binary_selection, NULL, binaries_get_str, binaries_get_cnt, mui_u8g2_u16_list_goto_w1_pi),
 
   MUIF_VARIABLE("CM", &process_control, mui_u8g2_u8_opt_line_wa_mse_pi),
   MUIF_BUTTON("CN", mui_u8g2_btn_goto_process_control),
   MUIF_BUTTON("CO", mui_u8g2_btn_goto_wm_fi),
 
   MUIF_BUTTON("AL", mui_u8g2_btn_goto_load_new_app),
-  // MUIF_RO("AL", mui_u8g2_btn_goto_load_new_app),
-  // MUIF_BUTTON("NO", mui_u8g2_btn_goto_no_load),
 
 
 };
@@ -418,26 +415,22 @@ fds_t* fds =
   MUI_XYA("GC", 5, 49, 2)
 
 
-
+  // APP LOAD SCREEN
   MUI_FORM(20)
   MUI_STYLE(0)
   MUI_LABEL(5, 10, "Select Application")
   MUI_XY("HR", 0, 12)
   MUI_STYLE(1)
-  MUI_XYA("LA", 5, 25, 0)
-  MUI_XYA("LA", 5, 37, 1)
-  MUI_XYA("LA", 5, 49, 2)
-  MUI_XYA("LA", 5, 61, 3)
+  MUI_XYA("BS", 5, 25, 0)
+  MUI_XYA("BS", 5, 37, 1)
+  MUI_XYA("BS", 5, 49, 2)
+  // MUI_XYA("BS", 5, 61, 3)
 
   MUI_FORM(21)
-  // MUI_AUX("AL")
   MUI_STYLE(0)
   MUI_LABEL(5, 10, "Load Application?")
-  // &u8g2.drawButtonUTF8(62, 20, U8G2_BTN_HCENTER, 34,  2,  2, "Yes" );
-  // MUI_XYAT("AL", 45, 25, 24, "Yes")
   MUI_XYAT(".G", 45, 35, 40, "Yes")
   MUI_XYAT(".G", 55, 48, 20, "No")
-  // MUI_XYT("NO", 65, 38, "No")
 
 
   MUI_FORM(40)
@@ -459,6 +452,7 @@ fds_t* fds =
   MUI_XYAT(".G", 46, 25, 20, "Back")
 
 
+  // PROCESS CONTROL SCREEN
   MUI_FORM(3)
   MUI_STYLE(0)
   MUI_LABEL(5, 10, "Process List")
