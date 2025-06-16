c427d95192edacad73f134436ea5e0525f491157
Eugene Shamis <eugene.shamis@amd.com>
2025-06-16 11:55:57 -0400

    Clean up unused task queue code

--- libtock-sync/kernel/read_only_state.c
+++ libtock-sync/kernel/read_only_state.c
@@ -1,18 +1,14 @@
 #include "read_only_state.h"
 
 returncode_t libtocksync_read_only_state_quick_yield(void* base, int* pending_tasks) {
-  if (yield_check_tasks()) {
-    *pending_tasks = 1;
-    return RETURNCODE_SUCCESS;
-  } else {
-    uint32_t tasks = libtock_read_only_state_get_pending_tasks(base);
 
-    if (tasks > 0) {
-      // Waiting tasks, call yield
-      yield();
-    }
+  uint32_t tasks = libtock_read_only_state_get_pending_tasks(base);
 
-    *pending_tasks = tasks;
-    return RETURNCODE_SUCCESS;
+  if (tasks > 0) {
+    // Waiting tasks, call yield
+    yield();
   }
+
+  *pending_tasks = tasks;
+  return RETURNCODE_SUCCESS;
 }
