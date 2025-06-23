2179029b8f1cc24a04c43c1ebb5d478d89c79756
Kat Fox <kat@zerorisc.com>
2025-06-23 10:47:03 -0700

    tutorials: root_of_trust: switch to snake case to match libtock-c convention

---
+++ examples/tutorials/root_of_trust/suspicious_service/main.c
@@ -0,0 +1,97 @@
+#include "libtock/tock.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <libtock-sync/interface/console.h>
+#include <libtock/kernel/ipc.h>
+
+#include "oracle.h"
+
+#define LOG_WIDTH             32
+
+bool started          = false;
+bool log_done         = false;
+size_t screen_service = -1;
+char log_buf[LOG_WIDTH] __attribute__((aligned(LOG_WIDTH)));
+
+const char SCREEN_SERVICE_NAME[] = "org.tockos.tutorials.attestation.screen";
+
+static void ipc_callback(__attribute__ ((unused)) int   pid,
+                         __attribute__ ((unused)) int   len,
+                         __attribute__ ((unused)) int   arg2,
+                         __attribute__ ((unused)) void* ud) {
+  started = true;
+}
+
+static void log_done_callback(__attribute__ ((unused)) int   pid,
+                              __attribute__ ((unused)) int   len,
+                              __attribute__ ((unused)) int   arg2,
+                              __attribute__ ((unused)) void* ud) {
+  log_done = true;
+}
+
+static void wait_for_start(void) {
+  // Register an IPC callback and wait for it to be called by the
+  // screen app based on the user's app selection.
+  ipc_register_service_callback("org.tockos.tutorials.attestation.suspicious", ipc_callback,
+                                NULL);
+  yield_for(&started);
+}
+
+static int setup_logging() {
+  returncode_t ret;
+
+  // Find the PID of the screen logging service
+  ret = ipc_discover(SCREEN_SERVICE_NAME, &screen_service);
+  if (ret != RETURNCODE_SUCCESS) {
+    printf("Screen logging service not found.\n");
+    return ret;
+  }
+
+  // Set up a callback and share so we can supply log messages
+  // and know when they've been completely logged.
+  ipc_register_client_callback(screen_service, log_done_callback, NULL);
+  ipc_share(screen_service, log_buf, LOG_WIDTH);
+
+  return 0;
+}
+
+static int log_to_screen(const char* message) {
+  returncode_t ret;
+
+  // Copy up to the log buffer's size of the message, with room for a null byte.
+  uint16_t len = strnlen(message, sizeof(log_buf) - 1);
+  memcpy(log_buf, message, len);
+
+  // Add the null byte.
+  log_buf[len] = '\0';
+
+  // Start the logging process.
+  ret = ipc_notify_service(screen_service);
+  if (ret != RETURNCODE_SUCCESS) {
+    printf("Failed to request a log to screen.\n");
+    return ret;
+  }
+
+  // Wait for the log to complete.
+  yield_for(&log_done);
+  log_done = false;
+
+  return 0;
+}
+
+int main(void) {
+  // Wait to receive the signal to start from the app selector.
+  wait_for_start();
+
+  // Set up logging service.
+  setup_logging();
+
+  // Retrieve the first byte of the plaintext buffer to get a secret
+  log_to_screen("Stealing plaintext...");
+  for (uint8_t *addr = (uint8_t *)0x2000E7D8; addr < (uint8_t *)0x2000E800; addr++) { 
+    printf("%p: %02X\n", addr, *addr);
+  }
+  
+  return 0;
+}
