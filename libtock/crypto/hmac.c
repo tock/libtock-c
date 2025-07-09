04abdc08c705f47ee6854b9cd97e53058f8ca51d
Brad Campbell <bradjc5@gmail.com>
2025-07-09 13:55:02 -0400

    libtock: hmac: correct buffer arg name

--- libtock/crypto/hmac.c
+++ libtock/crypto/hmac.c
@@ -11,7 +11,7 @@ static void hmac_upcall(int ret,
 returncode_t libtock_hmac_simple(libtock_hmac_algorithm_t hmac_type,
                                  uint8_t* key_buffer, uint32_t key_length,
                                  uint8_t* input_buffer, uint32_t input_length,
-                                 uint8_t* hash_buffer, uint32_t hash_length,
+                                 uint8_t* hmac_buffer, uint32_t hmac_length,
                                  libtock_hmac_callback_hmac cb) {
 
   returncode_t ret;
@@ -25,7 +25,7 @@ returncode_t libtock_hmac_simple(libtock_hmac_algorithm_t hmac_type,
   ret = libtock_hmac_set_readonly_allow_data_buffer(input_buffer, input_length);
   if (ret != RETURNCODE_SUCCESS) return ret;
 
-  ret = libtock_hmac_set_readwrite_allow_destination_buffer(hash_buffer, hash_length);
+  ret = libtock_hmac_set_readwrite_allow_destination_buffer(hmac_buffer, hmac_length);
   if (ret != RETURNCODE_SUCCESS) return ret;
 
   ret = libtock_hmac_set_upcall(hmac_upcall, cb);
