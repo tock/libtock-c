cf1d693c34954f6393f3d6c221c8432cf05f6717
Brad Campbell <bradjc5@gmail.com>
2025-07-09 21:18:57 -0400

    ywf: screen

--- libtock-sync/display/screen.h
+++ libtock-sync/display/screen.h
@@ -1,13 +1,38 @@
 #pragma once
 
-#include <libtock/display/screen.h>
+#include <libtock/display/screen_types.h>
 #include <libtock/tock.h>
+
+#include "syscalls/screen_syscalls.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 bool libtocksync_screen_exists(void);
+
+// Allocate and setup `buffer` with size `len`.
+//
+// This will allocate len bytes and assign buffer to the array. This buffer can
+// be used for drawing the screen.
+statuscode_t libtocksync_screen_buffer_init(size_t len, uint8_t** buffer);
+
+// Get the number of supported resolutions for the screen.
+returncode_t libtocksync_screen_get_supported_resolutions(uint32_t* resolutions);
+
+// Get a particular resolution based on `index`. `width` and `height` will be
+// set with the resolution corresponding to that index.
+returncode_t libtocksync_screen_get_supported_resolution(size_t index, uint32_t* width, uint32_t* height);
+
+// Get the number or supported pixel formats.
+returncode_t libtocksync_screen_get_supported_pixel_formats(uint32_t* formats);
+
+// Get the particular format based on `index`. `format` will be set with the
+// correct pixel format for that index.
+returncode_t libtocksync_screen_get_supported_pixel_format(size_t index, libtock_screen_format_t* format);
+
+// Calculate and return the number of bits per pixel for the given pixel format.
+int libtocksync_screen_get_bits_per_pixel(libtock_screen_format_t format);
 
 // Set the screen brightness.
 returncode_t libtocksync_screen_set_brightness(uint32_t brightness);
@@ -20,6 +45,9 @@ returncode_t libtocksync_screen_invert_off(void);
 
 // Get the current pixel format used by the screen.
 returncode_t libtocksync_screen_get_pixel_format(libtock_screen_format_t* format);
+
+// Get the current screen resolution.
+returncode_t libtocksync_screen_get_resolution(uint32_t* width, uint32_t* height);
 
 // Get the current screen rotation.
 returncode_t libtocksync_screen_get_rotation(libtock_screen_rotation_t* rotation);
--- libtock/display/screen.h
+++ libtock/display/screen.h
@@ -1,27 +1,11 @@
 #pragma once
 
 #include "../tock.h"
+#include "screen_types.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-// Supported pixel formats.
-typedef enum {
-  MONO     =0,
-  RGB_233  =1,
-  RGB_565  =2,
-  RGB_888  =3,
-  ARGB_8888=4,
-} libtock_screen_format_t;
-
-// Supported screen rotations.
-typedef enum {
-  ROTATION_NORMAL=0,
-  ROTATION_90    =1,
-  ROTATION_180   =2,
-  ROTATION_270   =3,
-} libtock_screen_rotation_t;
 
 // Callback when an operation has completed.
 //
