e42a47e4876cf1bd0cd12cb94adcff042d043ca6
Brad Campbell <bradjc5@gmail.com>
2025-07-09 20:38:52 -0400

    ywf: sha

--- libtock-sync/crypto/sha.c
+++ libtock-sync/crypto/sha.c
@@ -1,36 +1,30 @@
 #include "sha.h"
 
-struct sha_data {
-  bool fired;
-  returncode_t ret;
-};
-
-static struct sha_data result = {.fired = false};
-
-static void sha_cb_hash(returncode_t ret) {
-  result.fired = true;
-  result.ret   = ret;
-}
-
 returncode_t libtocksync_sha_simple_hash(libtock_sha_algorithm_t hash_type,
                                          uint8_t* input_buffer, uint32_t input_length,
                                          uint8_t* hash_buffer, uint32_t hash_length) {
   returncode_t ret;
 
-  result.fired = false;
-
-  ret = libtock_sha_simple_hash(hash_type, input_buffer, input_length, hash_buffer, hash_length, sha_cb_hash);
+  ret = libtock_sha_command_set_algorithm((uint8_t) hash_type);
   if (ret != RETURNCODE_SUCCESS) return ret;
 
-  // Wait for the callback.
-  yield_for(&result.fired);
-  if (result.ret != RETURNCODE_SUCCESS) return result.ret;
-
-  ret = libtock_sha_set_readonly_allow_data_buffer(NULL, 0);
+  ret = libtock_sha_set_readonly_allow_data_buffer(input_buffer, input_length);
   if (ret != RETURNCODE_SUCCESS) return ret;
 
-  ret = libtock_sha_set_readwrite_allow_destination_buffer(NULL, 0);
-  if (ret != RETURNCODE_SUCCESS) return ret;
+  ret = libtock_sha_set_readwrite_allow_destination_buffer(hash_buffer, hash_length);
+  if (ret != RETURNCODE_SUCCESS) goto exit1;
 
-  return RETURNCODE_SUCCESS;
+  ret = libtock_sha_command_run();
+  if (ret != RETURNCODE_SUCCESS) goto exit2;
+
+  // Wait for the operation.
+  ret = libtocksync_sha_yield_wait_for();
+
+exit2:
+  libtock_sha_set_readwrite_allow_destination_buffer(NULL, 0);
+
+exit1:
+  libtock_sha_set_readonly_allow_data_buffer(NULL, 0);
+
+  return ret;
 }
