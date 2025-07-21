ede5ed8e26f26f7de0a34993853c215dd29444ea
Pat Pannuto <pat.pannuto@gmail.com>
2025-07-21 15:46:59 -0700

    defer: name parameter in macro to avoid warning

--- libtock/defer.h
+++ libtock/defer.h
@@ -5,9 +5,9 @@ extern "C" {
 #endif
 
 #define __DEFER__(F, V) \
-  auto void F(int*); \
+  auto void F(int* V ## SENTINEL); \
   [[gnu::cleanup(F)]] int V; \
-  auto void F(int*)
+  auto void F(__attribute__ ((unused)) int* V ## SENTINEL)
 
 #define defer __DEFER(__COUNTER__)
 #define __DEFER(N) __DEFER_(N)
