48041b0a3432d1a02a866b86e499a3b17d34c2d4
Pat Pannuto <pat.pannuto@gmail.com>
2025-07-23 10:56:05 -0700

    build: check compiler version and enforce >= 13

--- Helpers.mk
+++ Helpers.mk
@@ -40,26 +40,6 @@ ifdef TOCK_USERLAND_BASE_DIR
     endif
 endif
 
-# # Validate the the toolchain is new enough (known not to work for gcc <= 5.1)
-# CC_VERSION_MAJOR := $(shell $(CC) -dumpversion | cut -d '.' -f1)
-# ifeq (1,$(shell expr $(CC_VERSION_MAJOR) \>= 6))
-#   # Opportunistically turn on gcc 6.0+ warnings since we're already version checking:
-#   override CPPFLAGS += -Wduplicated-cond #  if (p->q != NULL) { ... } else if (p->q != NULL) { ... }
-#   override CPPFLAGS += -Wnull-dereference # deref of NULL (thought default if -fdelete-null-pointer-checks, in -Os, but no?)
-# else
-#   ifneq (5,$(CC_VERSION_MAJOR))
-#     $(info CC=$(CC))
-#     $(info $$(CC) -dumpversion: $(shell $(CC) -dumpversion))
-#     $(error Your compiler is too old. Need gcc version > 5.1)
-#   endif
-#     CC_VERSION_MINOR := $(shell $(CC) -dumpversion | cut -d '.' -f2)
-#   ifneq (1,$(shell expr $(CC_VERSION_MINOR) \> 1))
-#     $(info CC=$(CC))
-#     $(info $$(CC) -dumpversion: $(shell $(CC) -dumpversion))
-#     $(error Your compiler is too old. Need gcc version > 5.1)
-#   endif
-# endif
-
 
 # Format check rule
 .PHONY: _format_check_unstaged
