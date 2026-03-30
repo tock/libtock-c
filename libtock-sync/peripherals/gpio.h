e128cc1d81e5b2543f9102c3616d4073f9336847
Brad Campbell <bradjc5@gmail.com>
2026-03-30 12:24:59 -0400

    libtock: use _types.h for peripherals

--- libtock-sync/peripherals/gpio.h
+++ libtock-sync/peripherals/gpio.h
@@ -9,8 +9,23 @@ extern "C" {
 
 bool libtocksync_gpio_exists(void);
 
+// Returns the number of GPIO pins configured on the board.
+returncode_t libtocksync_gpio_count(int* count);
+
+// Configure a GPIO pin as an output.
+returncode_t libtocksync_gpio_enable_output(uint32_t pin);
+
 // Configure a GPIO pin as an input.
 returncode_t libtocksync_gpio_enable_input(uint32_t pin, libtock_gpio_input_mode_t pin_config);
+
+// Set an output pin high.
+returncode_t libtocksync_gpio_set(uint32_t pin);
+
+// Set an output pin low.
+returncode_t libtocksync_gpio_clear(uint32_t pin);
+
+// Toggle an output pin.
+returncode_t libtocksync_gpio_toggle(uint32_t pin);
 
 // Read the current state of a GPIO input pin.
 returncode_t libtocksync_gpio_read(uint32_t pin, int* pin_value);
