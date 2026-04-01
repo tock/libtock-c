46080415fc9c21d708a839a5e55a2b9d1efd31eb
Brad Campbell <bradjc5@gmail.com>
2026-03-31 23:43:34 -0400

    libtock-sync: hack udp

--- libtock-sync/net/udp.c
+++ libtock-sync/net/udp.c
@@ -3,20 +3,21 @@
 #include <libtock/defer.h>
 #include <libtock/net/syscalls/udp_syscalls.h>
 
-#include "udp.h"
 #include "syscalls/udp_syscalls.h"
+#include "udp.h"
 
 bool libtocksync_udp_exists(void) {
   return libtock_udp_driver_exists();
 }
 
 returncode_t libtocksync_udp_send(void* buf, size_t len,
-                                  sock_addr_t* dst_addr) {
+                                  __attribute__ ((unused)) sock_addr_t* dst_addr) {
   returncode_t ret;
 
   ret = libtock_udp_set_readonly_allow(buf, len);
   if (ret != RETURNCODE_SUCCESS) return ret;
-  defer { libtock_udp_set_readonly_allow(NULL, 0); }
+  defer { libtock_udp_set_readonly_allow(NULL, 0);
+  }
 
   ret = libtocksync_udp_yield_wait_for_send();
   return ret;
@@ -27,7 +28,8 @@ returncode_t libtocksync_udp_recv(void* buf, size_t len, size_t* received_len) {
 
   ret = libtock_udp_set_readwrite_allow_rx(buf, len);
   if (ret != RETURNCODE_SUCCESS) return ret;
-  defer { libtock_udp_set_readwrite_allow_rx(NULL, 0); }
+  defer { libtock_udp_set_readwrite_allow_rx(NULL, 0);
+  }
 
   ret = libtocksync_udp_yield_wait_for_recv(received_len);
   return ret;
