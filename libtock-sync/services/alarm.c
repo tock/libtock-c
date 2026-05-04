369316b09d626608a5c0e78436364ba59403b7cc
Brad Campbell <bradjc5@gmail.com>
2026-05-04 13:03:50 -0400

    ywf: revert alarm.c changes
    Using YWF with alarm is more nuanced because users might reasonably
    expect to have both periodic timers and do a delay_ms.

--- libtock-sync/services/alarm.c
+++ libtock-sync/services/alarm.c
@@ -1,25 +1,31 @@
 #include "alarm.h"
 
-#include "syscalls/alarm_syscalls.h"
+struct alarm_cb_data {
+  bool fired;
+};
 
-static void alarm_noop_cb(__attribute__ ((unused)) uint32_t now,
-                          __attribute__ ((unused)) uint32_t scheduled,
-                          __attribute__ ((unused)) void*    opaque) {}
+static struct alarm_cb_data delay_data = { .fired = false };
+
+static void delay_cb(__attribute__ ((unused)) uint32_t now,
+                     __attribute__ ((unused)) uint32_t scheduled,
+                     __attribute__ ((unused)) void*    opaque) {
+  delay_data.fired = true;
+}
 
 int libtocksync_alarm_delay_ms(uint32_t ms) {
+  delay_data.fired = false;
   libtock_alarm_t alarm;
   int rc;
 
-  if ((rc = libtock_alarm_in_ms(ms, alarm_noop_cb, NULL, &alarm)) != RETURNCODE_SUCCESS) {
+  if ((rc = libtock_alarm_in_ms(ms, delay_cb, NULL, &alarm)) != RETURNCODE_SUCCESS) {
     return rc;
   }
 
-  return libtocksync_alarm_yield_wait_for();
+  yield_for(&delay_data.fired);
+  return rc;
 }
 
-static struct alarm_cb_data {
-  bool fired;
-} yf_timeout_data = { .fired = false };
+static struct alarm_cb_data yf_timeout_data = { .fired = false };
 
 static void yf_timeout_cb(__attribute__ ((unused)) uint32_t now,
                           __attribute__ ((unused)) uint32_t scheduled,
