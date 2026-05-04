1c2cf069a41849f5aff6f3c27dd8148977d60b14
Brad Campbell <bradjc5@gmail.com>
2026-05-04 16:40:16 -0400

    kv: ywf: fix exists, statuscode

--- libtock-sync/storage/syscalls/kv_syscalls.c
+++ libtock-sync/storage/syscalls/kv_syscalls.c
@@ -4,7 +4,7 @@ returncode_t libtocksync_kv_yield_wait_for_get(int* length) {
   yield_waitfor_return_t ywf;
   returncode_t ret;
   ywf = yield_wait_for(DRIVER_NUM_KV, 0);
-  ret = (returncode_t) ywf.data0;
+  ret = tock_status_to_returncode(ywf.data0);
   if (ret != RETURNCODE_SUCCESS) return ret;
   *length = (int) ywf.data1;
   return ret;
@@ -13,5 +13,5 @@ returncode_t libtocksync_kv_yield_wait_for_get(int* length) {
 returncode_t libtocksync_kv_yield_wait_for_done(void) {
   yield_waitfor_return_t ywf;
   ywf = yield_wait_for(DRIVER_NUM_KV, 0);
-  return (returncode_t) ywf.data0;
+  return tock_status_to_returncode(ywf.data0);
 }
